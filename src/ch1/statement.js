34c50996131a97250bfed59417b3164fe6f795b6
Bell <bell04204@gmail.com>
2022-12-27 00:58:41 +0900

    refactor: rename function name format -> usd (change function declaration)

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -10,11 +10,11 @@ function statement(invoice, plays) {
         volumeCredits += volumeCreditsFor(perf);
 
         // 청구 내역을 출력한다.
-        result += ` ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${perf.audience}석)\n`
+        result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`
         totalAmount += amountFor(perf);
     }
 
-    result += `총액: ${format(totalAmount / 100)}\n`;
+    result += `총액: ${usd(totalAmount)}\n`;
     result += `적립 포인트: ${volumeCredits}점\n`;
 
     return result;
@@ -62,11 +62,11 @@ function statement(invoice, plays) {
     }
 }
 
-function format(aNumber) {
+function usd(aNumber) {
     return new Intl.NumberFormat(
         "en-US",
         {style: "currency", currency: "USD", minimumFractionDigits: 2}
-    ).format(aNumber);
+    ).format(aNumber / 100);
 }
 
 const result = statement(invoicesJson[0], playsJson);
