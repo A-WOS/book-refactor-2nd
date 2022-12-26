cc503f2427b65301b5540d0e1bf10994aa421d53
Bell <bell04204@gmail.com>
2022-12-27 04:10:31 +0900

    feat: 공연료 계산기 클래스 기반 추가

--- src/ch1/createStatementData.js
+++ src/ch1/createStatementData.js
@@ -9,6 +9,7 @@ export default function createStatementData(invoice, plays) {
     return result;
 
     function enrichPerformance(aPerformance) {
+        const calculator = new PerformanceCalculator(aPerformance);
         const result = Object.assign({}, aPerformance); // 얕은 복사 수행 (1 depth copy)
 
         result.play = playFor(result);
@@ -68,5 +69,11 @@ export default function createStatementData(invoice, plays) {
     function totalVolumeCredits(data) {
         return data.performances
             .reduce((total, p) => total + p.volumeCredits, 0);
+    }
+}
+
+class PerformanceCalculator {
+    constructor(aPerformance) {
+        this.performance = aPerformance;
     }
 }
