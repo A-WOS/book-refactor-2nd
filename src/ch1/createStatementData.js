ca4a69f4ef1d86840650f6aa49634e5b9097e71d
Bell <bell04204@gmail.com>
2022-12-27 04:24:04 +0900

    refactor: createPerformanceCalculator 팩토리 메서드 추가 (Replace Constructor with Factory Function)

--- src/ch1/createStatementData.js
+++ src/ch1/createStatementData.js
@@ -9,7 +9,7 @@ export default function createStatementData(invoice, plays) {
     return result;
 
     function enrichPerformance(aPerformance) {
-        const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
+        const calculator = createPerformanceCalculator(aPerformance, playFor(aPerformance));
         const result = Object.assign({}, aPerformance); // 얕은 복사 수행 (1 depth copy)
 
         result.play = calculator.play;
@@ -32,6 +32,10 @@ export default function createStatementData(invoice, plays) {
         return data.performances
             .reduce((total, p) => total + p.volumeCredits, 0);
     }
+}
+
+function createPerformanceCalculator(aPerformance, aPlay) {
+    return new PerformanceCalculator(aPerformance, aPlay);
 }
 
 class PerformanceCalculator {
