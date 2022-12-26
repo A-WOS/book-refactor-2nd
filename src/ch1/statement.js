ff24bb9209dc1df690428a729e614a568bb70a8d
Bell <bell04204@gmail.com>
2022-12-27 00:34:07 +0900

    refactor: amountFor method 선언 변경

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -11,7 +11,7 @@ function statement(invoice, plays) {
     ).format;
 
     for (let perf of invoice.performances) {
-        let thisAmount = amountFor(perf, playFor(perf));
+        let thisAmount = amountFor(perf);
 
         // 포인트를 적립한다.
         volumeCredits += Math.max(perf.audience - 30, 0);
@@ -28,35 +28,35 @@ function statement(invoice, plays) {
 
     return result;
 
+    function amountFor(aPerformance) {
+        let result = 0;
+
+        switch (playFor(aPerformance).type) {
+            case "tragedy": // 비극
+                result = 40000;
+                if (aPerformance.audience > 30) {
+                    result += 1000 * (aPerformance.audience - 30);
+                }
+                break;
+
+            case "comedy": // 희극
+                result = 30000;
+                if (aPerformance.audience > 20) {
+                    result += 10000 + 500 * (aPerformance.audience - 20);
+                }
+                result += 300 * aPerformance.audience;
+                break;
+
+            default:
+                throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
+        }
+
+        return result;
+    }
+
     function playFor(perf) {
         return plays[perf.playID];
     }
-}
-
-function amountFor(aPerformance, play) {
-    let result = 0;
-
-    switch (play.type) {
-        case "tragedy": // 비극
-            result = 40000;
-            if (aPerformance.audience > 30) {
-                result += 1000 * (aPerformance.audience - 30);
-            }
-            break;
-
-        case "comedy": // 희극
-            result = 30000;
-            if (aPerformance.audience > 20) {
-                result += 10000 + 500 * (aPerformance.audience - 20);
-            }
-            result += 300 * aPerformance.audience;
-            break;
-
-        default:
-            throw new Error(`알 수 없는 장르: ${play.type}`);
-    }
-
-    return result;
 }
 
 const result = statement(invoicesJson[0], playsJson);
