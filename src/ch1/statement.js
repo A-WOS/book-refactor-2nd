41863cf3770068b14dd7c05f16089887fb924d5a
Bell <bell04204@gmail.com>
2022-12-27 03:57:28 +0900

    feat: html 렌더링 버전 추가

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -18,18 +18,40 @@ function renderPlainText(data, plays) {
     result += `적립 포인트: ${(data.totalVolumeCredits)}점\n`;
 
     return result;
+}
 
-    function usd(aNumber) {
-        return new Intl.NumberFormat(
-            "en-US",
-            {style: "currency", currency: "USD", minimumFractionDigits: 2}
-        ).format(aNumber / 100);
+function htmlStatement(invoice, plays) {
+    return renderHtml(createStatementData(invoice, plays));
+}
+
+function renderHtml(data) {
+    let result = `<h1>청구 내역 (고객명: ${data.customer})</h1>\n`;
+
+    result += "<table>\n";
+    result += "<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>";
+
+    for (let perf of data.performances) {
+        result += `  <tr><td>${perf.play.name}</td><td>(${perf.audience}석)</td>`;
+        result += `<td>${usd(perf.amount)}</td></tr>\n`;
     }
+
+    result += "</table>\n";
+    result += `<p>총액: <em>${usd(data.totalAmount)}</em></p>\n`;
+    result += `<p>적립 포인트: <em>${data.totalVolumeCredits}</em>점</p>\n`;
+
+    return result;
+}
+
+function usd(aNumber) {
+    return new Intl.NumberFormat(
+        "en-US",
+        {style: "currency", currency: "USD", minimumFractionDigits: 2}
+    ).format(aNumber / 100);
 }
 
 // NOTE: 예제 코드 실행을 위한 임시 구문
 const invoicesJson = require('./invoices.json');
 const playsJson = require('./plays.json');
 
-const result = statement(invoicesJson[0], playsJson);
-console.log(result);
+// console.log(statement(invoicesJson[0], playsJson));
+console.log(htmlStatement(invoicesJson[0], playsJson));
