6faef46e7a7e32bae03fef76c421fb50528137f7
Bell <bell04204@gmail.com>
2022-12-27 00:59:41 +0900

    refactor: remove volumeCredits variable (split loop)

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -7,11 +7,12 @@ function statement(invoice, plays) {
     let result = `청구 내역 (고객명: ${invoice.customer})\n`;
 
     for (let perf of invoice.performances) {
-        volumeCredits += volumeCreditsFor(perf);
-
         // 청구 내역을 출력한다.
         result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`
         totalAmount += amountFor(perf);
+    }
+    for (let perf of invoice.performances) {
+        volumeCredits += volumeCreditsFor(perf);
     }
 
     result += `총액: ${usd(totalAmount)}\n`;
