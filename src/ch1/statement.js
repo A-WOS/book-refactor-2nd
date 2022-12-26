ca596c120bd9cefa2107939c8f810cd3841d9f92
Bell <bell04204@gmail.com>
2022-12-27 02:36:20 +0900

    refactor: invoice.performances value shallow copy

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -5,9 +5,14 @@ function statement(invoice, plays) {
     const statementData = {};
 
     statementData.customer = invoice.customer;
-    statementData.performances = invoice.performances;
+    statementData.performances = invoice.performances.map(enrichPerformance);
 
     return renderPlainText(statementData, plays);
+
+    function enrichPerformance(aPerformance) {
+        const result = Object.assign({}, aPerformance); // 얕은 복사 수행 (1 depth copy)
+        return result;
+    }
 }
 
 function renderPlainText(data, plays) {
