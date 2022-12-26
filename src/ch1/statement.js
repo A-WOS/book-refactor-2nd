f3f6dc08cf664942c4218b8344f2b86c1c037011
Bell <bell04204@gmail.com>
2022-12-27 02:16:28 +0900

    refactor: wrapping invoice.customer variable

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -2,14 +2,17 @@ const invoicesJson = require('./invoices.json');
 const playsJson = require('./plays.json');
 
 function statement(invoice, plays) {
-    return renderPlainText(invoice, plays);
+    const statementData = {};
+
+    statementData.customer = invoice.customer;
+
+    return renderPlainText(statementData, invoice, plays);
 }
 
-function renderPlainText(invoice, plays) {
-    let result = `청구 내역 (고객명: ${invoice.customer})\n`;
+function renderPlainText(data, invoice, plays) {
+    let result = `청구 내역 (고객명: ${data.customer})\n`;
 
     for (let perf of invoice.performances) {
-        // 청구 내역을 출력한다.
         result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`
     }
 
