58b7c48f67db45600a97259a61284e93c44445d1
Bell <bell04204@gmail.com>
2022-12-27 02:18:22 +0900

    refactor: wrapping invoice.performances variable

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -5,14 +5,15 @@ function statement(invoice, plays) {
     const statementData = {};
 
     statementData.customer = invoice.customer;
+    statementData.performances = invoice.performances;
 
-    return renderPlainText(statementData, invoice, plays);
+    return renderPlainText(statementData, plays);
 }
 
-function renderPlainText(data, invoice, plays) {
+function renderPlainText(data, plays) {
     let result = `청구 내역 (고객명: ${data.customer})\n`;
 
-    for (let perf of invoice.performances) {
+    for (let perf of data.performances) {
         result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`
     }
 
@@ -24,7 +25,7 @@ function renderPlainText(data, invoice, plays) {
     function totalAmount() {
         let result = 0;
 
-        for (let perf of invoice.performances) {
+        for (let perf of data.performances) {
             result += amountFor(perf);
         }
 
@@ -33,7 +34,7 @@ function renderPlainText(data, invoice, plays) {
 
     function totalVolumeCredits() {
         let volumeCredits = 0;
-        for (let perf of invoice.performances) {
+        for (let perf of data.performances) {
             volumeCredits += volumeCreditsFor(perf);
         }
         return volumeCredits;
