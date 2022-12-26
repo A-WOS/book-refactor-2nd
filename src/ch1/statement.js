dd41d24df1fb7434d3ca73fdcd1c3ae44fb512c7
Bell <bell04204@gmail.com>
2022-12-27 02:47:21 +0900

    refactor: move playFor function

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -11,7 +11,14 @@ function statement(invoice, plays) {
 
     function enrichPerformance(aPerformance) {
         const result = Object.assign({}, aPerformance); // 얕은 복사 수행 (1 depth copy)
+
+        result.play = playFor(result);
+
         return result;
+    }
+
+    function playFor(perf) {
+        return plays[perf.playID];
     }
 }
 
@@ -19,7 +26,7 @@ function renderPlainText(data, plays) {
     let result = `청구 내역 (고객명: ${data.customer})\n`;
 
     for (let perf of data.performances) {
-        result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`
+        result += ` ${perf.play.name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`
     }
 
     result += `총액: ${usd(totalAmount())}\n`;
@@ -57,21 +64,17 @@ function renderPlainText(data, plays) {
 
         volumeCredits += Math.max(perf.audience - 30, 0);
 
-        if ("comedy" === playFor(perf).type) {
+        if ("comedy" === perf.play.type) {
             volumeCredits += Math.floor(perf.audience / 5);
         }
 
         return volumeCredits;
     }
 
-    function playFor(perf) {
-        return plays[perf.playID];
-    }
-
     function amountFor(aPerformance) {
         let result = 0;
 
-        switch (playFor(aPerformance).type) {
+        switch (aPerformance.play.type) {
             case "tragedy": // 비극
                 result = 40000;
                 if (aPerformance.audience > 30) {
@@ -88,7 +91,7 @@ function renderPlainText(data, plays) {
                 break;
 
             default:
-                throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
+                throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
         }
 
         return result;
