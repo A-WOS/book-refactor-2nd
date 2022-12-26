69eeef55dd749717d62c9db715b4cdcb51b42ab2
Bell <bell04204@gmail.com>
2022-12-27 04:28:40 +0900

    refactor: 조건부 로직을 다형성으로 변경을 위한 기반 추가 (Replace Conditional with Polymorphism)

--- src/ch1/createStatementData.js
+++ src/ch1/createStatementData.js
@@ -35,7 +35,16 @@ export default function createStatementData(invoice, plays) {
 }
 
 function createPerformanceCalculator(aPerformance, aPlay) {
-    return new PerformanceCalculator(aPerformance, aPlay);
+    switch (aPlay.type) {
+        case "tragedy":
+            return new TragedyCalculator(aPerformance, aPlay);
+
+        case "comedy":
+            return new ComedyCalculator(aPerformance, aPlay);
+
+        default:
+            throw new Error(`알 수 없는 장르: ${aPlay.type}`);
+    }
 }
 
 class PerformanceCalculator {
@@ -81,4 +90,10 @@ class PerformanceCalculator {
 
         return result;
     }
+}
+
+class TragedyCalculator extends PerformanceCalculator {
+}
+
+class ComedyCalculator extends PerformanceCalculator {
 }
