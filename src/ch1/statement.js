848040f9656f4582f105c6598962a3133b02b794
Bell <bell04204@gmail.com>
2022-12-27 01:38:01 +0900

    refactor: move totalAmount calculate logic (extract method)

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -2,19 +2,29 @@ const invoicesJson = require('./invoices.json');
 const playsJson = require('./plays.json');
 
 function statement(invoice, plays) {
-    let totalAmount = 0;
     let result = `청구 내역 (고객명: ${invoice.customer})\n`;
 
     for (let perf of invoice.performances) {
         // 청구 내역을 출력한다.
         result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`
-        totalAmount += amountFor(perf);
     }
+
+    let totalAmount = appleSauce();
 
     result += `총액: ${usd(totalAmount)}\n`;
     result += `적립 포인트: ${(totalVolumeCredits())}점\n`;
 
     return result;
+
+    function appleSauce() {
+        let totalAmount = 0;
+
+        for (let perf of invoice.performances) {
+            totalAmount += amountFor(perf);
+        }
+
+        return totalAmount;
+    }
 
     function amountFor(aPerformance) {
         let result = 0;
