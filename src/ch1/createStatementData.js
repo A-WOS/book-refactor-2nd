102db10ece84735199b65da360a0825aaa7035c8
Bell <bell04204@gmail.com>
2022-12-27 04:17:40 +0900

    refactor: move amountFor function

--- src/ch1/createStatementData.js
+++ src/ch1/createStatementData.js
@@ -13,7 +13,7 @@ export default function createStatementData(invoice, plays) {
         const result = Object.assign({}, aPerformance); // 얕은 복사 수행 (1 depth copy)
 
         result.play = calculator.play;
-        result.amount = amountFor(result);
+        result.amount = calculator.amount;
         result.volumeCredits = volumeCreditsFor(result);
 
         return result;
@@ -21,32 +21,6 @@ export default function createStatementData(invoice, plays) {
 
     function playFor(perf) {
         return plays[perf.playID];
-    }
-
-    function amountFor(aPerformance) {
-        let result = 0;
-
-        switch (aPerformance.play.type) {
-            case "tragedy": // 비극
-                result = 40000;
-                if (aPerformance.audience > 30) {
-                    result += 1000 * (aPerformance.audience - 30);
-                }
-                break;
-
-            case "comedy": // 희극
-                result = 30000;
-                if (aPerformance.audience > 20) {
-                    result += 10000 + 500 * (aPerformance.audience - 20);
-                }
-                result += 300 * aPerformance.audience;
-                break;
-
-            default:
-                throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
-        }
-
-        return result;
     }
 
     function volumeCreditsFor(aPerformance) {
@@ -76,5 +50,31 @@ class PerformanceCalculator {
     constructor(aPerformance, aPlay) {
         this.performance = aPerformance;
         this.play = aPlay;
+    }
+
+    get amount() {
+        let result = 0;
+
+        switch (this.play.type) {
+            case "tragedy": // 비극
+                result = 40000;
+                if (this.performance.audience > 30) {
+                    result += 1000 * (this.performance.audience - 30);
+                }
+                break;
+
+            case "comedy": // 희극
+                result = 30000;
+                if (this.performance.audience > 20) {
+                    result += 10000 + 500 * (this.performance.audience - 20);
+                }
+                result += 300 * this.performance.audience;
+                break;
+
+            default:
+                throw new Error(`알 수 없는 장르: ${this.play.type}`);
+        }
+
+        return result;
     }
 }
