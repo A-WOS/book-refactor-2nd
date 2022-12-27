1f2c9d3f5512c019551d7885c9e2b7e01f9933cb
Clustern <homerun372@gmail.com>
2022-12-28 07:08:02 +0900

    refactor: appleSauce 함수 이름을 totalAmount 로 변경

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -6,7 +6,7 @@ function statement(invoice, plays) {
     for (let perf of invoice.performances) {
         result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`
     }
-    result += `총액: ${usd(appleSauce())}\n`;
+    result += `총액: ${usd(totalAmount())}\n`;
     result += `적립 포인트: ${(totalVolumeCredits())}점\n`;
     return result;
 
@@ -66,7 +66,7 @@ function statement(invoice, plays) {
         return volumeCredits;
     }
 
-    function appleSauce() {
+    function totalAmount() {
         let totalAmount = 0;
         for (let perf of invoice.performances) {
             totalAmount += amountFor(perf);
