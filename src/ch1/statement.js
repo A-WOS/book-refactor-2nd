a9801395522b971b620af218077f90bac3d06279
Clustern <homerun372@gmail.com>
2022-12-28 06:38:08 +0900

    refactor: format 함수 이름을 usd 로 변경

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -10,11 +10,11 @@ function statement(invoice, plays) {
         volumeCredits += volumeCreditsFor(perf);
 
         // 청구 내역을 출력한다.
-        result += ` ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${perf.audience}석)\n`
+        result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`
         totalAmount += amountFor(perf);
     }
 
-    result += `총액: ${format(totalAmount / 100)}\n`;
+    result += `총액: ${usd(totalAmount)}\n`;
     result += `적립 포인트: ${volumeCredits}점\n`;
 
     return result;
@@ -59,11 +59,11 @@ function statement(invoice, plays) {
         return plays[aPerformance.playID];
     }
 
-    function format(aNumber) {
+    function usd(aNumber) {
         return new Intl.NumberFormat(
             "en-US",
             {style: "currency", currency: "USD",
-                minimumFractionDigits: 2}).format(aNumber);
+                minimumFractionDigits: 2}).format(aNumber / 100);
     }
 }
 
