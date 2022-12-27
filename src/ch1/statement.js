8ca4271c4ff3fbce3c638d4241b1cf2e9e201a50
Clustern <homerun372@gmail.com>
2022-12-28 06:26:23 +0900

    refactor: statement 함수의 volumeCredits 변수 관련 처리 내용을 다른 함수로 추출

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -11,10 +11,7 @@ function statement(invoice, plays) {
     ).format;
 
     for (let perf of invoice.performances) {
-        // 포인트를 적립한다.
-        volumeCredits += Math.max(perf.audience - 30, 0);
-        //희극 관객 5명마다 추가 포인트를 제공한다.
-        if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
+        volumeCredits += volumeCreditsFor(perf);
 
         // 청구 내역을 출력한다.
         result += ` ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${perf.audience}석)\n`
@@ -50,6 +47,16 @@ function statement(invoice, plays) {
         }
 
         return result;
+    }
+
+    function volumeCreditsFor(perf) {
+        let volumeCredits = 0;
+        
+        volumeCredits += Math.max(perf.audience - 30, 0);
+        if ("comedy" === playFor(perf).type)
+            volumeCredits += Math.floor(perf.audience / 5);
+
+        return volumeCredits;
     }
 
     function playFor(aPerformance) {
