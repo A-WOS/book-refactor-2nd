8b9edd1f9206c917b90eab849b56ee25345d1ceb
Clustern <homerun372@gmail.com>
2022-12-28 06:48:38 +0900

    refactor: statement 함수의 volumeCredits 변수 인라인

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -11,10 +11,8 @@ function statement(invoice, plays) {
         totalAmount += amountFor(perf);
     }
 
-    let volumeCredits = totalVolumeCredits();
-
     result += `총액: ${usd(totalAmount)}\n`;
-    result += `적립 포인트: ${volumeCredits}점\n`;
+    result += `적립 포인트: ${(totalVolumeCredits())}점\n`;
 
     return result;
 
