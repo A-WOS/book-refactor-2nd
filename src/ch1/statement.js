5f61b3a4b1feba9337da7eb3924a5b8138549379
Clustern <homerun372@gmail.com>
2022-12-28 06:17:12 +0900

    refactor: amountFor 함수의 play 메소드 제거

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -11,7 +11,7 @@ function statement(invoice, plays) {
     ).format;
 
     for (let perf of invoice.performances) {
-        let thisAmount = amountFor(perf, playFor(perf));
+        let thisAmount = amountFor(perf);
 
         // 포인트를 적립한다.
         volumeCredits += Math.max(perf.audience - 30, 0);
@@ -28,7 +28,7 @@ function statement(invoice, plays) {
 
     return result;
 
-    function amountFor(aPerformance, play) {
+    function amountFor(aPerformance) {
         let result = 0;
 
         switch (playFor(aPerformance).type) {
