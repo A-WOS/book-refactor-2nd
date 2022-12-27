1140e86e4bb2a0c3d814ddcb0e1042ae5829587b
Clustern <homerun372@gmail.com>
2022-12-28 06:15:51 +0900

    refactor: amountFor 함수 선언 변경

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -28,37 +28,36 @@ function statement(invoice, plays) {
 
     return result;
 
+    function amountFor(aPerformance, play) {
+        let result = 0;
+
+        switch (playFor(aPerformance).type) {
+            case "tragedy": // 비극
+                result = 40000;
+                if (aPerformance.audience > 30) {
+                    result += 1000 * (aPerformance.audience - 30);
+                }
+                break;
+
+            case "comedy": // 희극
+                result = 30000;
+                if (aPerformance.audience > 20) {
+                    result += 10000 + 500 * (aPerformance.audience - 20);
+                }
+                result += 300 * aPerformance.audience;
+                break;
+
+            default:
+                throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
+        }
+
+        return result;
+    }
+
     function playFor(aPerformance) {
         return plays[aPerformance.playID];
     }
 }
-
-function amountFor(aPerformance, play) {
-    let result = 0;
-
-    switch (play.type) {
-        case "tragedy": // 비극
-            result = 40000;
-            if (aPerformance.audience > 30) {
-                result += 1000 * (aPerformance.audience - 30);
-            }
-            break;
-
-        case "comedy": // 희극
-            result = 30000;
-            if (aPerformance.audience > 20) {
-                result += 10000 + 500 * (aPerformance.audience - 20);
-            }
-            result += 300 * aPerformance.audience;
-            break;
-
-        default:
-            throw new Error(`알 수 없는 장르: ${play.type}`);
-    }
-
-    return result;
-}
-
 
 
 const result = statement(invoicesJson[0], playsJson);
