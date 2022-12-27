1d9291378d3d387c8351fc0153b3c357a7ef29b1
Clustern <homerun372@gmail.com>
2022-12-28 06:20:10 +0900

    refactor: statement 함수의 thisAmount 변수 인라인

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -11,16 +11,14 @@ function statement(invoice, plays) {
     ).format;
 
     for (let perf of invoice.performances) {
-        let thisAmount = amountFor(perf);
-
         // 포인트를 적립한다.
         volumeCredits += Math.max(perf.audience - 30, 0);
         //희극 관객 5명마다 추가 포인트를 제공한다.
         if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
 
         // 청구 내역을 출력한다.
-        result += ` ${playFor(perf).name}: ${format(thisAmount / 100)} (${perf.audience}석)\n`
-        totalAmount += thisAmount;
+        result += ` ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${perf.audience}석)\n`
+        totalAmount += amountFor(perf);
     }
 
     result += `총액: ${format(totalAmount / 100)}\n`;
@@ -58,7 +56,6 @@ function statement(invoice, plays) {
         return plays[aPerformance.playID];
     }
 }
-
 
 const result = statement(invoicesJson[0], playsJson);
 console.log(result);
