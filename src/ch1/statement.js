30db01e2644b05611e2b07f1233b4b9c43628750
김윤지 <kwg531@naver.com>
2022-12-29 02:45:02 +0900

    refactor: 적립 포인트 계산 이동

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -12,6 +12,16 @@ function statement(invoice, plays) {
         const result = Object.assign({}, aPerformance); // 얕은 복사 수행
         result.play = playFor(result);
         result.amount = amountFor(result);
+        result.volumeCredits = volumeCreditsFor(result);
+        return result;
+    }
+
+    function volumeCreditsFor(aPerformance) {
+        let result = 0;
+
+        result += Math.max(aPerformance.audience - 30, 0);
+        if ("comedy" === aPerformance.play.type)
+            result += Math.floor(aPerformance.audience / 5);
         return result;
     }
 
@@ -67,7 +77,7 @@ function renderPlainText(data, plays) {
     function totalVolumeCredits() {
         let result = 0;
         for (let perf of data.performances) {
-            result += volumeCreditsFor(perf);
+            result += perf.volumeCredits;
         }
         return result;
     }
@@ -78,14 +88,6 @@ function renderPlainText(data, plays) {
         ).format(aNumber / 100);
     }
 
-    function volumeCreditsFor(aPerformance) {
-        let result = 0;
-
-        result += Math.max(aPerformance.audience - 30, 0);
-        if ("comedy" === aPerformance.play.type)
-            result += Math.floor(aPerformance.audience / 5);
-        return result;
-    }
 }
 
 
