90bbf6c4f1d14a5af4848c549027993ed8b68bcb
김윤지 <kwg531@naver.com>
2022-12-28 22:53:36 +0900

    refactor: play 변수 제거하기 1
    playFor 함수 생성

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -12,7 +12,7 @@ function statement(invoice, plays) {
     ).format;
 
     for (let perf of invoice.performances) {
-        const play = plays[perf.playID];
+        const play = playFor(perf);
         let thisAmount = amountFor(perf, play);
 
         // 포인트를 적립한다.
@@ -53,6 +53,10 @@ function statement(invoice, plays) {
                 throw new Error(`알 수 없는 장르: ${play.type}`);
         }
         return result;
+    }
+
+    function playFor(aPerformance) {
+        return plays[aPerformance.playID];
     }
 }
 
