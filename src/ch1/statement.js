73396dfb6ccac51dd3878463608be1f5cd3be0b9
김윤지 <kwg531@naver.com>
2022-12-28 23:44:23 +0900

    refactor: play 변수 제거하기 3
    playFor()를 사용하도록 amountFor()를 수정함

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -32,7 +32,7 @@ function statement(invoice, plays) {
     function amountFor(aPerformance, play) {
         let result = 0;
 
-        switch (play.type) {
+        switch (playFor(aPerformance).type) {
             case "tragedy": // 비극
                 result = 40000;
                 if (aPerformance.audience > 30) {
@@ -49,7 +49,7 @@ function statement(invoice, plays) {
                 break;
 
             default:
-                throw new Error(`알 수 없는 장르: ${play.type}`);
+                throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
         }
         return result;
     }
