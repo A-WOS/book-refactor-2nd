4e4238fb1b08126e8a8bc75000af84f8089ea4cc
김윤지 <kwg531@naver.com>
2022-12-29 01:39:41 +0900

    refactor: 중간 점검
    중첩되는 totalVolumeCredits()의 변수명 result로 변경

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -13,6 +13,41 @@ function statement(invoice, plays) {
     result += `적립 포인트: ${totalVolumeCredits()}점\n`;
     return result;
 
+    function totalAmount() {
+        let result = 0;
+        for (let perf of invoice.performances) {
+            result += amountFor(perf);
+        }
+        return result;
+    }
+
+    function totalVolumeCredits() {
+        let result = 0;
+        for (let perf of invoice.performances) {
+            result += volumeCreditsFor(perf);
+        }
+        return result;
+    }
+
+    function usd(aNumber) {
+        return new Intl.NumberFormat("en-US",
+            {style: "currency", currency: "USD", minimumFractionDigits: 2}
+        ).format(aNumber/100);
+    }
+
+    function volumeCreditsFor(aPerformance) {
+        let result = 0;
+
+        result += Math.max(aPerformance.audience - 30, 0);
+        if ("comedy" === playFor(aPerformance).type)
+            result += Math.floor(aPerformance.audience / 5);
+        return result;
+    }
+
+    function playFor(aPerformance) {
+        return plays[aPerformance.playID];
+    }
+
     function amountFor(aPerformance) {
         let result = 0;
 
@@ -34,41 +69,6 @@ function statement(invoice, plays) {
 
             default:
                 throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
-        }
-        return result;
-    }
-
-    function playFor(aPerformance) {
-        return plays[aPerformance.playID];
-    }
-
-    function volumeCreditsFor(aPerformance) {
-        let volumeCredits = 0;
-
-        volumeCredits += Math.max(aPerformance.audience - 30, 0);
-        if ("comedy" === playFor(aPerformance).type)
-            volumeCredits += Math.floor(aPerformance.audience / 5);
-        return volumeCredits;
-    }
-
-    function usd(aNumber) {
-        return new Intl.NumberFormat("en-US",
-            {style: "currency", currency: "USD", minimumFractionDigits: 2}
-        ).format(aNumber/100);
-    }
-
-    function totalVolumeCredits() {
-        let result = 0;
-        for (let perf of invoice.performances) {
-            result += volumeCreditsFor(perf);
-        }
-        return result;
-    }
-
-    function totalAmount() {
-        let result = 0;
-        for (let perf of invoice.performances) {
-            result += amountFor(perf);
         }
         return result;
     }
