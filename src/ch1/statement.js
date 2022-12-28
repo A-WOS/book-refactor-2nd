a3369aea559d64ce47eb2388a7929f50538d287c
김윤지 <kwg531@naver.com>
2022-12-28 22:50:55 +0900

    refactor: 변수, 매개변수 이름 변경
    - thisAmount -> result
    - perf -> aPerformance

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -30,29 +30,29 @@ function statement(invoice, plays) {
     result += `적립 포인트: ${volumeCredits}점\n`;
     return result;
 
-    function amountFor(perf, play) {
-        let thisAmount = 0;
+    function amountFor(aPerformance, play) {
+        let result = 0;
 
         switch (play.type) {
             case "tragedy": // 비극
-                thisAmount = 40000;
-                if (perf.audience > 30) {
-                    thisAmount += 1000 * (perf.audience - 30);
+                result = 40000;
+                if (aPerformance.audience > 30) {
+                    result += 1000 * (aPerformance.audience - 30);
                 }
                 break;
 
             case "comedy": // 희극
-                thisAmount = 30000;
-                if (perf.audience > 20) {
-                    thisAmount += 10000 + 500 * (perf.audience - 20);
+                result = 30000;
+                if (aPerformance.audience > 20) {
+                    result += 10000 + 500 * (aPerformance.audience - 20);
                 }
-                thisAmount += 300 * perf.audience;
+                result += 300 * aPerformance.audience;
                 break;
 
             default:
                 throw new Error(`알 수 없는 장르: ${play.type}`);
         }
-        return thisAmount;
+        return result;
     }
 }
 
