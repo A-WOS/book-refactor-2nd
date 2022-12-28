c4c9c28dba8ae10d5ebb23026f82dd46e384e198
김윤지 <kwg531@naver.com>
2022-12-29 01:49:37 +0900

    refactor: statement() 함수 추출하기
    renderPlainText()로 이름 변경

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -3,12 +3,16 @@ import {createRequire} from "module";
 const require = createRequire(import.meta.url);
 
 function statement(invoice, plays) {
+    return renderPlainText(invoice, plays);
+}
+
+function renderPlainText(invoice, plays) {
     let result = `청구 내역 (고객명: ${invoice.customer})\n`;
     for (let perf of invoice.performances) {
         // 청구 내역을 출력하기
         result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`
-
     }
+
     result += `총액: ${usd(totalAmount())}\n`;
     result += `적립 포인트: ${totalVolumeCredits()}점\n`;
     return result;
@@ -32,7 +36,7 @@ function statement(invoice, plays) {
     function usd(aNumber) {
         return new Intl.NumberFormat("en-US",
             {style: "currency", currency: "USD", minimumFractionDigits: 2}
-        ).format(aNumber/100);
+        ).format(aNumber / 100);
     }
 
     function volumeCreditsFor(aPerformance) {
