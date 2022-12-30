45fca9ffd87466f54745b57c48df8ad8fbde7032
김윤지 <kwg531@naver.com>
2022-12-31 07:28:08 +0900

    refactor: html 코드 작성

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -7,6 +7,31 @@ function statement(invoice, plays) {
     return renderPlainText(createStatementData(invoice, plays));
 }
 
+function htmlStatement(invoice, plays) {
+    return renderHtml(createStatementData(invoice, plays));
+}
+
+function renderHtml(data) {
+    let result = `<h1>청구 내역 (고객명: ${data.customer})</h1>\n`;
+    result += "<table>\n";
+    result += "<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>";
+
+    for (let perf of data.performances) {
+        result += ` <tr><td>${perf.play.name}</td><td>(${perf.audience}석)</td>`;
+        result += `<td>${usd(perf.amount)}</td></tr>\n`
+    }
+    result += "</table>\n"
+    result += `<p>총액: <em>${usd(data.totalAmount)}</em></p>\n`;
+    result += `<p>적립 포인트: <em>${data.totalVolumeCredits}</em>점</p>\n`;
+    return result;
+}
+
+function usd(aNumber) {
+    return new Intl.NumberFormat("en-US",
+        {style: "currency", currency: "USD", minimumFractionDigits: 2}
+    ).format(aNumber / 100);
+}
+
 function renderPlainText(data, plays) {
     let result = `청구 내역 (고객명: ${data.customer})\n`;
 
@@ -16,12 +41,6 @@ function renderPlainText(data, plays) {
     result += `총액: ${usd(data.totalAmount)}\n`;
     result += `적립 포인트: ${data.totalVolumeCredits}점`;
     return result;
-
-    function usd(aNumber) {
-        return new Intl.NumberFormat("en-US",
-            {style: "currency", currency: "USD", minimumFractionDigits: 2}
-        ).format(aNumber / 100);
-    }
 }
 
 // NOTE: 예제 코드 실행을 위한 임시 구문
