4e499293f406d67e5be61ba81fe4e3f6e90c38bc
김윤지 <kwg531@naver.com>
2022-12-31 07:10:46 +0900

    refactor: createStatementData 파일 생성 후 이동
    statement(), createStatementData() 함수 별도 파일에 저장

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -1,75 +1,10 @@
 import {createRequire} from "module";
+import createStatementData from './createStatementData.js';
 
 const require = createRequire(import.meta.url);
 
-
 function statement(invoice, plays) {
     return renderPlainText(createStatementData(invoice, plays));
-}
-
-function createStatementData(invoice, plays) {
-    const statementData = {};
-    statementData.customer = invoice.customer;
-    statementData.performances = invoice.performances.map(enrichPerformance);
-    statementData.totalAmount = totalAmount(statementData);
-    statementData.totalVolumeCredits = totalVolumeCredits(statementData);
-    return statementData;
-
-    function enrichPerformance(aPerformance) {
-        const result = Object.assign({}, aPerformance); // 얕은 복사 수행
-        result.play = playFor(result);
-        result.amount = amountFor(result);
-        result.volumeCredits = volumeCreditsFor(result);
-        return result;
-    }
-
-    function totalAmount(data) {
-        return data.performances
-            .reduce((total, p) => total + p.amount, 0);
-    }
-
-    function totalVolumeCredits(data) {
-        return data.performances
-            .reduce((total, p) => total + p.volumeCredits, 0);
-    }
-
-    function amountFor(aPerformance) {
-        let result = 0;
-
-        switch (aPerformance.play.type) {
-            case "tragedy": // 비극
-                result = 40000;
-                if (aPerformance.audience > 30) {
-                    result += 1000 * (aPerformance.audience - 30);
-                }
-                break;
-
-            case "comedy": // 희극
-                result = 30000;
-                if (aPerformance.audience > 20) {
-                    result += 10000 + 500 * (aPerformance.audience - 20);
-                }
-                result += 300 * aPerformance.audience;
-                break;
-
-            default:
-                throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
-        }
-        return result;
-    }
-
-    function volumeCreditsFor(aPerformance) {
-        let result = 0;
-
-        result += Math.max(aPerformance.audience - 30, 0);
-        if ("comedy" === aPerformance.play.type)
-            result += Math.floor(aPerformance.audience / 5);
-        return result;
-    }
-
-    function playFor(aPerformance) {
-        return plays[aPerformance.playID];
-    }
 }
 
 function renderPlainText(data, plays) {
@@ -79,7 +14,7 @@ function renderPlainText(data, plays) {
         result += ` ${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`
     }
     result += `총액: ${usd(data.totalAmount)}\n`;
-    result += `적립 포인트: ${data.totalVolumeCredits}점\n`;
+    result += `적립 포인트: ${data.totalVolumeCredits}점`;
     return result;
 
     function usd(aNumber) {
