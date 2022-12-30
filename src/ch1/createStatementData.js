696544467f5218cafc1aa0637310330ba46d975b
김윤지 <kwg531@naver.com>
2022-12-31 08:30:40 +0900

    fix: 함수 선언 바꾸기
    계산기 클래스의 생성자에 함수 선언 바꾸기 적용하여 공연할 연극을 계산기로 적용

--- src/ch1/createStatementData.js
+++ src/ch1/createStatementData.js
@@ -1,3 +1,10 @@
+
+class PerformanceCalculator {
+    constructor(aPerformance) {
+        this.performance = aPerformance;
+    }
+}
+
 export default function createStatementData(invoice, plays) {
     const statementData = {};
     statementData.customer = invoice.customer;
@@ -7,6 +14,7 @@ export default function createStatementData(invoice, plays) {
     return statementData;
 
     function enrichPerformance(aPerformance) {
+        const calculator = new PerformanceCalculator(aPerformance);
         const result = Object.assign({}, aPerformance); // 얕은 복사 수행
         result.play = playFor(result);
         result.amount = amountFor(result);
