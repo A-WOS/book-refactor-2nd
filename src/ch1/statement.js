2bf6d254de0ef5e687ed07a6720ae38b9deed294
김윤지 <kwg531@naver.com>
2022-12-31 06:30:03 +0900

    refactor: playFor() 호출 부분 중간 데이터 사용

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -2,10 +2,13 @@ import {createRequire} from "module";
 
 const require = createRequire(import.meta.url);
 
+
 function statement(invoice, plays) {
     const statementData = {};
     statementData.customer = invoice.customer;
     statementData.performances = invoice.performances.map(enrichPerformance);
+    statementData.totalAmount = totalAmount(statementData);
+    statementData.totalVolumeCredits = totalVolumeCredits(statementData);
     return renderPlainText(statementData, plays);
 
     function enrichPerformance(aPerformance) {
@@ -13,6 +16,21 @@ function statement(invoice, plays) {
         result.play = playFor(result);
         result.amount = amountFor(result);
         result.volumeCredits = volumeCreditsFor(result);
+        return result;
+    }
+    function totalAmount(data) {
+        let result = 0;
+        for (let perf of data.performances) {
+            result += amountFor(perf);
+        }
+        return result;
+    }
+
+    function totalVolumeCredits(data) {
+        let result = 0;
+        for (let perf of data.performances) {
+            result += perf.volumeCredits;
+        }
         return result;
     }
 
@@ -54,33 +72,15 @@ function statement(invoice, plays) {
         return result;
     }
 }
-
 function renderPlainText(data, plays) {
     let result = `청구 내역 (고객명: ${data.customer})\n`;
+
     for (let perf of data.performances) {
         result += ` ${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`
     }
-
-    result += `총액: ${usd(totalAmount())}\n`;
-    result += `적립 포인트: ${totalVolumeCredits()}점\n`;
-
+    result += `총액: ${usd(data.totalAmount)}\n`;
+    result += `적립 포인트: ${data.totalVolumeCredits}점\n`;
     return result;
-
-    function totalAmount() {
-        let result = 0;
-        for (let perf of data.performances) {
-            result += perf.amount;
-        }
-        return result;
-    }
-
-    function totalVolumeCredits() {
-        let result = 0;
-        for (let perf of data.performances) {
-            result += perf.volumeCredits;
-        }
-        return result;
-    }
 
     function usd(aNumber) {
         return new Intl.NumberFormat("en-US",
