9370018ae7982c89e57a1cf9bcffe566826331a8
Clustern <homerun372@gmail.com>
2022-12-31 15:35:09 +0900

    feat: 연극 제목을 statementData.performances 변수에 추가하기 위한 enrichPerformance 함수 작성

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -6,8 +6,13 @@ const require = createRequire(import.meta.url);
 function statement(invoice, plays) {
     const statementData = {};
     statementData.customer = invoice.customer;
-    statementData.performances = invoice.performances;
+    statementData.performances = invoice.performances.map(enrichPerformance);
     return renderPlainText(statementData, plays);
+
+    function enrichPerformance(aPerformance) {
+        const result = Object.assign({}, aPerformance);
+        return result;
+    }
 }
 
 function renderPlainText(data, plays) {
