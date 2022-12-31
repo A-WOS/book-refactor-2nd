da44da7b99af169660b87a96f4ab4cb72ca6f1fd
Clustern <homerun372@gmail.com>
2022-12-31 16:29:55 +0900

    refactor: rederPlainText 함수의 중첩 함수 amountFor 를 statement 함수로 이동 및 결과값을 statementData 에 추가

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -12,23 +12,13 @@ function statement(invoice, plays) {
     function enrichPerformance(aPerformance) {
         const result = Object.assign({}, aPerformance);
         result.play = playFor(result);
+        result.amount = amountFor(result)
         return result;
     }
 
     function playFor(aPerformance) {
         return plays[aPerformance.playID];
     }
-}
-
-function renderPlainText(data, plays) {
-    let result = `청구 내역 (고객명: ${data.customer})\n`;
-    for (let perf of data.performances) {
-        result += ` ${perf.play.name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
-    }
-    result += `총액: ${usd(totalAmount())}\n`;
-    result += `적립 포인트: ${(totalVolumeCredits())}점\n`;
-
-    return result;
 
     function amountFor(aPerformance) {
         let result = 0;
@@ -55,6 +45,17 @@ function renderPlainText(data, plays) {
 
         return result;
     }
+}
+
+function renderPlainText(data, plays) {
+    let result = `청구 내역 (고객명: ${data.customer})\n`;
+    for (let perf of data.performances) {
+        result += ` ${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`;
+    }
+    result += `총액: ${usd(totalAmount())}\n`;
+    result += `적립 포인트: ${(totalVolumeCredits())}점\n`;
+
+    return result;
 
     function volumeCreditsFor(aPerformance) {
         let volumeCredits = 0;
@@ -87,7 +88,7 @@ function renderPlainText(data, plays) {
     function totalAmount() {
         let result = 0;
         for (let perf of data.performances) {
-            result += amountFor(perf);
+            result += perf.amount;
         }
 
         return result;
