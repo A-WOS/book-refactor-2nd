78f2e6bc736b83cf96be386f091cc30a8c8246a5
김윤지 <kwg531@naver.com>
2022-12-31 10:57:33 +0900

    refactor: 포인트 계산 함수 인라인
    get volumeCredits()

--- src/ch1/createStatementData.js
+++ src/ch1/createStatementData.js
@@ -28,6 +28,15 @@ class PerformanceCalculator {
         }
         return result;
     }
+
+    get volumeCredits() {
+        let result = 0;
+
+        result += Math.max(this.performance.audience - 30, 0);
+        if ("comedy" === this.play.type)
+            result += Math.floor(this.performance.audience / 5);
+        return result;
+    }
 }
 
 export default function createStatementData(invoice, plays) {
@@ -47,7 +56,7 @@ export default function createStatementData(invoice, plays) {
         const result = Object.assign({}, aPerformance); // 얕은 복사 수행
         result.play = calculator.play;
         result.amount = calculator.amount;
-        result.volumeCredits = volumeCreditsFor(result);
+        result.volumeCredits = calculator.volumeCredits;
         return result;
     }
 
