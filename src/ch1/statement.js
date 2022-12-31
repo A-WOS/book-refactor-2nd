baffd281aa37dd3bb2bb011ba3a9845c47e03b33
Clustern <homerun372@gmail.com>
2022-12-31 15:04:03 +0900

    feat: 출력 결과 검사를 위한 테스트 코드 추가

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -1,4 +1,5 @@
 import {createRequire} from "module";
+import {expect} from "chai";
 
 const require = createRequire(import.meta.url);
 
@@ -89,4 +90,16 @@ function renderPlainText(data, invoice, plays) {
 const invoicesJson = require('./invoices.json');
 const playsJson = require('./plays.json');
 
-console.log(statement(invoicesJson[0], playsJson))
+let result = statement(invoicesJson[0], playsJson)
+console.log(result)
+
+it('출력 결과 검사', () => {
+    expect(
+        '청구 내역 (고객명: BigCo)\n' +
+        ' Hamlet: $650.00 (55석)\n' +
+        ' As You Like It: $580.00 (35석)\n' +
+        ' Othello: $500.00 (40석)\n' +
+        '총액: $1,730.00\n' +
+        '적립 포인트: 47점\n'
+    ).to.equal(result);
+});
