e93294054fc497eeff2645f1020b68ade00f9b9f
Clustern <homerun372@gmail.com>
2022-12-31 16:30:25 +0900

    refactor: 사용하지 않는 renderPlainText 함수의 plays 인수 제거

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -47,7 +47,7 @@ function statement(invoice, plays) {
     }
 }
 
-function renderPlainText(data, plays) {
+function renderPlainText(data) {
     let result = `청구 내역 (고객명: ${data.customer})\n`;
     for (let perf of data.performances) {
         result += ` ${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`;
