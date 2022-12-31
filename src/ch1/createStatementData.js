107b8128fed4b34ac891df8f6b0a562bab925194
김윤지 <kwg531@naver.com>
2022-12-31 11:30:21 +0900

    refactor: 조건부 로직을 다형성으로 바꾸기1
    TragedyCalculator() 공연료 계산

--- src/ch1/createStatementData.js
+++ src/ch1/createStatementData.js
@@ -1,6 +1,10 @@
-
 function createPerformanceCalculator(aPerformance, aPlay) {
-    return new PerformanceCalculator(aPerformance, aPlay);
+    switch (aPlay.type) {
+        case "tragedy": return new TragedyCalculator(aPerformance, aPlay);
+        case "comedy": return new ComedyCalculator(aPerformance, aPlay);
+        default:
+            throw new Error(`알 수 없는 장르: ${aPlay.type}`);
+    }
 }
 
 class PerformanceCalculator {
@@ -41,6 +45,19 @@ class PerformanceCalculator {
             result += Math.floor(this.performance.audience / 5);
         return result;
     }
+}
+
+class TragedyCalculator extends PerformanceCalculator {
+    get amount() {
+        let result = 40000;
+        if (this.performance.audience > 30) {
+            result += 10000 * (this.performance.audience - 30);
+        }
+        return result;
+    }
+}
+
+class ComedyCalculator extends PerformanceCalculator {
 }
 
 export default function createStatementData(invoice, plays) {
