7f975620e5bf25b64f94d06c2e0a6f3a5bdc47f3
Clustern <homerun372@gmail.com>
2022-12-31 15:11:31 +0900

    refactor: invoice.performances 데이터를 statementData.performances 에서 가져오도록 수정

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -6,12 +6,13 @@ const require = createRequire(import.meta.url);
 function statement(invoice, plays) {
     const statementData = {};
     statementData.customer = invoice.customer;
+    statementData.performances = invoice.performances;
     return renderPlainText(statementData, invoice, plays);
 }
 
 function renderPlainText(data, invoice, plays) {
     let result = `청구 내역 (고객명: ${data.customer})\n`;
-    for (let perf of invoice.performances) {
+    for (let perf of data.performances) {
         result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
     }
     result += `총액: ${usd(totalAmount())}\n`;
@@ -70,7 +71,7 @@ function renderPlainText(data, invoice, plays) {
 
     function totalVolumeCredits() {
         let result = 0;
-        for (let perf of invoice.performances) {
+        for (let perf of data.performances) {
             result += volumeCreditsFor(perf);
         }
 
@@ -79,7 +80,7 @@ function renderPlainText(data, invoice, plays) {
 
     function totalAmount() {
         let result = 0;
-        for (let perf of invoice.performances) {
+        for (let perf of data.performances) {
             result += amountFor(perf);
         }
 
