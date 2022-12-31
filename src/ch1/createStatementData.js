62b10dc2a6c1fa911399c6a92b30085830f053c6
Clustern <homerun372@gmail.com>
2022-12-31 18:40:06 +0900

    feat: PerformanceCalculator 클래스에 amountFor 함수의 amount 게터로 코드를 복제

--- src/ch1/createStatementData.js
+++ src/ch1/createStatementData.js
@@ -4,6 +4,31 @@ class PerformanceCalculator {
         this.play = aplay;
     }
 
+    get amount() {
+        let result = 0;
+
+        switch (this.play.type) {
+            case "tragedy": // 비극
+                result = 40000;
+                if (this.performance.audience > 30) {
+                    result += 1000 * (this.performance.audience - 30);
+                }
+                break;
+
+            case "comedy": // 희극
+                result = 30000;
+                if (this.performance.audience > 20) {
+                    result += 10000 + 500 * (this.performance.audience - 20);
+                }
+                result += 300 * this.performance.audience;
+                break;
+
+            default:
+                throw new Error(`알 수 없는 장르: ${this.play.type}`);
+        }
+
+        return result;
+    }
 }
 
 export default function createStatementData(invoice, plays) {
