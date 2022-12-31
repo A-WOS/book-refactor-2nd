44304a83cf710913f0e2975abf2034aa0518e452
Clustern <homerun372@gmail.com>
2022-12-31 18:55:07 +0900

    refactor: volumeCredits 함수를 PerformanceCalculator 클래스의 volumeCredits 게터로 이동

--- src/ch1/createStatementData.js
+++ src/ch1/createStatementData.js
@@ -29,6 +29,16 @@ class PerformanceCalculator {
 
         return result;
     }
+
+    get volumeCredits() {
+        let volumeCredits = 0;
+
+        volumeCredits += Math.max(this.performance.audience - 30, 0);
+        if ("comedy" === this.play.type)
+            volumeCredits += Math.floor(this.performance.audience / 5);
+
+        return volumeCredits;
+    }
 }
 
 export default function createStatementData(invoice, plays) {
@@ -44,22 +54,12 @@ export default function createStatementData(invoice, plays) {
         const result = Object.assign({}, aPerformance);
         result.play = playFor(result);
         result.amount = calculator.amount;
-        result.volumeCredits = volumeCreditsFor(result);
+        result.volumeCredits = calculator.volumeCredits;
         return result;
     }
 
     function playFor(aPerformance) {
         return plays[aPerformance.playID];
-    }
-
-    function volumeCreditsFor(aPerformance) {
-        let volumeCredits = 0;
-
-        volumeCredits += Math.max(aPerformance.audience - 30, 0);
-        if ("comedy" === aPerformance.play.type)
-            volumeCredits += Math.floor(aPerformance.audience / 5);
-
-        return volumeCredits;
     }
 
     function totalAmount(data) {
