e34de50c055f530f837a310f471a9ab6f6772eca
Clustern <homerun372@gmail.com>
2022-12-31 19:34:09 +0900

    misc: 책의 1.9 상태점검에 따라 클래스 위치 재정렬

--- src/ch1/createStatementData.js
+++ src/ch1/createStatementData.js
@@ -1,3 +1,44 @@
+export default function createStatementData(invoice, plays) {
+    const result = {};
+    result.customer = invoice.customer;
+    result.performances = invoice.performances.map(enrichPerformance);
+    result.totalAmount = totalAmount(result);
+    result.totalVolumeCredits = totalVolumeCredits(result);
+    return result;
+
+    function enrichPerformance(aPerformance) {
+        const calculator = createPerformanceCalculator(aPerformance, playFor(aPerformance));
+        const result = Object.assign({}, aPerformance);
+        result.play = calculator.play;
+        result.amount = calculator.amount;
+        result.volumeCredits = calculator.volumeCredits;
+        return result;
+    }
+
+    function playFor(aPerformance) {
+        return plays[aPerformance.playID];
+    }
+
+    function totalAmount(data) {
+        return data.performances
+            .reduce((total, p) => total + p.amount, 0);
+    }
+
+    function totalVolumeCredits(data) {
+        return data.performances
+            .reduce((total, p) => total + p.volumeCredits, 0);
+    }
+}
+
+function createPerformanceCalculator(aPerformance, aPlay) {
+    switch (aPlay.type) {
+        case "tragedy": return new TragedyCalculator(aPerformance, aPlay);
+        case "comedy": return new ComedyCalculator(aPerformance, aPlay);
+        default:
+            throw new Error(`알 수 없는 장르: ${this.play.type}`);
+    }
+}
+
 class PerformanceCalculator {
     constructor(aPerformance, aplay) {
         this.performance = aPerformance;
@@ -5,7 +46,7 @@ class PerformanceCalculator {
     }
 
     get amount() {
-            throw new Error(`서브클래스에서 처리하도록 설계되었습니다.`);
+        throw new Error(`서브클래스에서 처리하도록 설계되었습니다.`);
     }
 
     get volumeCredits() {
@@ -40,46 +81,5 @@ class ComedyCalculator extends PerformanceCalculator {
 
     get volumeCredits() {
         return super.volumeCredits + Math.floor(this.performance.audience / 5);
-    }
-}
-
-function createPerformanceCalculator(aPerformance, aPlay) {
-    switch (aPlay.type) {
-        case "tragedy": return new TragedyCalculator(aPerformance, aPlay);
-        case "comedy": return new ComedyCalculator(aPerformance, aPlay);
-        default:
-            throw new Error(`알 수 없는 장르: ${this.play.type}`);
-    }
-}
-
-export default function createStatementData(invoice, plays) {
-    const result = {};
-    result.customer = invoice.customer;
-    result.performances = invoice.performances.map(enrichPerformance);
-    result.totalAmount = totalAmount(result);
-    result.totalVolumeCredits = totalVolumeCredits(result);
-    return result;
-
-    function enrichPerformance(aPerformance) {
-        const calculator = createPerformanceCalculator(aPerformance, playFor(aPerformance));
-        const result = Object.assign({}, aPerformance);
-        result.play = calculator.play;
-        result.amount = calculator.amount;
-        result.volumeCredits = calculator.volumeCredits;
-        return result;
-    }
-
-    function playFor(aPerformance) {
-        return plays[aPerformance.playID];
-    }
-
-    function totalAmount(data) {
-        return data.performances
-            .reduce((total, p) => total + p.amount, 0);
-    }
-
-    function totalVolumeCredits(data) {
-        return data.performances
-            .reduce((total, p) => total + p.volumeCredits, 0);
     }
 }
