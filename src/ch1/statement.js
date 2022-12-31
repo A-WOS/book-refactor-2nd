4277eafccfcf29a12c021bfec2826934ebfbceed
Clustern <homerun372@gmail.com>
2022-12-31 15:26:16 +0900

    refactor: 필요 없어진 renderPlainText 함수의 invoice 인수 제거

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -7,10 +7,10 @@ function statement(invoice, plays) {
     const statementData = {};
     statementData.customer = invoice.customer;
     statementData.performances = invoice.performances;
-    return renderPlainText(statementData, invoice, plays);
+    return renderPlainText(statementData, plays);
 }
 
-function renderPlainText(data, invoice, plays) {
+function renderPlainText(data, plays) {
     let result = `청구 내역 (고객명: ${data.customer})\n`;
     for (let perf of data.performances) {
         result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
