16e7aba78583f235ae2ab48dd726e380efe55602
Tetrahedrite <det3652000@naver.com>
2023-01-02 23:07:39 +0900

    refactor: extract method to get total volume credits (totalVolumeCredits)

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -11,14 +11,21 @@ export function statement(invoice, plays) {
         result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`
         totalAmount += amountFor(perf);
     }
-    let volumeCredits = 0;
-    for (let perf of invoice.performances) {
-        volumeCredits += volumeCreditsFor(perf);
-    }
+
+    let volumeCredits = totalVolumeCredits();
 
     result += `총액: ${usd(totalAmount)}\n`;
+
     result += `적립 포인트: ${volumeCredits}점\n`;
     return result;
+
+    function totalVolumeCredits() {
+        let volumeCredits = 0;
+        for (let perf of invoice.performances) {
+            volumeCredits += volumeCreditsFor(perf);
+        }
+        return volumeCredits;
+    }
 
     function usd(aNumber) {
         return new Intl.NumberFormat(
