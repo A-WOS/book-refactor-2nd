c9c9428ba7bf9e8d8bcbb5c5351437707f43f925
Tetrahedrite <det3652000@naver.com>
2023-01-03 00:03:33 +0900

    refactor: extract method for create statement data (createStatementData)

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -3,12 +3,16 @@ import {createRequire} from "module";
 const require = createRequire(import.meta.url);
 
 export function statement(invoice, plays) {
-    const statementData = {};
-    statementData.customer = invoice.customer;
-    statementData.performances = invoice.performances.map(enrichPerformance);
-    statementData.totalAmount = totalAmount(statementData);
-    statementData.totalVolumeCredits = totalVolumeCredits(statementData);
-    return renderPlainText(statementData, plays);
+    return renderPlainText(createStatementData(invoice, plays));
+
+    function createStatementData(invoice, plays) {
+        const statementData = {};
+        statementData.customer = invoice.customer;
+        statementData.performances = invoice.performances.map(enrichPerformance);
+        statementData.totalAmount = totalAmount(statementData);
+        statementData.totalVolumeCredits = totalVolumeCredits(statementData);
+        return statementData;
+    }
 
     function enrichPerformance(aPerformance) {
         const result = Object.assign({}, aPerformance);
