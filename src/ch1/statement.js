571bdaeac99bcbd09fb7053f36b4da9a8c8df215
Tetrahedrite <det3652000@naver.com>
2023-01-03 00:25:30 +0900

    refactor: change position of function in statement.js

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -7,6 +7,18 @@ export function statement(invoice, plays) {
     return renderPlainText(createStatementData(invoice, plays));
 }
 
+function renderPlainText(data, plays) {
+    let result = `청구 내역 (고객명: ${data.customer})\n`;
+    for (let perf of data.performances) {
+        result += ` ${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`;
+    }
+    result += `총액: ${usd(data.totalAmount)}\n`;
+    result += `적립 포인트: ${(data.totalVolumeCredits)}점\n`;
+
+    return result;
+
+}
+
 export function htmlStatement(invoice, plays) {
     return renderHtml(createStatementData(invoice, plays));
 }
@@ -23,18 +35,6 @@ function renderHtml(data) {
     result += `<p>총액: <em>${usd(data.totalAmount)}</em></p>\n`;
     result += `<p>적립 포인트: <em>${data.totalVolumeCredits}</em>점</p>\n`;
     return result;
-}
-
-function renderPlainText(data, plays) {
-    let result = `청구 내역 (고객명: ${data.customer})\n`;
-    for (let perf of data.performances) {
-        result += ` ${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`;
-    }
-    result += `총액: ${usd(data.totalAmount)}\n`;
-    result += `적립 포인트: ${(data.totalVolumeCredits)}점\n`;
-
-    return result;
-
 }
 
 function usd(aNumber) {
