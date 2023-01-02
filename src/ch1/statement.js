2b5c7f1c83421a2d94e9e2a59d69f2a3b244c7e4
Tetrahedrite <det3652000@naver.com>
2023-01-02 21:59:20 +0900

    refactor: extract method (amountFor)

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -13,8 +13,23 @@ function statement(invoice, plays) {
 
     for (let perf of invoice.performances) {
         const play = plays[perf.playID];
-        let thisAmount = 0;
+        let thisAmount = amountFor(play, perf);
 
+        // 포인트를 적립한다.
+        volumeCredits += Math.max(perf.audience - 30, 0);
+        // 희극 관객 5명마다 추가 포인트를 제공한다.
+        if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+
+        // 청구 내역을 출력한다.
+        result += ` ${play.name}: ${format(thisAmount / 100)} (${perf.audience}석)\n`
+        totalAmount += thisAmount;
+    }
+    result += `총액: ${format(totalAmount / 100)}\n`;
+    result += `적립 포인트: ${volumeCredits}점\n`;
+    return result;
+
+    function amountFor(play, perf) {
+        let thisAmount = 0;
         switch (play.type) {
             case "tragedy": // 비극
                 thisAmount = 40000;
@@ -34,22 +49,11 @@ function statement(invoice, plays) {
             default:
                 throw new Error(`알 수 없는 장르: ${play.type}`);
         }
-
-        // 포인트를 적립한다.
-        volumeCredits += Math.max(perf.audience - 30, 0);
-        //희극 관객 5명마다 추가 포인트를 제공한다.
-        if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
-
-        // 청구 내역을 출력한다.
-        result += ` ${play.name}: ${format(thisAmount / 100)} (${perf.audience}석)\n`
-        totalAmount += thisAmount;
+        return thisAmount;
     }
-
-    result += `총액: ${format(totalAmount / 100)}\n`;
-    result += `적립 포인트: ${volumeCredits}점\n`;
-
-    return result;
 }
+
+
 
 // NOTE: 예제 코드 실행을 위한 임시 구문
 const invoicesJson = require('./invoices.json');
