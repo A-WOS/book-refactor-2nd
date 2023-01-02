e0b40eff0ea25e41cad19d9269f0d6b578485bec
Tetrahedrite <det3652000@naver.com>
2023-01-02 23:15:10 +0900

    refactor: extract method to get total amount (totalAmount), rename variables

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -3,25 +3,29 @@ import {createRequire} from "module";
 const require = createRequire(import.meta.url);
 
 export function statement(invoice, plays) {
-    let totalAmount = 0;
     let result = `청구 내역 (고객명: ${invoice.customer})\n`;
+
     for (let perf of invoice.performances) {
-
-        // 청구 내역을 출력한다.
         result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`
-        totalAmount += amountFor(perf);
     }
-
-    result += `총액: ${usd(totalAmount)}\n`;
+    result += `총액: ${usd(totalAmount())}\n`;
     result += `적립 포인트: ${(totalVolumeCredits())}점\n`;
     return result;
 
-    function totalVolumeCredits() {
-        let volumeCredits = 0;
+    function totalAmount() {
+        let result = 0;
         for (let perf of invoice.performances) {
-            volumeCredits += volumeCreditsFor(perf);
+            result += amountFor(perf);
         }
-        return volumeCredits;
+        return result;
+    }
+
+    function totalVolumeCredits() {
+        let result = 0;
+        for (let perf of invoice.performances) {
+            result += volumeCreditsFor(perf);
+        }
+        return result;
     }
 
     function usd(aNumber) {
