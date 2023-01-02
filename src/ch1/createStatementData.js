ace9b60c78b7fa109beb4977757395a47138be5c
Tetrahedrite <det3652000@naver.com>
2023-01-03 00:48:45 +0900

    refactor: move get volumeCredits to each subclass

--- src/ch1/createStatementData.js
+++ src/ch1/createStatementData.js
@@ -8,11 +8,7 @@ class PerformanceCalculator {
     }
 
     get volumeCredits() {
-        let volumeCredits = 0;
-        volumeCredits += Math.max(this.performance.audience - 30, 0);
-        if ("comedy" === this.play.type)
-            volumeCredits += Math.floor(this.performance.audience / 5);
-        return volumeCredits;
+        return Math.max(this.performance.audience - 30, 0);
     }
 }
 
@@ -34,6 +30,10 @@ class ComedyCalculator extends PerformanceCalculator {
         }
         result += 300 * this.performance.audience;
         return result;
+    }
+
+    get volumeCredits() {
+        return super.volumeCredits + Math.floor(this.performance.audience / 5);
     }
 }
 
