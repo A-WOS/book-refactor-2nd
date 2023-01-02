3ea0422caaf1abbda77e65d98e385a9839944e05
Tetrahedrite <det3652000@naver.com>
2023-01-03 00:46:19 +0900

    refactor: create subclass of PerformanceCalculator called TragedyCalculator, ComedyCalculator

--- src/ch1/createStatementData.js
+++ src/ch1/createStatementData.js
@@ -4,27 +4,7 @@ class PerformanceCalculator {
         this.play = aPlay;
     }
     get amount() {
-        let result = 0;
-        switch (this.play.type) {
-            case "tragedy": // 비극
-                result = 40000;
-                if (this.performance.audience > 30) {
-                    result += 1000 * (this.performance.audience - 30);
-                }
-                break;
-
-            case "comedy": // 희극
-                result = 30000;
-                if (this.performance.audience > 20) {
-                    result += 10000 + 500 * (this.performance.audience - 20);
-                }
-                result += 300 * this.performance.audience;
-                break;
-
-            default:
-                throw new Error(`알 수 없는 장르: ${this.play.type}`);
-        }
-        return result;
+        throw new Error('서브클래스에서 처리하도록 설계되었습니다.');
     }
 
     get volumeCredits() {
@@ -36,6 +16,26 @@ class PerformanceCalculator {
     }
 }
 
+class TragedyCalculator extends PerformanceCalculator {
+    get amount() {
+        let result = 40000;
+        if (this.performance.audience > 30) {
+            result += 1000 * (this.performance.audience - 30);
+        }
+        return result;
+    }
+}
+
+class ComedyCalculator extends PerformanceCalculator {
+    get amount() {
+        let result = 30000;
+        if (this.performance.audience > 20) {
+            result += 10000 + 500 * (this.performance.audience - 20);
+        }
+        result += 300 * this.performance.audience;
+        return result;
+    }
+}
 
 export default function createStatementData(invoice, plays) {
     const statementData = {};
@@ -56,7 +56,12 @@ export default function createStatementData(invoice, plays) {
     }
 
     function createPerformanceCalculator(aPerformance, aPlay) {
-        return new PerformanceCalculator(aPerformance, aPlay);
+        switch (aPlay.type) {
+            case "tragedy": return new TragedyCalculator(aPerformance, aPlay);
+            case "comedy": return new ComedyCalculator(aPerformance, aPlay);
+            default:
+                throw new Error(`알 수 없는 장르: ${aPlay.type}`);
+        }
     }
 
     function playFor(aPerformance) {
