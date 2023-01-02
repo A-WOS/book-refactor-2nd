8dd993a43de735bd0d02278813230e877656a277
Tetrahedrite <det3652000@naver.com>
2023-01-03 00:14:00 +0900

    feat: add renderHtml and htmlStatement

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -7,6 +7,24 @@ export function statement(invoice, plays) {
     return renderPlainText(createStatementData(invoice, plays));
 }
 
+export function htmlStatement(invoice, plays) {
+    return renderHtml(createStatementData(invoice, plays));
+}
+
+function renderHtml(data) {
+    let result = `<h1>청구 내역 (고객명: ${data.customer})</h1>\n`;
+    result += "<table>\n";
+    result += "<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>";
+    for (let perf of data.performances) {
+        result += `  <tr><td>${perf.play.name}</td><td>(${perf.audience}석)</td>`;
+        result += `<td>${usd(perf.amount)}</td></tr>\n`;
+    }
+    result += "</table>\n";
+    result += `<p>총액: <em>${usd(data.totalAmount)}</em></p>\n`;
+    result += `<p>적립 포인트: <em>${data.totalVolumeCredits}</em>점</p>\n`;
+    return result;
+}
+
 function renderPlainText(data, plays) {
     let result = `청구 내역 (고객명: ${data.customer})\n`;
     for (let perf of data.performances) {
