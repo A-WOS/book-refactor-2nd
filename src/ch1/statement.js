94cb2ecb6749520d3bb7a5e4bb3dcc74bc8e39bb
Tetrahedrite <det3652000@naver.com>
2023-01-02 23:45:47 +0900

    refactor: pass shallow-copied parameter to renderPlainText

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -5,8 +5,13 @@ const require = createRequire(import.meta.url);
 export function statement(invoice, plays) {
     const statementData = {};
     statementData.customer = invoice.customer;
-    statementData.performances = invoice.performances;
+    statementData.performances = invoice.performances.map(enrichPerformance);
     return renderPlainText(statementData, plays);
+
+    function enrichPerformance(aPerformance) {
+        const result = Object.assign({}, aPerformance);
+        return result;
+    }
 }
 
 function renderPlainText(data, plays) {
