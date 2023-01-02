694442a347be0bc7f567b93bfa40057f72180151
Tetrahedrite <det3652000@naver.com>
2023-01-02 23:56:09 +0900

    refactor: move calculation functions to statement and enrich aPerformance

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -6,6 +6,8 @@ export function statement(invoice, plays) {
     const statementData = {};
     statementData.customer = invoice.customer;
     statementData.performances = invoice.performances.map(enrichPerformance);
+    statementData.totalAmount = totalAmount(statementData);
+    statementData.totalVolumeCredits = totalVolumeCredits(statementData);
     return renderPlainText(statementData, plays);
 
     function enrichPerformance(aPerformance) {
@@ -51,19 +53,8 @@ export function statement(invoice, plays) {
             volumeCredits += Math.floor(aPerformance.audience / 5);
         return volumeCredits;
     }
-}
 
-function renderPlainText(data, plays) {
-    let result = `청구 내역 (고객명: ${data.customer})\n`;
-    for (let perf of data.performances) {
-        result += ` ${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`;
-    }
-    result += `총액: ${usd(totalAmount())}\n`;
-    result += `적립 포인트: ${(totalVolumeCredits())}점\n`;
-
-    return result;
-
-    function totalAmount() {
+    function totalAmount(data) {
         let result = 0;
         for (let perf of data.performances) {
             result += perf.amount;
@@ -71,13 +62,24 @@ function renderPlainText(data, plays) {
         return result;
     }
 
-    function totalVolumeCredits() {
+    function totalVolumeCredits(data) {
         let result = 0;
         for (let perf of data.performances) {
             result += perf.volumeCredits;
         }
         return result;
     }
+}
+
+function renderPlainText(data, plays) {
+    let result = `청구 내역 (고객명: ${data.customer})\n`;
+    for (let perf of data.performances) {
+        result += ` ${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`;
+    }
+    result += `총액: ${usd(data.totalAmount)}\n`;
+    result += `적립 포인트: ${(data.totalVolumeCredits)}점\n`;
+
+    return result;
 
     function usd(aNumber) {
         return new Intl.NumberFormat(
