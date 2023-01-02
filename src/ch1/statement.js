346b610bb75c5a0cfd12880b3ff1b197f1bd4ab5
Tetrahedrite <det3652000@naver.com>
2023-01-02 22:13:49 +0900

    refactor: remove second parameter play (amountFor)

--- src/ch1/statement.js
+++ src/ch1/statement.js
@@ -12,7 +12,7 @@ function statement(invoice, plays) {
     ).format;
 
     for (let perf of invoice.performances) {
-        let thisAmount = amountFor(perf, playFor(perf));
+        let thisAmount = amountFor(perf);
 
         // 포인트를 적립한다.
         volumeCredits += Math.max(perf.audience - 30, 0);
