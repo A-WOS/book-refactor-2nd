ae0e1d426a16bd794c65d3dbdec908a9ac9dee6f
김윤지 <kwg531@naver.com>
2023-01-05 20:24:29 +0900

    refactor: production() 세터 테스트

--- test/ch4/index.test.js
+++ test/ch4/index.test.js
@@ -11,10 +11,15 @@ describe('province', function() {
     beforeEach(function () {
         asia = new Province(sampleProvinceData());  // 픽스처
     });
-    it('shortfall', function () {
-        expect(asia.shortfall).equal(5);    // 검증
-    });
-    it('profit', function () {
-        expect(asia.profit).equal(230);     // 검증
+    // it('shortfall', function () {
+    //     expect(asia.shortfall).equal(5);    // 검증
+    // });
+    // it('profit', function () {
+    //     expect(asia.profit).equal(230);     // 검증
+    // });
+    it('change production', function () {
+        asia.producers[0].production = 20;
+        expect(asia.shortfall).equal(-6);
+        expect(asia.profit).equal(292);
     });
 });
