253ca1c9b3335c49ee2ed6f7abd9487697c3bd76
김윤지 <kwg531@naver.com>
2023-01-05 23:27:15 +0900

    feat: 세부사항 추출 후

--- src/ch6/index.js
+++ src/ch6/index.js
@@ -3,7 +3,6 @@ function printOwing(invoice) {
 
     printBanner();
 
-
     // 미해결 채무(outstanding)를 계산한다
     for (const o of invoice.orders) {
         outstanding += o.amount;
@@ -14,14 +13,17 @@ function printOwing(invoice) {
     invoice.dueDate = new Date(today.getFullYear(), today.getMonth(),
         today.getDate() + 30);
 
-    // 세부 사항을 출력한다.
-    console.log(`고객명: ${invoice.customer}`);
-    console.log(`채무액: ${outstanding}`);
-    console.log(`마감일: ${invoice.dueDate.toLocaleDateString()}`);
+    printDetails();
 }
 
 function printBanner() {
     console.log('***********');
     console.log('**고객 채무**');
     console.log('***********');
+}
+
+function printDetails() {
+    console.log(`고객명: ${invoice.customer}`);
+    console.log(`채무액: ${outstanding}`);
+    console.log(`마감일: ${invoice.dueDate.toLocaleDateString()}`);
 }
