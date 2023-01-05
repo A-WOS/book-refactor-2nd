414b81eca73f1ec17e326be1722b32b9e5ac3e41
김윤지 <kwg531@naver.com>
2023-01-05 20:02:19 +0900

    feat: shortfall 테스트 실패 검증

--- src/ch4/index.js
+++ src/ch4/index.js
@@ -23,7 +23,7 @@ export class Province {
     set price(arg) {this._price = parseInt(arg);}       // 숫자로 파싱해서 저장
 
     get shortfall() {
-        return this.demand - this.totalProduction;
+        return this.demand - this.totalProduction * 2;
     }
     get profit() {
         return this.demandValue - this.demandCost;
