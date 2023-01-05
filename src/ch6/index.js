bee5eb6829378c2a8f26d4e35dbe0934e652db83
김윤지 <kwg531@naver.com>
2023-01-05 23:33:34 +0900

    refactor: recordDueDate() 매개변수로 넘긴 후 필드값 수정

--- src/ch6/index.js
+++ src/ch6/index.js
@@ -7,12 +7,7 @@ function printOwing(invoice) {
     for (const o of invoice.orders) {
         outstanding += o.amount;
     }
-
-    // 마감일 (dueDate)을 기록한다
-    const today = Clock.today;
-    invoice.dueDate = new Date(today.getFullYear(), today.getMonth(),
-        today.getDate() + 30);
-
+    recordDueDate(invoice); // 마감일 설정 로직을 함수로 추출
     printDetails(invoice, outstanding); // 지역 변수를 매개변수로 전달
 }
 
@@ -26,4 +21,10 @@ function printDetails(invoice, outstanding) {
     console.log(`고객명: ${invoice.customer}`);
     console.log(`채무액: ${outstanding}`);
     console.log(`마감일: ${invoice.dueDate.toLocaleDateString()}`);
+}
+
+function recordDueDate(invoice) {
+    const today = Clock.today;
+    invoice.dueDate = new Date(today.getFullYear(), today.getMonth(),
+        today.getDate() + 30);
 }
