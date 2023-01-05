0c734f9d30e039a872b080acef9735880b4447b7
김윤지 <kwg531@naver.com>
2023-01-05 14:06:46 +0900

    feat: Province 클래스에 접근자 생성

--- test/index.test.js
+++ test/index.test.js
@@ -9,6 +9,16 @@ class Province {
         doc.producers.forEach(d => this.addProducer(new Producer(this, d)));
     }
 
+    get name() {return this._name;}
+    get producers() {return this._producers.slice();}
+    get totalProduction() {return this._totalProduction;}
+    set totalProduction(arg) {return this._totalProduction = arg;}
+    get demand() {return this._demand;}
+    set demand(arg) {return this._demand = parseInt(arg);}  // 숫자로 파싱해서 저장
+    get price() {return this._price;}
+    set price(arg) {return this._price = parseInt(arg);}    // 숫자로 파싱해서 저장
+
+
     addProducer(arg) {
         this._producers.push(arg);
         this._totalProduction += arg.production;
