703f70056d6a7f8ab45c1bab1b13fee2486f6196
AWOS <uws0331@gmail.com>
2023-01-06 09:46:27 +0900

    feat: 테스트를 위한 Province 클래스 생성

---
+++ test/index.test.js
@@ -0,0 +1,15 @@
+class Province {
+    constructor(doc) {
+        this._name = doc.name;
+        this._producers = [];
+        this._totalProduction = 0;
+        this._demand = doc.demand;
+        this._price = doc.price;
+        doc.producers.forEach(d => this.addProducer(new Producer(this, d)))
+    }
+
+    addProducer(arg) {
+        this._producers.push(arg);
+        this._totalProduction += arg.totalProduction;
+    }
+}
