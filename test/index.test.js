c9e8c3e5ca42efd7d890ec2d3dbbb3358212666d
AWOS <uws0331@gmail.com>
2023-01-07 07:59:02 +0900

    test: 생산 부족분 테스트 코드 작성

--- test/index.test.js
+++ test/index.test.js
@@ -1,97 +1,11 @@
-class Province {
-    constructor(doc) {
-        this._name = doc.name;
-        this._producers = [];
-        this._totalProduction = 0;
-        this._demand = doc.demand;
-        this._price = doc.price;
-        doc.producers.forEach(d => this.addProducer(new Producer(this, d)))
-    }
+import {createRequire} from "module";
+import {Province, sampleProvinceData} from "../src/ch4/index.js";
 
-    get name() {
-        return this._name;
-    }
-
-    get producers() {
-        return this._producers.slice();
-    }
-
-    get totalProduction() {
-        return this._totalProduction;
-    }
-
-    set totalProduction(arg) {
-        this._totalProduction = arg;
-    }
-
-    get demand() {
-        return this._demand;
-    }
-
-    set demand(arg) {
-        this._demand = parseInt(arg); // 숫자로 파싱해서 저장
-    }
-
-    get price() {
-        return this._price;
-    }
-
-    set price(arg) {
-        this._price = parseInt(arg); // 숫자로 파싱해서 저장
-    }
-
-    get shortfall() {
-        return this._demand - this.totalProduction;
-    }
-
-    addProducer(arg) {
-        this._producers.push(arg);
-        this._totalProduction += arg.totalProduction;
-    }
-
-}
-
-function sampleProvinceData() {
-    return {
-        name: "Asia",
-        producers: [
-            {name: 'Byzantium', cost: 10, production: 9},
-            {name: 'Attalia', cost: 12, production: 10},
-            {name: 'Sinope', cost: 10, production: 6},
-        ],
-        demand: 30,
-        price: 20,
-    };
-}
-
-class Producer {
-    constructor(aProvince, data) {
-        this._province = aProvince;
-        this._cost = data.cost;
-        this._name = data.name;
-        this._production = data.production || 0;
-    }
-
-    get name() {
-        return this._name;
-    }
-
-    get cost() {
-        return this._cost;
-    }
-
-    set cost(arg){
-        this._cost = parseInt(arg);
-    }
-
-    get production() {
-        return this._production;
-    }
-
-    set production(amountStr){
-        const amount = parseInt(amountStr);
-        const newProduction = Number.isNaN(amount) ? 0 : amount;
-        this._province.totalProduction += newProduction - this._production;
-        this._production = newProduction;
-    }
-}
+const require = createRequire(import.meta.url);
+const assert = require('assert');
+describe('province', function() {
+    it('shortfall', function () {
+        const asia = new Province(sampleProvinceData()); // 픽스처 설정
+        assert.equal(asia.shortfall, 5); // 검증
+    });
+});
