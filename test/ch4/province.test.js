5012319d3d3ca439043cb58b18df2de445590f7d
Clustern <homerun372@gmail.com>
2023-01-07 05:15:20 +0900

    test: 총수익 계산 정확성 테스트 코드 작성

--- test/ch4/province.test.js
+++ test/ch4/province.test.js
@@ -17,4 +17,9 @@ describe('province', function () {
         const asia = new Province(sampleProvinceData());
         expect(asia.shortfall).equal(5);
     })
+
+    it('profit', function () {
+        const asia = new Province(sampleProvinceData());
+        expect(asia.profit).equal(230);
+    })
 })
