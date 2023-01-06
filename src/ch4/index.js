198a07f2bf7366ba1c05eb53eae8fe68f4902b17
AWOS <uws0331@gmail.com>
2023-01-07 08:58:36 +0900

    test: 오류 주입 및 테스트 수행

--- src/ch4/index.js
+++ src/ch4/index.js
@@ -41,7 +41,7 @@ export class Province {
     }
 
     get shortfall() {
-        return this._demand - this.totalProduction;
+        return this._demand - this.totalProduction * 2;
     }
 
     get profit() {
