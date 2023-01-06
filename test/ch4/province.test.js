654554bb95c7d3cc9529281cbabc62bec41f4208
Bell <bell04204@gmail.com>
2023-01-06 17:07:04 +0900

    test: 기존 테스트에 동일하게 생성하는 데이터셋을 beforeEach 절로 이동

--- test/ch4/province.test.js
+++ test/ch4/province.test.js
@@ -2,13 +2,17 @@ import {expect} from 'chai';
 import {Province, sampleProvinceData} from "../../src/ch4/province.js";
 
 describe('province', () => {
+    let asia;
+
+    beforeEach(() => {
+        asia = new Province(sampleProvinceData());
+    });
+
     it('shortfall', () => {
-        const asia = new Province(sampleProvinceData());
         expect(asia.shortfall).equal(5);
     });
 
     it('profit', () => {
-        const asia = new Province(sampleProvinceData());
         expect(asia.profit).equal(230);
     });
 });
