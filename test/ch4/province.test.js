ae4dc2a0d21f57f111f51fa2db7b7d40a022042b
Bell <bell04204@gmail.com>
2023-01-06 17:03:45 +0900

    test: 테스트 어셜선을 assert 대신 chai 라이브러리를 사용하도록 수정

--- test/ch4/province.test.js
+++ test/ch4/province.test.js
@@ -1,13 +1,9 @@
-import {createRequire} from "module";
 import {expect} from 'chai';
 import {Province, sampleProvinceData} from "../../src/ch4/province.js";
-
-const require = createRequire(import.meta.url);
-const assert = require('assert');
 
 describe('province', () => {
     it('shortfall', () => {
         const asia = new Province(sampleProvinceData());
-        assert.equal(asia.shortfall, 5);
+        expect(asia.shortfall).equal(5);
     });
 });
