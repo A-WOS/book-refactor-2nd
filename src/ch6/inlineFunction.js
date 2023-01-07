1f81e9711eb8907c9f99fc3f5c968e8104c3d672
Bell <bell04204@gmail.com>
2023-01-07 17:42:24 +0900

    feat: reportLines 함수 추가

--- src/ch6/inlineFunction.js
+++ src/ch6/inlineFunction.js
@@ -2,3 +2,16 @@
 function rating(aDriver) {
     return aDriver.numberOfLateDeliveries > 5 ? 2 : 1;
 }
+
+function reportLines(aCustomer) {
+    const lines = [];
+
+    gatherCustomerData(lines, aCustomer);
+
+    return lines;
+}
+
+function gatherCustomerData(out, aCustomer) {
+    out.push(["name", aCustomer.name]);
+    out.push(["location", aCustomer.location]);
+}
