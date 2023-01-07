314f97e22a224feeff4408991c55bacdff5f239e
Tetrahedrite <det3652000@naver.com>
2023-01-07 19:21:50 +0900

    feat: add calculation function for Province class

--- src/ch4/province.js
+++ src/ch4/province.js
@@ -55,4 +55,30 @@ class Province {
     set demand(arg) {this._demand = arg}
     get price() {return this._price;}
     set price(arg) {this._price = arg}
+
+    get shortfall() {
+        return this._demand - this.totalProduction;
+    }
+
+    get profit() {
+        return this.demandValue - this.demandCost;
+    }
+
+    get demandValue() {
+        return this.satisfiedDemand * this.price;
+    }
+
+    get demandCost() {
+        let remainingDemand = this.demand;
+        let result = 0;
+        this.producers
+            .sort((a, b) => a.cost - b.cost)
+            .forEach(p => {
+                const contribution = Math.min(remainingDemand, p.production);
+                remainingDemand -= contribution;
+                result += contribution * p.cost;
+            });
+
+        return result;
+    }
 }
