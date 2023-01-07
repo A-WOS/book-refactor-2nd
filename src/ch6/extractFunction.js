3877517bc2c98342c63946fe0afd1131ee7384a1
Bell <bell04204@gmail.com>
2023-01-07 17:31:22 +0900

    refactor: 출력 함수 추출

--- src/ch6/extractFunction.js
+++ src/ch6/extractFunction.js
@@ -13,17 +13,17 @@ function printOwing(invoice) {
     const today = Clock.today;
     invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
 
-    printDetails();
+    printDetails(invoice, outstanding);
 
     function printBanner() {
         console.log("**************");
         console.log("**** 고객 채무 ****");
         console.log("**************");
     }
+}
 
-    function printDetails() {
-        console.log(`고객명: ${invoice.customer}`);
-        console.log(`채무액: ${outstanding}`);
-        console.log(`마감일: ${invoice.dueDate.toLocaleDateString()}`);
-    }
+function printDetails(invoice, outstanding) {
+    console.log(`고객명: ${invoice.customer}`);
+    console.log(`채무액: ${outstanding}`);
+    console.log(`마감일: ${invoice.dueDate.toLocaleDateString()}`);
 }
