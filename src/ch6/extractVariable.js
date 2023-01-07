4da5f7589a75934733d460ef4b13b6be3f16ca8d
AWOS <uws0331@gmail.com>
2023-01-07 20:50:37 +0900

    feat: 클래스에서의 변수가 아닌 메서드로 추출하기 위한 코드 초기화

--- src/ch6/extractVariable.js
+++ src/ch6/extractVariable.js
@@ -1,3 +1,23 @@
+class Order {
+    constructor(aRecord) {
+        this._data = aRecord;
+    }
+
+    get quantity() {
+        return this._data.quantity;
+    }
+
+    get itemPrice() {
+        return this._data.itemPrice;
+    }
+
+    get price() {
+        return order.quantity * order.itemPrice -
+            Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
+            Math.min(order.quantity * order.itemPrice * 0.1, 100);
+    }
+}
+
 function price(order) {
     // 가격(price) = 기본 가격 - 수량 할인 + 배송비
     const basePrice = order.quantity * order.itemPrice;
