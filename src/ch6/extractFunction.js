a34c62ca5545932ae52fa4f8b65d3cb77782729d
Bell <bell04204@gmail.com>
2023-01-07 17:12:10 +0900

    refactor: extract printOwing function

--- src/ch6/extractFunction.js
+++ src/ch6/extractFunction.js
@@ -1,9 +1,13 @@
 
 function printOwing(invoice) {
     printBanner();
+
     let outstanding = calculateOutstanding();
 
-    // 세부사항 출력
-    console.log(`고객명: ${invoice.customer}`);
-    console.log(`채무액: ${outstanding}`);
+    printDetails(outstanding);
+
+    function printDetails(outstanding) {
+        console.log(`고객명: ${invoice.customer}`);
+        console.log(`채무액: ${outstanding}`);
+    }
 }
