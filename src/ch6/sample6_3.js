24445c327f252e0c7d73dd7c09f05f34d095982f
Clustern <homerun372@gmail.com>
2023-01-07 18:22:03 +0900

    refactor: 수량에 따른 할인을 계산하는 코드를 quantityDiscount 변수로 추출

--- src/ch6/sample6_3.js
+++ src/ch6/sample6_3.js
@@ -1,7 +1,7 @@
 function price(order) {
     //가격(price) = 기본 가격 - 수량 할인 + 배송비
     const basePrice = order.quantity * order.itemPrice;
-    return basePrice -
-        Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
+    const quantityDiscount = Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
+    return basePrice - quantityDiscount +
         Math.min(basePrice * 0.1, 100);
 }
