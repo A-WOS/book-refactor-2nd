110f12c08a43d313efe5d2de52ec81fb4c27c8cb
Tetrahedrite <det3652000@naver.com>
2023-01-07 21:22:26 +0900

    refactor: extract method printBanner/printDetails from printOwing

--- src/ch6/extract_function.js
+++ src/ch6/extract_function.js
@@ -1,9 +1,7 @@
 function printOwing(invoice) {
     let outstanding = 0;
 
-    console.log("******************");
-    console.log("**** 고객 채무 ****");
-    console.log("******************");
+    printBanner();
 
     // 미해결 채무(outstanding)을 계산한다.
     for (const o of invoice.orders) {
@@ -15,10 +13,20 @@ function printOwing(invoice) {
     invoice.dueDate = new Date(today.getFullYear(), today.getMonth(),
         today.getDate() + 30);
 
-    // 세부 사항을 출력한다.
-    console.log(`고객명: ${invoice.customer}`);
-    console.log(`채무액: ${outstanding}`);
-    console.log(`마감일: ${invoice.dueDate.toLocaleDateString()}`);
+// 세부 사항을 출력한다.
+    printDetails();
+
+    function printDetails() {
+        console.log(`고객명: ${invoice.customer}`);
+        console.log(`채무액: ${outstanding}`);
+        console.log(`마감일: ${invoice.dueDate.toLocaleDateString()}`);
+    }
+}
+
+function printBanner() {
+    console.log("******************");
+    console.log("**** 고객 채무 ****");
+    console.log("******************");
 }
 
 class Clock {
