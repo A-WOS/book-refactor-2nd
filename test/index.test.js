e14881179d564e5d171c952f09c3f90361f13f56
AWOS <uws0331@gmail.com>
2023-01-07 10:21:29 +0900

    test: 총수익을 계산하는 테스트 코드 추가

--- test/index.test.js
+++ test/index.test.js
@@ -6,4 +6,9 @@ describe('province', function() {
         const asia = new Province(sampleProvinceData()); // 픽스처 설정
         expect(asia.shortfall).equal(5); // 검증
     });
+
+    it('profit', function () {
+        const asia = new Province(sampleProvinceData()); // 픽스처 설정
+        expect(asia.profit).equal(230);
+    });
 });
