09ac8d562c6b1ccf1960dae4126345ad85fc5bc0
Bell <bell04204@gmail.com>
2023-01-07 18:54:59 +0900

    test: 로직 변경에 따른 테스트 개선

--- test/ch6/encapsulateVariable.test.js
+++ test/ch6/encapsulateVariable.test.js
@@ -1,13 +1,9 @@
 import {expect} from 'chai';
 import {defaultOwner} from "../../src/ch6/encapsulateVariable.js";
 
-describe('defaultOwner() 를 통해 기본 소유자를 여러 번 가져온 뒤', () => {
-    it('첫 번째 객체의 lastName 을 변경해도 다른 객체의 lastName 은 변경되지 않는다.', () => {
-        const owner1 = defaultOwner();
-        expect(owner1.lastName).to.equal('파울러');
-
-        const owner2 = defaultOwner();
-        owner2.lastName = '방울';
-        expect(owner1.lastName).to.equal('파울러');
+describe('defaultOwner() 를 통해 기본 소유자를 가져온 뒤', () => {
+    it('lastName 값 변경 시도 시 변경할 수 없다.', () => {
+        const owner = defaultOwner();
+        expect(() => owner.lastName = '방울').to.throw(TypeError);
     });
 });
