ff39d24f293d8d322f701c5770ebf67e37afe221
AWOS <uws0331@gmail.com>
2023-01-07 10:35:04 +0900

    refactor: beforeEach 구문을 통해 테스트를 실행하기 전 공통적으로 쓰이는 asia 를 초기화

--- test/index.test.js
+++ test/index.test.js
@@ -2,13 +2,16 @@ import {expect} from 'chai';
 import {Province, sampleProvinceData} from "../src/ch4/index.js";
 
 describe('province', function() {
+    let asia;
+    beforeEach(function() {
+        asia = new Province(sampleProvinceData());
+    });
+
     it('shortfall', function () {
-        const asia = new Province(sampleProvinceData()); // 픽스처 설정
         expect(asia.shortfall).equal(5); // 검증
     });
 
     it('profit', function () {
-        const asia = new Province(sampleProvinceData()); // 픽스처 설정
         expect(asia.profit).equal(230);
     });
 });
