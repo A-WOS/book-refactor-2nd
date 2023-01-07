63b1ce40b33d5b108d236976892e5739803cca72
Tetrahedrite <det3652000@naver.com>
2023-01-07 21:31:54 +0900

    refactor: inlining moreThanFiveLateDeliveries call

--- src/ch6/inline_function.js
+++ src/ch6/inline_function.js
@@ -1,7 +1,3 @@
 function rating(aDriver) {
-    return moreThanFiveLateDeliveries(aDriver) ? 2 : 1;
-}
-
-function moreThanFiveLateDeliveries(aDriver) {
-    return aDriver.numberOfLateDeliveries > 5;
+    return aDriver.numberOfLateDeliveries > 5 ? 2 : 1;
 }
