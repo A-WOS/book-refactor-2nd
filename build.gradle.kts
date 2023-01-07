22655602f860994b171e9cb475f3dc77a6da8514
Bell <bell04204@gmail.com>
2023-01-07 20:52:48 +0900

    feat: jackson 사용을 위한 의존성 주입

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,8 +10,10 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.14.1")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
 }
 
 tasks.getByName<Test>("test") {
