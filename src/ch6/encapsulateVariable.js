b9ed89ab808484515d4abfd07d8226f25bd6eec3
Bell <bell04204@gmail.com>
2023-01-07 18:48:49 +0900

    fix: defaultOwner() 를 통해 가져온 값을 수정하면 다른 객체도 같이 변경되는 문제 수정

--- src/ch6/encapsulateVariable.js
+++ src/ch6/encapsulateVariable.js
@@ -1,7 +1,7 @@
 let defaultOwnerData = {firstName: "마틴", lastName: "파울러"};
 
 export function defaultOwner() {
-    return defaultOwnerData;
+    return Object.assign({}, defaultOwnerData);
 }
 
 export function setDefaultOwner(arg) {
