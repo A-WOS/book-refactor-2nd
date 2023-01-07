a5bc01f0c18c809f04feb6779110811b5b77ece5
Tetrahedrite <det3652000@naver.com>
2023-01-07 19:46:55 +0900

    fix: add satisfiedDemand function

--- test/ch4.js
+++ test/ch4.js
@@ -10,6 +10,11 @@ describe('province', function () {
         const asia = new Province(sampleProvinceData());
         expect(asia.shortfall).equal(5);
     })
+
+    it('profit', function() {
+        const asia = new Province(sampleProvinceData());
+        expect(asia.profit).equal(230);
+    })
 })
 
 
