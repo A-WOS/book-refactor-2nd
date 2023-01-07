a98ef891b13a1bcba0a79f42eb8e5dceebeee2fd
AWOS <uws0331@gmail.com>
2023-01-07 10:21:03 +0900

    style: 쓰지 않는 assert 제거

--- test/index.test.js
+++ test/index.test.js
@@ -1,13 +1,9 @@
-import {createRequire} from "module";
 import {expect} from 'chai';
 import {Province, sampleProvinceData} from "../src/ch4/index.js";
 
-const require = createRequire(import.meta.url);
-const assert = require('assert');
 describe('province', function() {
     it('shortfall', function () {
         const asia = new Province(sampleProvinceData()); // 픽스처 설정
-        // assert.equal(asia.shortfall, 5); // 검증 1
-        expect(asia.shortfall).equal(5); // 검증 2
+        expect(asia.shortfall).equal(5); // 검증
     });
 });
