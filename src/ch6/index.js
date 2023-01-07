d2e48d668d0c8e009166fabe08b1a3398329da30
김윤지 <kwg531@naver.com>
2023-01-07 21:20:49 +0900

    refactor: 기존 표현식에서 basePrice에 해당하는 부분 교체

--- src/ch6/index.js
+++ src/ch6/index.js
@@ -48,7 +48,7 @@ function reportLines(aCustomer) {
 function price(order) {
     // 가격 (price) = 기본 가격 - 수량 할인 +
     const basePrice = order.quantity * order.itemPrice;
-    return order.quantity * order.itemPrice -
+    return basePrice -
         Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
-        Math.min(0, order.quantity * order.itemPrice * 0.1, 100);
+        Math.min(basePrice * 0.1, 100);
 }
