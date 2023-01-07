32671d513ece3765bea4596e184386f64b861d23
Bell <bell04204@gmail.com>
2023-01-07 17:39:23 +0900

    feat: 함수 인라인 리팩토링에 사용할 예제 추가

---
+++ src/ch6/inlineFunction.js
@@ -0,0 +1,8 @@
+
+function rating(aDriver) {
+    return moreThanFiveLateDeliveries(aDriver) ? 2 : 1;
+}
+
+function moreThanFiveLateDeliveries(aDriver) {
+    return aDriver.numberOfLateDeliveries > 5;
+}
