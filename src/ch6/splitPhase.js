f6456c5861445e799057fca825d022c65b9a52f5
Bell <bell04204@gmail.com>
2023-01-07 20:20:36 +0900

    refactor: quantity 정보를 dto 를 통해 전달하도록 개선

--- src/ch6/splitPhase.js
+++ src/ch6/splitPhase.js
@@ -1,16 +1,17 @@
 export function priceOrder(product, quantity, shippingMethod) {
     const basePrice = product.basePrice * quantity;
     const discount = Math.max(quantity - product.discountThreshold, 0) * product.basePrice * product.discountRate;
-    const priceData = {basePrice: basePrice};
-    const price = applyShipping(priceData, shippingMethod, quantity, discount);
+    const priceData = {basePrice: basePrice, quantity: quantity};
+
+    const price = applyShipping(priceData, shippingMethod, discount);
 
     return price;
 }
 
-function applyShipping(priceData, shippingMethod, quantity, discount) {
+function applyShipping(priceData, shippingMethod, discount) {
     const shippingPerCase = (priceData.basePrice > shippingMethod.discountThreshold)
         ? shippingMethod.discountedFee : shippingMethod.feePerCase;
-    const shippingCost = quantity * shippingPerCase;
+    const shippingCost = priceData.quantity * shippingPerCase;
     const price = priceData.basePrice - discount + shippingCost;
 
     return price;
