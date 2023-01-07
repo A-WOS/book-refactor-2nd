5da7d334e15973d3d5dbed6cc2413785d913dcae
Bell <bell04204@gmail.com>
2023-01-07 17:35:46 +0900

    refactor: 미해결 채무 계산 로직 추출

--- src/ch6/extractFunction.js
+++ src/ch6/extractFunction.js
@@ -1,26 +1,31 @@
 
 function printOwing(invoice) {
-    let outstanding = 0;
-
     printBanner();
 
-    // 미해결 채무(outstanding)를 계산한다.
-    for (const o of invoice.orders) {
-        outstanding += o.amount;
-    }
-
+    const outstanding = calculateOutstanding(invoice);
     recordDueDate(invoice);
-    printDetails(invoice, outstanding);
 
-    function recordDueDate(invoice) {
-        const today = Clock.today;
-        invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
-    }
+    printDetails(invoice, outstanding);
 
     function printBanner() {
         console.log("**************");
         console.log("**** 고객 채무 ****");
         console.log("**************");
+    }
+
+    function calculateOutstanding(invoice) {
+        let result = 0;
+
+        for (const o of invoice.orders) {
+            result += o.amount;
+        }
+
+        return result;
+    }
+
+    function recordDueDate(invoice) {
+        const today = Clock.today;
+        invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
     }
 }
 
