dcb196b7ae58ff93bbde4bf402ccbb3d1dae4761
Bell <bell04204@gmail.com>
2023-01-07 18:20:41 +0900

    refactor: stateCode 조회 로직 추출

--- src/ch6/changeFunctionDeclaration.js
+++ src/ch6/changeFunctionDeclaration.js
@@ -1,5 +1,7 @@
 function inNewEngland(aCustomer) {
-    return ["MA", "CT", "ME", "VT", "NH", "RI"].includes(aCustomer.address.state);
+    const stateCode = aCustomer.address.state;
+
+    return ["MA", "CT", "ME", "VT", "NH", "RI"].includes(stateCode);
 }
 
 const newEnglanders = someCustomers.filter(c => inNewEngland(c));
