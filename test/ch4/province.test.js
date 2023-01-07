078cc8d29338d5c0fd691382dd622e48e90d9e63
Clustern <homerun372@gmail.com>
2023-01-07 15:03:24 +0900

    test: producers 컬렉션이 비어있는 경우의 테스트 코드 작성

--- test/ch4/province.test.js
+++ test/ch4/province.test.js
@@ -32,3 +32,24 @@ describe('chai_province', function () {
        expect(asia.profit).equal(292);
     });
 })
+
+describe('no producers', function () {
+    let noProducers;
+    beforeEach(function () {
+        const data = {
+            name: "No producers",
+            producers: [],
+            demand: 30,
+            price: 20
+        };
+        noProducers = new Province(data);
+    });
+
+    it('shortfall', function () {
+        expect(noProducers.shortfall).equal(30);
+    });
+
+    it('profit', function () {
+        expect(noProducers.profit).equal(0);
+    });
+});
