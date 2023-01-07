3a16987c1975b431180c9d4259612049d5ddf456
Clustern <homerun372@gmail.com>
2023-01-07 16:53:37 +0900

    refactor: gatherCustomerData 함수의 코드를 인라인 진행

--- src/ch6/sample6_2-2.js
+++ src/ch6/sample6_2-2.js
@@ -1,10 +1,10 @@
 function reportLines(aCustomer) {
     const lines = [];
+    lines.push(["name", aCustomer.name]);
     gatherCustomerData(lines, aCustomer);
     return lines;
 }
 
 function gatherCustomerData(out, aCustomer) {
-    out.push(["name", aCustomer.name]);
     out.push(["location", aCustomer.location]);
 }
