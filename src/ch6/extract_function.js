fc184cbe28e8ecbb6ee4698d7f33f1d7f9bb250d
Tetrahedrite <det3652000@naver.com>
2023-01-07 21:28:12 +0900

    refactor: extract method calculateOutstanding from printOwing

--- src/ch6/extract_function.js
+++ src/ch6/extract_function.js
@@ -1,28 +1,28 @@
 function printOwing(invoice) {
-    let outstanding = 0;
-
     printBanner();
-
-    // 미해결 채무(outstanding)을 계산한다.
-    for (const o of invoice.orders) {
-        outstanding += o.amount;
-    }
-
-    // 마감일(dueDate)을 기록한다.
+    const outstanding = calculateOutstanding(invoice);
     recordDueDate(invoice);
     printDetails(invoice, outstanding);
-}
-
-function recordDueDate(invoice) {
-    const today = Clock.today;
-    invoice.dueDate = new Date(today.getFullYear(), today.getMonth(),
-        today.getDate() + 30);
 }
 
 function printBanner() {
     console.log("******************");
     console.log("**** 고객 채무 ****");
     console.log("******************");
+}
+
+function calculateOutstanding(invoice) {
+    let result = 0;
+    for (const o of invoice.orders) {
+        result += o.amount;
+    }
+    return result;
+}
+
+function recordDueDate(invoice) {
+    const today = Clock.today;
+    invoice.dueDate = new Date(today.getFullYear(), today.getMonth(),
+        today.getDate() + 30);
 }
 
 function printDetails(invoice, outstanding) {
