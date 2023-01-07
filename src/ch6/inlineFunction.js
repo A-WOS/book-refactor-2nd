8941dfb5cb526cfe60058eeaeb8e95c1fe065201
AWOS <uws0331@gmail.com>
2023-01-07 20:33:44 +0900

    feat: 인라인할 reportLines, gatherCustomerData 함수 생성

--- src/ch6/inlineFunction.js
+++ src/ch6/inlineFunction.js
@@ -2,3 +2,13 @@ function rating(aDriver) {
     return aDriver.numberOfLateDeliveries > 5 ? 2: 1;
 }
 
+function reportLines(aCustomer) {
+    const lines = [];
+    gatherCustomerData(lines, aCustomer);
+    return lines;
+}
+
+function gatherCustomerData(aLines, aCustomer) {
+    out.push(["name", aCustomer.name]);
+    out.push(["location", aCustomer.location]);
+}
