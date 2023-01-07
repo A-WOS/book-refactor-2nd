515c8d13923905d7166bc3e919d2e498d14680c7
Bell <bell04204@gmail.com>
2023-01-07 17:53:00 +0900

    feat: Order class 추가

--- src/ch6/extractVariable.js
+++ src/ch6/extractVariable.js
@@ -5,3 +5,23 @@ function price(order) {
 
     return basePrice - quantityDiscount + shipping;
 }
+
+class Order {
+    constructor(aRecord) {
+        this._data = aRecord;
+    }
+
+    get quantity() {
+        return this._data.quantity;
+    }
+
+    get itemPrice() {
+        return this._data.itemPrice;
+    }
+
+    get price() {
+        return this.quantity * this.itemPrice -
+            Math.max(0, this.quantity - 500) * this.itemPrice * 0.05 +
+            Math.min(this.quantity * this.itemPrice * 0.1, 100);
+    }
+}
