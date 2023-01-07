544a6bab186ecbac60ec64bab1c7130189f541a7
Bell <bell04204@gmail.com>
2023-01-07 17:47:20 +0900

    refactor: 수량 할인 계산식 추출

--- src/ch6/extractVariable.js
+++ src/ch6/extractVariable.js
@@ -2,7 +2,8 @@ function price(order) {
     // 가격(price) = 기본 가격 - 수량 할인 + 배송비
     const basePrice = order.quantity * order.itemPrice;
 
+    let quantityDiscount = Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
     return basePrice -
-        Math.max(0, order.quantity - 500) * order.itemPrice * 0.05 +
+        quantityDiscount +
         Math.min(basePrice * 0.1, 100);
 }
