60b470eea1413278710b458c485a9d29eae3508b
Tetrahedrite <det3652000@naver.com>
2023-01-07 19:12:48 +0900

    feat: add sampleProvinceData function

--- src/ch4/province.js
+++ src/ch4/province.js
@@ -1,3 +1,16 @@
+function sampleProvinceData() {
+    return {
+        name: "Asia",
+        producers: [
+            {name: "Byzantium", cost: 10,   production: 9},
+            {name: "Attalia",   cost: 12,   production: 10},
+            {name: "Sinope",    cost: 10,   production: 6},
+        ],
+        demand: 30,
+        price: 20
+    };
+}
+
 class Province {
     constructor(doc) {
         this._name = doc.name;
