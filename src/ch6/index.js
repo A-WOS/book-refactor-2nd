75dac88ae178caa11023285004eab6c467a116c4
AWOS <uws0331@gmail.com>
2023-01-07 19:59:30 +0900

    refactor: 배너 출력 로직을 printBanner 함수로 추출

--- src/ch6/index.js
+++ src/ch6/index.js
@@ -1,9 +1,7 @@
 function printOwing(invoice) {
     let outstanding = 0;
 
-    console.log("******************");
-    console.log("**** 고객 채무 ****");
-    console.log("******************");
+    printBanner();
 
     // 미해결 채무(outstanding) 를 계산한다.
     for (const o of invoice.orders) {
@@ -13,10 +11,17 @@ function printOwing(invoice) {
     // 마감일(dueData)을 기록한다.
     const today = Clock.today;
     invoice.dueDate = new Data(today.getFullYear(), today.getMonth(),
-                        today.getDate() + 30);
+        today.getDate() + 30);
 
     // 세부 사항을 출력한다.
     console.log('고객명: ${invoice.customer}');
     console.log('채무액: ${outstanding}');
     console.log('마감일: ${invoice.dueDate.toLocaleDateString()}');
+
+    function printBanner(){
+        console.log("******************");
+        console.log("**** 고객 채무 ****");
+        console.log("******************");
+    }
+
 }
