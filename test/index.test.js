078ccd2d3b3e17dbe86b9be831822090f3512866
AWOS <uws0331@gmail.com>
2023-01-07 10:07:33 +0900

    test: assert 대신 expect 로도 테스트 검증 가능

--- test/index.test.js
+++ test/index.test.js
@@ -1,4 +1,5 @@
 import {createRequire} from "module";
+import {expect} from 'chai';
 import {Province, sampleProvinceData} from "../src/ch4/index.js";
 
 const require = createRequire(import.meta.url);
@@ -6,6 +7,7 @@ const assert = require('assert');
 describe('province', function() {
     it('shortfall', function () {
         const asia = new Province(sampleProvinceData()); // 픽스처 설정
-        assert.equal(asia.shortfall, 5); // 검증
+        // assert.equal(asia.shortfall, 5); // 검증 1
+        expect(asia.shortfall).equal(5); // 검증 2
     });
 });
