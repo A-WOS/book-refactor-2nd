a99e1e9942657049df1e2e1440a944f8f7cced91
Bell <bell04204@gmail.com>
2023-01-07 20:20:56 +0900

    refactor: discount 정보를 dto 를 통해 전달하도록 개선

--- src/ch6/splitPhase.js
+++ src/ch6/splitPhase.js
@@ -1,18 +1,18 @@
 export function priceOrder(product, quantity, shippingMethod) {
     const basePrice = product.basePrice * quantity;
     const discount = Math.max(quantity - product.discountThreshold, 0) * product.basePrice * product.discountRate;
-    const priceData = {basePrice: basePrice, quantity: quantity};
+    const priceData = {basePrice: basePrice, quantity: quantity, discount: discount};
 
-    const price = applyShipping(priceData, shippingMethod, discount);
+    const price = applyShipping(priceData, shippingMethod);
 
     return price;
 }
 
-function applyShipping(priceData, shippingMethod, discount) {
+function applyShipping(priceData, shippingMethod) {
     const shippingPerCase = (priceData.basePrice > shippingMethod.discountThreshold)
         ? shippingMethod.discountedFee : shippingMethod.feePerCase;
     const shippingCost = priceData.quantity * shippingPerCase;
-    const price = priceData.basePrice - discount + shippingCost;
+    const price = priceData.basePrice - priceData.discount + shippingCost;
 
     return price;
 }
