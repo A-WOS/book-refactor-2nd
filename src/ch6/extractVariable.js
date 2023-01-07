0bc47c6127dac5ab319c22a30c1dee0b9de6da28
AWOS <uws0331@gmail.com>
2023-01-07 20:55:46 +0900

    refactor: 배송비를 담을 shipping 메서드로 추출 및 리팩터링

--- src/ch6/extractVariable.js
+++ src/ch6/extractVariable.js
@@ -12,8 +12,7 @@ class Order {
     }
 
     get price() {
-        return this.basePrice - this.quantityDiscount +
-            Math.min(this.basePrice * 0.1, 100);
+        return this.basePrice - this.quantityDiscount + this.shipping;
     }
 
     get basePrice() {
@@ -22,6 +21,10 @@ class Order {
 
     get quantityDiscount() {
         return Math.max(0, this.quantity - 500) * this.itemPrice * 0.05;
+    }
+
+    get shipping() {
+        return Math.min(this.basePrice * 0.1, 100);
     }
 }
 
