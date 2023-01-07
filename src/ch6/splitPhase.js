6518a86b8cf79318b0471ec2cdfaa0ba01669fac
Bell <bell04204@gmail.com>
2023-01-07 20:23:05 +0900

    refactor: priceData 처리 로직 추출

--- src/ch6/splitPhase.js
+++ src/ch6/splitPhase.js
@@ -1,18 +1,20 @@
 export function priceOrder(product, quantity, shippingMethod) {
+    const priceData = calculatePricingData(product, quantity);
+
+    return applyShipping(priceData, shippingMethod);
+}
+
+function calculatePricingData(product, quantity) {
     const basePrice = product.basePrice * quantity;
     const discount = Math.max(quantity - product.discountThreshold, 0) * product.basePrice * product.discountRate;
-    const priceData = {basePrice: basePrice, quantity: quantity, discount: discount};
 
-    const price = applyShipping(priceData, shippingMethod);
-
-    return price;
+    return {basePrice: basePrice, quantity: quantity, discount: discount};
 }
 
 function applyShipping(priceData, shippingMethod) {
     const shippingPerCase = (priceData.basePrice > shippingMethod.discountThreshold)
         ? shippingMethod.discountedFee : shippingMethod.feePerCase;
     const shippingCost = priceData.quantity * shippingPerCase;
-    const price = priceData.basePrice - priceData.discount + shippingCost;
 
-    return price;
+    return priceData.basePrice - priceData.discount + shippingCost;
 }
