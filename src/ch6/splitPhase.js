06eb52faa0adcb21f96dd0446ac824f831d5f2a4
Bell <bell04204@gmail.com>
2023-01-07 20:17:46 +0900

    refactor: 배송비 계산 로직 추출

--- src/ch6/splitPhase.js
+++ src/ch6/splitPhase.js
@@ -1,6 +1,13 @@
 export function priceOrder(product, quantity, shippingMethod) {
     const basePrice = product.basePrice * quantity;
     const discount = Math.max(quantity - product.discountThreshold, 0) * product.basePrice * product.discountRate;
+
+    const price = applyShipping(basePrice, shippingMethod, quantity, discount);
+
+    return price;
+}
+
+function applyShipping(basePrice, shippingMethod, quantity, discount) {
     const shippingPerCase = (basePrice > shippingMethod.discountThreshold)
         ? shippingMethod.discountedFee : shippingMethod.feePerCase;
     const shippingCost = quantity * shippingPerCase;
