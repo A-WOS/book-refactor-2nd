d522f9e5a0d695744aeccc0b0c5b7e304d827fda
Bell <bell04204@gmail.com>
2023-01-07 20:55:53 +0900

    refactor: 메인 로직 추출

--- src/main/java/net/bellsoft/ch6/SplitPhase.java
+++ src/main/java/net/bellsoft/ch6/SplitPhase.java
@@ -3,26 +3,31 @@ package net.bellsoft.ch6;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
 
 public class SplitPhase {
     public static void main(String[] args) {
         try {
-            if (args.length == 0) throw new RuntimeException("파일명을 입력하세요.");
-
-            String filename = args[args.length - 1];
-            File input = Paths.get(filename).toFile();
-            ObjectMapper mapper = new ObjectMapper();
-            Order[] orders = mapper.readValue(input, Order[].class);
-
-            if (Stream.of(args).anyMatch(arg -> "-r".equals(arg)))
-                System.out.println(Stream.of(orders)
-                        .filter(o -> "ready".equals(o.status))
-                        .count());
+            run(args);
         } catch (Exception e) {
             System.err.println(e);
             System.exit(1);
         }
+    }
+
+    private static void run(String[] args) throws IOException {
+        if (args.length == 0) throw new RuntimeException("파일명을 입력하세요.");
+
+        String filename = args[args.length - 1];
+        File input = Paths.get(filename).toFile();
+        ObjectMapper mapper = new ObjectMapper();
+        Order[] orders = mapper.readValue(input, Order[].class);
+
+        if (Stream.of(args).anyMatch(arg -> "-r".equals(arg)))
+            System.out.println(Stream.of(orders)
+                    .filter(o -> "ready".equals(o.status))
+                    .count());
     }
 }
