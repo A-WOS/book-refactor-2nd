f7ffa307774b5f3f424e3c9df744cf6ca13b74b1
Tetrahedrite <det3652000@naver.com>
2023-01-07 19:48:29 +0900

    refactor: move asia province init fixture to beforeEach statement

--- test/ch4.js
+++ test/ch4.js
@@ -3,16 +3,17 @@ import {expect} from 'chai';
 import {sampleProvinceData} from '../src/ch4/province.js';
 import {Province} from '../src/ch4/province.js';
 
-const require = createRequire(import.meta.url);
-
 describe('province', function () {
+    let asia;
+    beforeEach(function () {
+        asia = new Province(sampleProvinceData());
+    })
+
     it('shortfall', function() {
-        const asia = new Province(sampleProvinceData());
         expect(asia.shortfall).equal(5);
     })
 
     it('profit', function() {
-        const asia = new Province(sampleProvinceData());
         expect(asia.profit).equal(230);
     })
 })
