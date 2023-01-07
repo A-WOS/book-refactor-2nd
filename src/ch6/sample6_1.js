911f8dc747e9b82981df64b3664dcc5ab9d9b8f9
Clustern <homerun372@gmail.com>
2023-01-07 16:12:29 +0900

    refactor: 배너 출력 코드를 printBanner 로 추출

--- src/ch6/sample6_1.js
+++ src/ch6/sample6_1.js
@@ -1,8 +1,16 @@
+
+
 function printOwing(invoice) {
     let outstanding = 0;
-    console.log("**************");
-    console.log("*** 고객 채무 ***");
-    console.log("**************");
+
+    function printBanner() {
+        console.log("**************");
+        console.log("*** 고객 채무 ***");
+        console.log("**************");
+    }
+
+    printBanner();
+
 
     // 미해결 채무(outstanding)를 계산한다.
     for (const o of invoice.orders) {
