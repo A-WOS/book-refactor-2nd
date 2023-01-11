cbdc41ea62648ebb892ec5434188606e1060ddc3
Clustern <homerun372@gmail.com>
2023-01-12 02:46:11 +0900

    refactor: 레코드 읽기 코드를 Organization 클래스의 게터로 변경

--- src/ch7/encapsulateRecord7_1-1.js
+++ src/ch7/encapsulateRecord7_1-1.js
@@ -4,11 +4,12 @@ class Organization {
     }
 
     set name(aString) {this._data.name = aString;}
+    get name() {return this._data.name;}
 }
 const organization = new Organization({name: "애크미 구스베리", country: "GB"});
 function getRawDataOfOrganization() {return organization._data;}
 function getOrganization() {return organization;}
 
 
-result += `<h1>${getRawDataOfOrganization().name}</h1>`;    //읽기 예
-getOrganization().name = newName;                           //쓰기 예
+result += `<h1>${getOrganization().name}</h1>`;    //읽기 예
+getOrganization().name = newName;                  //쓰기 예
