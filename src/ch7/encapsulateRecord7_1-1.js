c053cc1cd6ccf9b5e7cb25686de9e4aa539569da
Clustern <homerun372@gmail.com>
2023-01-12 02:37:25 +0900

    refactor: organization 상수를 Organization 클래스로 변경

--- src/ch7/encapsulateRecord7_1-1.js
+++ src/ch7/encapsulateRecord7_1-1.js
@@ -1,5 +1,10 @@
-const organization = {name: "애크미 구스베리", country: "GB"};
-function getRawDataOfOrganization() {return organization;}
+class Organization {
+    constructor(data) {
+        this._data = data;
+    }
+}
+const organization = new Organization({name: "애크미 구스베리", country: "GB"});
+function getRawDataOfOrganization() {return organization._data;}
 
 /*
 result += `<h1>${getRawDataOfOrganization().name}</h1>`;  //읽기 예
