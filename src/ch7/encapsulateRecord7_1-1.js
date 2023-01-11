7602365dbf54734ef5466d3a6cb9ad93a421d0ca
Clustern <homerun372@gmail.com>
2023-01-12 02:44:19 +0900

    refactor: 레코드 갱신 코드를 Organization 클래스의 세터로 변경

--- src/ch7/encapsulateRecord7_1-1.js
+++ src/ch7/encapsulateRecord7_1-1.js
@@ -2,12 +2,13 @@ class Organization {
     constructor(data) {
         this._data = data;
     }
+
+    set name(aString) {this._data.name = aString;}
 }
 const organization = new Organization({name: "애크미 구스베리", country: "GB"});
 function getRawDataOfOrganization() {return organization._data;}
 function getOrganization() {return organization;}
 
-/*
-result += `<h1>${getRawDataOfOrganization().name}</h1>`;  //읽기 예
-getRawDataOfOrganization().name = newName;                //쓰기 예
- */
+
+result += `<h1>${getRawDataOfOrganization().name}</h1>`;    //읽기 예
+getOrganization().name = newName;                           //쓰기 예
