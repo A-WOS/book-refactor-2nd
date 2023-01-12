71f0c71372a74f39864234dd8abdf2032849cf09
Bell <bell04204@gmail.com>
2023-01-13 06:01:13 +0900

    refactor: Priority class 추가

--- src/ch7/replacePrimitiveWithObject.js
+++ src/ch7/replacePrimitiveWithObject.js
@@ -3,11 +3,21 @@ class Order {
         this.priority = data.priority;
     }
 
-    get priority() {
-        return this._priority;
+    get priorityString() {
+        return this._priority.toString();
     }
 
     set priority(aString) {
-        this._priority = aString;
+        this._priority = new Priority(aString);
+    }
+}
+
+class Priority {
+    constructor(value) {
+        this._value = value;
+    }
+
+    toString() {
+        return this._value;
     }
 }
