b7ddbf42b5a98d618c7483a67ac604bfa78ba94f
Bell <bell04204@gmail.com>
2023-01-13 06:06:23 +0900

    refactor: Priority class 개선

--- src/ch7/replacePrimitiveWithObject.js
+++ src/ch7/replacePrimitiveWithObject.js
@@ -3,6 +3,10 @@ class Order {
         this.priority = data.priority;
     }
 
+    get priority() {
+        return this._priority;
+    }
+
     get priorityString() {
         return this._priority.toString();
     }
@@ -14,10 +18,36 @@ class Order {
 
 class Priority {
     constructor(value) {
-        this._value = value;
+        if (value instanceof Priority)
+            return value;
+
+        if (Priority.legalValues().includes(value))
+            this._value = value;
+        else
+            throw new Error(`<${value}>는 유효하지 않은 우선순위입니다.`);
     }
 
     toString() {
         return this._value;
+    }
+
+    get _index() {
+        return Priority.legalValues().findIndex(s => s === this._value);
+    }
+
+    static legalValues() {
+        return ['low', 'normal', 'high', 'rush'];
+    }
+
+    equals(other) {
+        return this._index === other._index;
+    }
+
+    higherThan(other) {
+        return this._index > other._index;
+    }
+
+    lowerThan(other) {
+        return this._index < other._index;
     }
 }
