2157ed9ef21aba72092fc249b90b6cb6aa7ebd20
Clustern <homerun372@gmail.com>
2023-01-12 15:40:51 +0900

    refactor: setUsage 함수를 CustomerData 클래스로 옮김

--- src/ch7/encapsulateRecord7_1-2.js
+++ src/ch7/encapsulateRecord7_1-2.js
@@ -2,18 +2,17 @@ class CustomerData {
     constructor(data) {
         this._data = data;
     }
+
+    setUsage(customerID, year, month, amount) {
+        this._data[customerID].usages[year][month] = amount;
+    }
 }
 
 function getCustomerData() {return customerData;}
 function getRawDataOfCustomers() {return customerData._data;}
 function setRawDataOfCustomers(arg) {customerData = new CustomerData(arg);}
 
-
-function setUsage(customerID, year, month, amount) {
-    getRawDataOfCustomers()[customerID].usages[year][month] = amount;
-}
-
-setUsage(customerID, year, month, amount);
+getCustomerData().setUsage(customerID, year, month, amount);
 
 function compareUsage(customerID), laterYear, month) {
     const later = getRawDataOfCustomers()[customerID].usages[laterYear][month];
