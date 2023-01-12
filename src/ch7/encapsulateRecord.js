d37b10178a592114b0227c27797d251c69caa78c
Bell <bell04204@gmail.com>
2023-01-13 03:25:34 +0900

    feat: Organization 클래스 추가

---
+++ src/ch7/encapsulateRecord.js
@@ -0,0 +1,15 @@
+class Organization {
+    constructor(data) {
+        this._data = data;
+    }
+}
+
+const organization = new Organization({name: "애크미 구스베리", country: "GB"});
+
+function getRawDataOfOrganization() {
+    return organization._data;
+}
+
+function getOrganization() {
+    return organization;
+}
