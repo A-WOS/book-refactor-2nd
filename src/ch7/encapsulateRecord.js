cacb67ddf60a284f10c3307ac98a5be9a824875c
AWOS <uws0331@gmail.com>
2023-01-13 18:09:24 +0900

    refactor: 데이터를 개별 필드로 펼치고 게터 세터 적용(간단한 레코드 캡슐화하기)

--- src/ch7/encapsulateRecord.js
+++ src/ch7/encapsulateRecord.js
@@ -1,19 +1,23 @@
 class Organization {
     constructor(data) {
-        this._data = data;
+        this._name = data.name;
+        this._country = data.country;
     }
 
-    get name() { return this._data.name; }
-    set name(aString) {
-        this._data.name = aString;
-    }
+    get name() { return this._name; }
+    set name(aString) { this._name = aString; }
+    get country() { return this._country; }
+    set country(aString) { return this._country = aString; }
 }
 
 const organization = new Organization({name: "Awos", country: "Korea"});
 function getOrganization() { return organization; }
 
 let result = "";
-result += `<h1>${getOrganization().name}</h1>`; // 읽기 예
-getOrganization().name = "AWS"; // 쓰기 예
+result += `<h1>name: ${getOrganization().name}</h1>\n`; // 읽기 예
+result += `<h1>country: ${getOrganization().country}</h1>`; // 읽기 예
 
-console.log(getOrganization()._data);
+getOrganization().name = "AWS"; // 쓰기 예
+getOrganization().country = "America"; // 쓰기 예
+
+console.log(result);
