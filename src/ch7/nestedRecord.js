c731dd49df7f5be42569cf2de5634183a7e86e90
AWOS <uws0331@gmail.com>
2023-01-13 18:21:54 +0900

    feat: 예제 추가(중첩된 레코드 캡슐화하기)

---
+++ src/ch7/nestedRecord.js
@@ -0,0 +1,7 @@
+customerData[customerID].usages[year][month] = amount; // 쓰기 예
+
+function compareUsage(customerID, laterYear, month) {
+    const later = customerData[customerID].usages[laterYear][month];
+    const earlier = customerData[customerID].usages[laterYear - 1][month];
+    return {laterAmount: later, change: later - earlier};
+}
