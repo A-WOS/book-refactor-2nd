e68f98452fa161a30437f2ecaa7b2d35df7b5649
Clustern <homerun372@gmail.com>
2023-01-14 05:10:41 +0900

    feat: Person 클래스에 수업을 하나씩 추가/제거 메서드 추가

--- src/ch7/EncapsulateCollection7_2-1.js
+++ src/ch7/EncapsulateCollection7_2-1.js
@@ -6,6 +6,14 @@ class Person {
     get name() {return this._name;}
     get courses() {return this._courses;}
     set courses(aList) {this._courses = aList;}
+    addCourse(aCourse) {
+        this._courses.push(aCourse);
+    }
+    removeCourse(aCourse, fnIfAbsent = () => {throw new RangeError();}) {
+        const index = this._courses.indexOf(aCourse);
+        if (index === -1) fnIfAbsent();
+        else this._courses.splice(index, 1);
+    }
 }
 
 class Course {
