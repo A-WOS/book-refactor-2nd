2a7016c1e2fa4e9a8705d0446ce0c2b432f9f3da
AWOS <uws0331@gmail.com>
2023-01-13 18:03:58 +0900

    refactor: 갱신하는 부분을 세터로, 읽는 부분을 게터로 적용(간단한 레코드 캡슐화하기)

--- src/ch7/encapsulateRecord.js
+++ src/ch7/encapsulateRecord.js
@@ -2,14 +2,18 @@ class Organization {
     constructor(data) {
         this._data = data;
     }
+
+    get name() { return this._data.name; }
+    set name(aString) {
+        this._data.name = aString;
+    }
 }
 
 const organization = new Organization({name: "Awos", country: "Korea"});
-function getRawDataOrganization() { return organization._data; }
 function getOrganization() { return organization; }
 
 let result = "";
-result += `<h1>${getRawDataOrganization().name}</h1>`; // 읽기 예
-getRawDataOrganization().name = "AWS"; // 쓰기 예
+result += `<h1>${getOrganization().name}</h1>`; // 읽기 예
+getOrganization().name = "AWS"; // 쓰기 예
 
 console.log(getOrganization()._data);
