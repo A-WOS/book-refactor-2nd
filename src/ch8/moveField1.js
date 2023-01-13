0faf9c81e7242e864392ee622af3e9621dd21949
Bell <bell04204@gmail.com>
2023-01-13 20:31:00 +0900

    feat: Customer class 기반 추가

---
+++ src/ch8/moveField1.js
@@ -0,0 +1,30 @@
+function dateToday() {
+    return Date.now();
+}
+
+class Customer {
+    constructor(name, discountRate) {
+        this._name = name;
+        this._discountRate = discountRate;
+        this._contract = new CustomerContract(dateToday());
+    }
+
+    get discountRate() {
+        return this._discountRate;
+    }
+
+    becomePreferred() {
+        this._discountRate += 0.03;
+        // 다른 멋진 일들
+    }
+
+    applyDiscount(amount) {
+        return amount.subtract(amount.multiply(this._discountRate));
+    }
+}
+
+class CustomerContract {
+    constructor(startDate) {
+        this._startDate = startDate;
+    }
+}
