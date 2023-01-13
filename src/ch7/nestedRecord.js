d61481bd588cfe50ad743a4839043889fc1c501c
AWOS <uws0331@gmail.com>
2023-01-13 18:51:36 +0900

    refactor: 읽는 코드를 독립 함수로 추출후 고객 데이터 클래스로 이동(중첩된 레코드 캡슐화하기)

--- src/ch7/nestedRecord.js
+++ src/ch7/nestedRecord.js
@@ -4,6 +4,9 @@ class CustomerData {
     }
 
     get rawData() { return _.cloneDeep(this._data); }
+    usage(customerID, year, month) {
+        return this._data[customerID].usages[year][month];
+    }
     setUsage(customerID, year, month, amount) {
         this._data[customerID].usages[year][month] = amount;
     }
@@ -16,7 +19,7 @@ function setRawDataOfCustomers(arg) { customerData = new CustomerData(arg);}
 getCustomerData().setUsage(customerID, year, month, amount);
 
 function compareUsage(customerID, laterYear, month) {
-    const later = getRawDataOfCustomers()[customerID].usages[laterYear][month];
-    const earlier = getRawDataOfCustomers()[customerID].usages[laterYear - 1][month];
+    const later = getCustomerData().usage(customerID, laterYear, month);
+    const earlier = getCustomerData().usage(customerID, laterYear - 1, month);
     return {laterAmount: later, change: later - earlier};
 }
