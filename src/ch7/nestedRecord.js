cd3faa23505a932635c5e405ee46b643b8325ba0
AWOS <uws0331@gmail.com>
2023-01-13 18:24:44 +0900

    refactor: 변수 캡슐화 적용(중첩된 레코드 캡슐화하기)

--- src/ch7/nestedRecord.js
+++ src/ch7/nestedRecord.js
@@ -1,7 +1,10 @@
-customerData[customerID].usages[year][month] = amount; // 쓰기 예
+function getRawDataOfCustomers() { return customerData; }
+function setRawDataOfCustomers(arg) { customerData = arg;}
+
+getRawDataOfCustomers()[customerID].usages[year][month] = amount; // 쓰기 예
 
 function compareUsage(customerID, laterYear, month) {
-    const later = customerData[customerID].usages[laterYear][month];
-    const earlier = customerData[customerID].usages[laterYear - 1][month];
+    const later = getRawDataOfCustomers()[customerID].usages[laterYear][month];
+    const earlier = getRawDataOfCustomers()[customerID].usages[laterYear - 1][month];
     return {laterAmount: later, change: later - earlier};
 }
