8af79c148db05fc58509165c3641c4083bd499f1
Clustern <homerun372@gmail.com>
2023-01-15 05:00:13 +0900

    refactor: TrackingInformation 클래스를 Shipment 클래스에 인라인화

--- src/ch7/7_6inlineClass-1.js
+++ src/ch7/7_6inlineClass-1.js
@@ -3,20 +3,20 @@ class TrackingInformation {
     set shippingCompany(arg) {this._shippingCompany = arg;}
     get trackingNumber() {return this._trackingNumber;}
     set trackingNumber(arg) {this._trackingNumber = arg;}
-    get display() {
-        return `${this.shippingCompany}: ${this.trackingNumber}`;
-    }
 }
 
 class Shipment {
     get trackingInfo() {
-        return this._trackingInformation.display;
+        return `${this.shippingCompany}: ${this.trackigInformation}`;
     }
     get trackigInformation() {return this._trackingInformation;}
     set trackigInformation(aTrackingInformation) {
         this._trackingInformation = aTrackingInformation;
     }
-    set shippingCompany(arg) {this._trackingInformation.shippingCompany = arg;}
+    get shippingCompany() {return this._shippingCompany;}
+    set shippingCompany(arg) {this._shippingCompany = arg;}
+    get trackingNumber() {return this._trackingNumber;}
+    set trackingNumber(arg) {this._trackingNumber = arg;}
 }
 
 /* 클라이언트
