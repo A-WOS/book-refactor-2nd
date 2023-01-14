1624d536f7736e7e082db70b246ab418a2fe39a5
Clustern <homerun372@gmail.com>
2023-01-15 05:00:52 +0900

    refactor: 불요한 TrackingInformation 클래스 제거

--- src/ch7/7_6inlineClass-1.js
+++ src/ch7/7_6inlineClass-1.js
@@ -1,10 +1,3 @@
-class TrackingInformation {
-    get shippingCompany() {return this._shippingCompany;}
-    set shippingCompany(arg) {this._shippingCompany = arg;}
-    get trackingNumber() {return this._trackingNumber;}
-    set trackingNumber(arg) {this._trackingNumber = arg;}
-}
-
 class Shipment {
     get trackingInfo() {
         return `${this.shippingCompany}: ${this.trackigInformation}`;
