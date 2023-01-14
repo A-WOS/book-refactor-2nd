3659a1faa38ecd9557a120e33674c8e5fc2e4d61
Clustern <homerun372@gmail.com>
2023-01-15 07:06:06 +0900

    refactor: CustomerConstract 클래스의 discountRate 필드를 Customer 클래스에 적용

--- src/ch8/8_2moveField-1.js
+++ src/ch8/8_2moveField-1.js
@@ -1,12 +1,12 @@
 class Customer {
     constructor(name, discountRate) {
         this._name = name;
-        this._setDiscountRate(discountRate);
         this._contract = new CustomerContract(dateToday());
+        this._setDiscountRate(discountRate);
     }
 
-    get discountRate() {return this._discountRate;}
-    _setDiscountRate(aNumber) {this._discountRate = aNumber;}
+    get discountRate() {return this._contract.discountRate;}
+    _setDiscountRate(aNumber) {this._contract.discountRate = aNumber;}
     becomePreferred() {
         this._setDiscountRate(this.discountRate + 0.03);
     }
