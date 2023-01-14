48af9fb23572e316e309bfa1c0088a65cd3efc8b
Clustern <homerun372@gmail.com>
2023-01-15 06:40:42 +0900

    refactor: overdraftCharge 함수를 AccountType 클래스로 복사

--- src/ch8/8_1moveFunction-2.js
+++ src/ch8/8_1moveFunction-2.js
@@ -17,3 +17,17 @@ class Account {
             return this.daysOverdrawn * 1.75;
     }
 }
+
+class AccountType {
+    overdraftCharge(daysOverdrawn) {
+        if (this.isPremium) {
+            const baseCharge = 10;
+            if (daysOverdrawn <= 7)
+                return baseCharge;
+            else
+                return baseCharge + (daysOverdrawn - 7) * 0.85;
+        }
+        else
+            return daysOverdrawn * 1.75;
+    }
+}
