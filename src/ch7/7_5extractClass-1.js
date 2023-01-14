f23ed418c9477c2bd1683868b18368c9e72f2a7a
Clustern <homerun372@gmail.com>
2023-01-15 03:55:29 +0900

    refactor: Person 클래스의 officeAreaCode 를 다른 객체로 추출

--- src/ch7/7_5extractClass-1.js
+++ src/ch7/7_5extractClass-1.js
@@ -1,9 +1,17 @@
 class Person {
+    constructor() {
+        this._telephoneNumber = new TelephoneNumber();
+    }
     get name() {return this._name;}
     set name(arg) {this._name = arg;}
     get telephoneNumber() {return `(${this.officeAreaCode}) ${this.officeNumber}`;}
-    get officeAreaCode() {return this._officeAreaCode;}
-    set officeAreaCode(arg) {this._officeAreaCode = arg;}
+    get officeAreaCode() {return this._telephoneNumber.officeAreaCode;}
+    set officeAreaCode(arg) {this._telephoneNumber.officeAreaCode = arg;}
     get officeNumber() {return this._officeNumber;}
     set officeNumber(arg) {this._officeNumber = arg;}
+}
+
+class TelephoneNumber {
+    get officeAreaCode() {return this._officeAreaCode;}
+    set officeAreaCode(arg) {this._officeAreaCode = arg;}
 }
