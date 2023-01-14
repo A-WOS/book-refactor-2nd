30bc60a7eb6c4da2bacf7421e7f2b3a791bef1a5
Clustern <homerun372@gmail.com>
2023-01-15 00:42:02 +0900

    refactor: Order 객체 내 변수 캡슐화

--- src/ch7/7_3replacePrimitiveWithObject-1.js
+++ src/ch7/7_3replacePrimitiveWithObject-1.js
@@ -2,6 +2,9 @@ class Order {
     constructor(data) {
         this._pirority = data.priority;
     }
+
+    get priority()  {return this._priority;}
+    set priority(aString)   {this._pirority = aString;}
 }
 
 
