f8637743ff0672a91ff731bbed06ed2c31e6061d
Clustern <homerun372@gmail.com>
2023-01-15 05:30:32 +0900

    refactor: 불요한 Department 클래스 제거

--- src/ch7/7_8removeMiddleMan-1.js
+++ src/ch7/7_8removeMiddleMan-1.js
@@ -4,8 +4,3 @@ class Person {
     get manager() {return this._department.manager;}
     get department() {return this._department;}
 }
-
-class Department {
-    get manager() {return this._manger;}
-}
-
