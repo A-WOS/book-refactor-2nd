40fe3612992626b3c1dcbaf4b4676479faa9d302
Clustern <homerun372@gmail.com>
2023-01-15 06:48:35 +0900

    refactor: _discountRate 변수를 _setDiscountRate 메소드로 캡슐화

--- src/ch8/8_2moveField-1.js
+++ src/ch8/8_2moveField-1.js
@@ -1,16 +1,17 @@
 class Customer {
     constructor(name, discountRate) {
         this._name = name;
-        this._discountRate = discountRate;
+        this._setDiscountRate(discountRate);
         this._contract = new CustomerContract(dateToday());
     }
 
     get discountRate() {return this._discountRate;}
+    _setDiscountRate(aNumber) {this._discountRate = aNumber;}
     becomePreferred() {
-        this._discountRate += 0.03;
+        this._setDiscountRate(this.discountRate + 0.03);
     }
     applyDiscount(amount) {
-        return amount.subtract(amount.multiply(this._discountRate));
+        return amount.subtract(amount.multiply(this.discountRate));
     }
 }
 
