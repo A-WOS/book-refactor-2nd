b5fbdbd175e3c4487cb6fc8dbaeda70f25f596ad
Clustern <homerun372@gmail.com>
2023-01-15 05:06:43 +0900

    feat: 7장 7절 예제 코드 작성

---
+++ src/ch7/7_7hideDelegate-1.js
@@ -0,0 +1,20 @@
+class Person {
+    constructor(name) {
+        this._name = name;
+    }
+
+    get name() {return this._name;}
+    get department() {return this._department;}
+    set department(arg) {this._department = arg;}
+}
+
+class Department {
+    get changeCode() {return this._chargeCode;}
+    set chargeCode(arg) {this._chargeCode = arg;}
+    get manger() {return this._manger;}
+    set manager(arg) {this._manger = arg;}
+}
+
+/* 클라이언트
+manger = aPerson.department.manger;
+ */
