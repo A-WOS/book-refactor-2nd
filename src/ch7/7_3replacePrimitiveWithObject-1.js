2b9800c35e8b48e325055ca3112e036cd5e6c56f
Clustern <homerun372@gmail.com>
2023-01-15 02:36:13 +0900

    refactor: Priority 클래스 기능 개선

--- src/ch7/7_3replacePrimitiveWithObject-1.js
+++ src/ch7/7_3replacePrimitiveWithObject-1.js
@@ -10,13 +10,22 @@ export class Order {
 
 class Priority {
     constructor(value) {
-        this._value = value;
+        if (value instanceof Priority) return value;
+        if (Priority.legalValues().includes(value))
+            this._value = value;
+        else
+            throw new Error(`<${value}>는 유효하지 않은 우선순위입니다.`);
     }
 
     toString() {return this._value;}
+    get _index() {return Priority.legalValues().findIndex(s => s === this._value);}
+    static legalValues() {return ['low', 'normal', 'high', 'rush'];}
+    equals(other) {return this._index === other._index;}
+    higherThan(other) {return this._index > other._index;}
+    lowerThan(other) {return this._index < other._index;}
 }
 
 
 // 클라이언트
-highPriorityCount = orders.filter(o => "high" === o.priorityString || "rush" === o.priorityString).length;
+highPriorityCount = orders.filter(o => o.priority.higherThan(new Priority("normal"))).length;
 
