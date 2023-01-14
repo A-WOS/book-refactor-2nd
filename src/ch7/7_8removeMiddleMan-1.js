df1bcd0f9b0aa6689957a3f48e16ae405c00e25a
Clustern <homerun372@gmail.com>
2023-01-15 05:28:57 +0900

    refactor: Person 클래스에 Department 클래스를 얻는 게터 작성

--- src/ch7/7_8removeMiddleMan-1.js
+++ src/ch7/7_8removeMiddleMan-1.js
@@ -2,8 +2,10 @@ manager = aPerson.manager;
 
 class Person {
     get manager() {return this._department.manager;}
+    get department() {return this._department;}
 }
 
 class Department {
     get manager() {return this._manger;}
 }
+
