42a765ac71e16f2163608c6b5238e4abeb969c89
Clustern <homerun372@gmail.com>
2023-01-15 03:11:33 +0900

    refactor: price 게터 내 discountFactor 변수 관련 코드를 메소드로 추출

--- src/ch7/7_4replaceTempWithQuery-1.js
+++ src/ch7/7_4replaceTempWithQuery-1.js
@@ -5,13 +5,17 @@ class Order {
     }
 
     get price() {
-        var discountFactor = 0.98;
-
-        if (this.basePrice() > 1000) discountFactor -= 0.03;
+        var discountFactor = this.discountFactor();
         return this.basePrice() * discountFactor;
     }
 
     get basePrice() {
         return this._quantity * this._item.price;
+    }
+
+    get discountFactor() {
+        var discountFactor = 0.98;
+        if (this.basePrice() > 1000) discountFactor -= 0.03;
+        return discountFactor;
     }
 }
