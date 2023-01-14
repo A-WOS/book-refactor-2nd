b9d899f509a945c65ed9b5b4798042d1e18ce858
Clustern <homerun372@gmail.com>
2023-01-15 06:50:16 +0900

    refactor: CustomerConstract 클래스에 _discountRate 필드 추가

--- src/ch8/8_2moveField-1.js
+++ src/ch8/8_2moveField-1.js
@@ -16,7 +16,11 @@ class Customer {
 }
 
 class CustomerConstract {
-    constructor(startDate) {
+    constructor(startDate, discountRate) {
         this._startDate = startDate;
+        this._discountRate = discountRate;
     }
+
+    get discountRate() {return this._discountRate;}
+    set discountRate(arg) {this._discountRate = arg;}
 }
