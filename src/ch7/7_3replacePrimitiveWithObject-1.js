581118cb557069e5ba056e7393d98408a1ec98f1
Clustern <homerun372@gmail.com>
2023-01-15 02:20:59 +0900

    refactor: Order 클래스에 Priority 클래스를 제공하는 게터를 작성

--- src/ch7/7_3replacePrimitiveWithObject-1.js
+++ src/ch7/7_3replacePrimitiveWithObject-1.js
@@ -3,8 +3,9 @@ export class Order {
         this._priority = data.priority;
     }
 
-    get priorityString()  {return this._priority.toString();}
-    set priority(aString)   {this._priority = new Priority(aString);}
+    get priority() {return this._priority;}
+    get priorityString() {return this._priority.toString();}
+    set priority(aString) {this._priority = new Priority(aString);}
 }
 
 class Priority {
