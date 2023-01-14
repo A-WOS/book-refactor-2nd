ead47e4e066e2cb24dce1d1b1d50f06b39ebb873
Clustern <homerun372@gmail.com>
2023-01-15 06:16:54 +0900

    feat: 8장 1절 예제 2번 코드 작성

---
+++ src/ch8/8_1moveFunction-2.js
@@ -0,0 +1,19 @@
+class Account {
+    get bankCharge() {
+        let result = 4.5;
+        if (this._daysOverdrawn > 0) result += this.overdraftCharge;
+        return result;
+    }
+
+    get overdraftCharge() {
+        if (this.type.isPremium) {
+            const baseCharge = 10;
+            if (this.daysOverdrawn <= 7)
+                return baseCharge;
+            else
+                return baseCharge + (this.daysOverdrawn - 7) * 0.85;
+        }
+        else
+            return this.daysOverdrawn * 1.75;
+    }
+}
