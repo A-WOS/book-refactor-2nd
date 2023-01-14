1a70bcb64c8eb11158d45fb2e132d2a83b950720
Clustern <homerun372@gmail.com>
2023-01-15 04:00:33 +0900

    refactor: Person 클래스의 officeNumber 를 TelephoneNumber 객체로 추출

--- src/ch7/7_5extractClass-1.js
+++ src/ch7/7_5extractClass-1.js
@@ -7,11 +7,13 @@ class Person {
     get telephoneNumber() {return `(${this.officeAreaCode}) ${this.officeNumber}`;}
     get officeAreaCode() {return this._telephoneNumber.officeAreaCode;}
     set officeAreaCode(arg) {this._telephoneNumber.officeAreaCode = arg;}
-    get officeNumber() {return this._officeNumber;}
-    set officeNumber(arg) {this._officeNumber = arg;}
+    get officeNumber() {return this._telephoneNumber.officeNumber;}
+    set officeNumber(arg) {this._telephoneNumber.officeNumber = arg;}
 }
 
 class TelephoneNumber {
     get officeAreaCode() {return this._officeAreaCode;}
     set officeAreaCode(arg) {this._officeAreaCode = arg;}
+    get officeNumber() {return this._officeNumber;}
+    set officeNumber(arg) {this._officeNumber = arg;}
 }
