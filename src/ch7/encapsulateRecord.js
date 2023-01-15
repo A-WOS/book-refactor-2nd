43e2e68469719f01102956d09af409f294bf82b6
Tetrahedrite <det3652000@naver.com>
2023-01-15 21:00:21 +0900

    feat: encapsulate Organization to class

--- src/ch7/encapsulateRecord.js
+++ src/ch7/encapsulateRecord.js
@@ -1,3 +1,10 @@
-const organization = {name: "애크미 구스베리", country: "GB"};
+class Organization {
+    constructor(data) {
+        this._data = data;
+    }
+}
 
-function getRawDataOfOrganization() {return organization;}
+const organization = new Organization({name: "애크미 구스베리", country: "GB"});
+
+function getRawDataOfOrganization() {return organization._data;}
+function getOrganization() {return organization;}
