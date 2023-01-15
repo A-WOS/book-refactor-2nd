a09b59552c0ba33c44d52a91927992ab55e8988c
Clustern <homerun372@gmail.com>
2023-01-15 18:21:55 +0900

    refactor: youngestAge 함수 코드 알고리즘 교체

--- src/ch8/8_7splitLoop-1.js
+++ src/ch8/8_7splitLoop-1.js
@@ -1,9 +1,5 @@
 function youngestAge() {
-    let youngest = people[0] ? people[0].age : Infinity;
-    for (const p of people) {
-        if (p.age < youngest) youngest = p.age;
-    }
-    return youngest;
+    return Math.min(people.map(p => p.age));
 }
 
 function totalSalary() {
