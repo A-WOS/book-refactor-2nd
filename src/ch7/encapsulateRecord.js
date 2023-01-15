d75d6794ff0ed79fb8e627049e9e0582426dfbe3
Tetrahedrite <det3652000@naver.com>
2023-01-15 21:02:29 +0900

    feat: add example function to get/set name of organization

--- src/ch7/encapsulateRecord.js
+++ src/ch7/encapsulateRecord.js
@@ -8,3 +8,11 @@ const organization = new Organization({name: "애크미 구스베리", country: 
 
 function getRawDataOfOrganization() {return organization._data;}
 function getOrganization() {return organization;}
+
+function getNameOfOrganization() {
+    return organization._data;
+}
+
+function setNameOfOrganization(newName) {
+    getRawDataOfOrganization().name = newName;
+}
