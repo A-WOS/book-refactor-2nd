80a873d660d05a862c48df19a7c91c83753baa99
Clustern <homerun372@gmail.com>
2023-01-15 19:31:04 +0900

    refactor: 기능이 상실된 반복문 제거 및 result 변수에 loopItems 인라인

--- src/ch8/8_8replaceLoopWithPipeline-1.js
+++ src/ch8/8_8replaceLoopWithPipeline-1.js
@@ -8,18 +8,13 @@ const input =
 
 function acquireData(input) {
     const lines = input.split("\n");
-    const result = [];
-    const loopItems = lines
+    const result = lines
         .slice(1)
         .filter(line => line.trim() !== "")
         .map(line => line.split(","))
         .filter(record => record[1].trim() === "India")
         .map(record => ({city: record[0].trim(), phone: record[2].trim()}))
     ;
-    for (const line of loopItems) {
-        const record = line;
-        result.push({city: record[0].trim(), phone: record[2].trim()});
-    }
     return result;
 }
 
