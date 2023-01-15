22c0d9ef8a48ae0f501264739e16976dc1e8b23b
Clustern <homerun372@gmail.com>
2023-01-15 18:14:33 +0900

    refactor: 최연소 계산 관련 코드를 메소드로 추출

--- src/ch8/8_7splitLoop-1.js
+++ src/ch8/8_7splitLoop-1.js
@@ -1,8 +1,13 @@
-function youngestSalary() {
+function youngestAge() {
     let youngest = people[0] ? people[0].age : Infinity;
     for (const p of people) {
         if (p.age < youngest) youngest = p.age;
     }
+    return youngest;
+}
+
+function youngestSalary() {
+    let youngest = youngestAge();
     let totalSalary = 0;
     for (const p of people) {
         totalSalary += p.salary;
