941b02bab60a012547b0c529e4a138b5fa570f04
Clustern <homerun372@gmail.com>
2023-01-15 18:13:34 +0900

    refactor: 문장 슬라이스를 위한 코드 위치 변경

--- src/ch8/8_7splitLoop-1.js
+++ src/ch8/8_7splitLoop-1.js
@@ -1,9 +1,9 @@
 function youngestSalary() {
     let youngest = people[0] ? people[0].age : Infinity;
-    let totalSalary = 0;
     for (const p of people) {
         if (p.age < youngest) youngest = p.age;
     }
+    let totalSalary = 0;
     for (const p of people) {
         totalSalary += p.salary;
     }
