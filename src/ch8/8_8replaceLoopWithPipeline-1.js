71656b143f6f161420da7e9e2abf96ce1adaa826
Clustern <homerun372@gmail.com>
2023-01-15 19:19:25 +0900

    refactor: 반복문의 컬렉션을 가리키는 변수를 따로 작성

--- src/ch8/8_8replaceLoopWithPipeline-1.js
+++ src/ch8/8_8replaceLoopWithPipeline-1.js
@@ -10,7 +10,8 @@ function acquireData(input) {
     const lines = input.split("\n");
     let firstLine = true;
     const result = [];
-    for (const line of lines) {
+    const loopItems = lines;
+    for (const line of loopItems) {
         if (firstLine) {
             firstLine = false;
             continue;
