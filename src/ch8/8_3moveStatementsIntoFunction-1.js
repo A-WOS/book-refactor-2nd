ad08faae749550a9e28f2ceef2d4014ea58594e1
Clustern <homerun372@gmail.com>
2023-01-15 16:36:22 +0900

    refactor: renderPerson 함수에 zznew 함수 적용

--- src/ch8/8_3moveStatementsIntoFunction-1.js
+++ src/ch8/8_3moveStatementsIntoFunction-1.js
@@ -2,8 +2,7 @@ function renderPerson(outStream, person) {
     const result = [];
     result.push(`<p>${person.name}</p>`);
     result.push(renderPhoto(person.photo));
-    result.push(`<p>제목: ${person.photo.title}</p>`);
-    result.push(emitPhotoData(person.photo));
+    result.push(zznew(person.photo));
     return result.join("\n");
 }
 
