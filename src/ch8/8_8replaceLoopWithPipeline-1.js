b0bb96fa93bbe7d9821e03671b2071362cbd813a
Clustern <homerun372@gmail.com>
2023-01-15 19:36:24 +0900

    style: 람다 함수 이름 변경 및 정렬

--- src/ch8/8_8replaceLoopWithPipeline-1.js
+++ src/ch8/8_8replaceLoopWithPipeline-1.js
@@ -10,10 +10,11 @@ function acquireData(input) {
     const lines = input.split("\n");
     return lines
         .slice(1)
-        .filter(line => line.trim() !== "")
-        .map(line => line.split(","))
-        .filter(record => record[1].trim() === "India")
-        .map(record => ({city: record[0].trim(), phone: record[2].trim()}));
+        .filter (line   => line.trim() !== "")
+        .map    (line   => line.split(","))
+        .filter (fields => fields[1].trim() === "India")
+        .map    (fields => ({city: fields[0].trim(), phone: fields[2].trim()}))
+        ;
 }
 
 console.log(acquireData(input))
