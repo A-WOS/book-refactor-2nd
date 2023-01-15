9beb8dc298719203bddbf2ee942ca1c70d085c3a
Clustern <homerun372@gmail.com>
2023-01-15 17:24:50 +0900

    refactor: zztmp 함수 이름을 emitPhotoData 로 변경

--- src/ch8/8_4moveStatementsToCallers-1.js
+++ src/ch8/8_4moveStatementsToCallers-1.js
@@ -1,20 +1,20 @@
 function renderPerson(outStream, person) {
     outStream.write(`<p>${person.name}</p>\n`);
     renderPhoto(outStream, person.photo);
-    zztmp(outStream, person.photo);
+    emitPhotoData(outStream, person.photo);
     outStream.write(`<p>위치: ${person.photo.location}</p>\n`)
 }
 
 function listRecentPhotos(outStream, photos) {
     photos.filter(p => p.data > recentDateCutoff()).forEach(p => {
         outStream.write("<div>\n");
-        zztmp(outStream, p);
+        emitPhotoData(outStream, p);
         outStream.write(`<p>위치: ${p.location}</p>\n`)
         outStream.write("</div>\n");
     });
 }
 
-function zztmp(outStream, photo) {
+function emitPhotoData(outStream, photo) {
     outStream.write(`<p>제목: ${photo.title}</p>\n`)
     outStream.write(`<p>날짜: ${photo.data.toDateString()}</p>\n`)
 }
