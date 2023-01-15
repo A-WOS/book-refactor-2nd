962c9eae695b0f9e818c2b65f0c08d421e4c1c68
Clustern <homerun372@gmail.com>
2023-01-15 16:53:18 +0900

    refactor: zznew 함수 이름을 emitPhotoData 로 변경

--- src/ch8/8_3moveStatementsIntoFunction-1.js
+++ src/ch8/8_3moveStatementsIntoFunction-1.js
@@ -2,19 +2,19 @@ function renderPerson(outStream, person) {
     const result = [];
     result.push(`<p>${person.name}</p>`);
     result.push(renderPhoto(person.photo));
-    result.push(zznew(person.photo));
+    result.push(emitPhotoData(person.photo));
     return result.join("\n");
 }
 
 function photoDiv(p) {
     return [
         "<div>",
-        zznew(p),
+        emitPhotoData(p),
         "</div>",
     ].join("\n");
 }
 
-function zznew(aPhoto) {
+function emitPhotoData(aPhoto) {
     return [
         `<p>제목: ${aPhoto.title}</p>`,
         `<p>위치: ${aPhoto.location}</p>`,
