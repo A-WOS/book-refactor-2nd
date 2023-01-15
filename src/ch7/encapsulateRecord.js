8ac276acc7098f425f17ea70f93141953a45d2d1
Tetrahedrite <det3652000@naver.com>
2023-01-15 21:05:28 +0900

    feat: add getter/setter to Organization class

--- src/ch7/encapsulateRecord.js
+++ src/ch7/encapsulateRecord.js
@@ -2,17 +2,19 @@ class Organization {
     constructor(data) {
         this._data = data;
     }
+
+    set name(aString) {this._data.name = aString;}
+    get name() {return this._data.name;}
 }
 
 const organization = new Organization({name: "애크미 구스베리", country: "GB"});
 
-function getRawDataOfOrganization() {return organization._data;}
 function getOrganization() {return organization;}
 
 function getNameOfOrganization() {
-    return organization._data.name;
+    return getOrganization().name;
 }
 
 function setNameOfOrganization(newName) {
-    getRawDataOfOrganization().name = newName;
+    getOrganization().name = newName;
 }
