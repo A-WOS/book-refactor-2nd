19a5c0ec5017991309f52a2ea474cd272092ad21
Clustern <homerun372@gmail.com>
2023-01-17 18:05:17 +0900

    feat: 9장 1절 예제 코드 추가

---
+++ src/ch9/9_1splitVariable-1.js
@@ -0,0 +1,13 @@
+function distanceTravelled (scenario, time) {
+    let result;
+    let acc = scenario.primaryForce / scenario.mass;
+    let primaryTime = Math.min(time, scenario.delay);
+    result = 0.5 * acc * primaryTime * primaryTime;
+    let secondaryTime = time -scenario.delay;
+    if (secondaryTime > 0) {
+        let primaryVelociry = acc * scenario.delay;
+        acc = (scenario.primaryForce + scenario.secondaryForce) / scenario.mass;
+        result += primaryVelociry * secondaryTime + 0.5 * acc * secondaryTime * secondaryTime;
+    }
+    return result;
+}
