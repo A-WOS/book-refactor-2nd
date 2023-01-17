ea39dc4b70da6bdfcbe44fa9be6af5689b2bfb69
Clustern <homerun372@gmail.com>
2023-01-17 20:06:59 +0900

    refactor: 생성자가 title 을 받을 수 있게 개선

--- src/ch9/9_2renameField-1.js
+++ src/ch9/9_2renameField-1.js
@@ -1,6 +1,6 @@
 class Organization {
     constructor(data) {
-        this._title = data.name;
+        this._title = (data.title !== undefined) ? data.title : data.name;
         this._country = data.country;
     }
 
