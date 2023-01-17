631b1a750280dc10c5c5db262e09ee8d5c1c0cb3
Clustern <homerun372@gmail.com>
2023-01-17 21:31:50 +0900

    refactor: _production 변수 개선을 위해 이를 분할

--- src/ch9/9_3replaceDerivedVariableWithQuery-2.js
+++ src/ch9/9_3replaceDerivedVariableWithQuery-2.js
@@ -1,10 +1,14 @@
 class ProductionPlan {
     constructor(production) {
-        this._production = production;
+        this._initialProduction = production;
+        this._productionAccumulator = 0;
         this._adjustments = [];
     }
 
-    get production() {return this._production;}
+    get production() {
+        return this._initialProduction + this._productionAccumulator;
+    }
+
     applyAdjustment(anAdjustment) {
         this._adjustments.push(anAdjustment);
         this._production += anAdjustment.amount;
