0181ca82c226939da465d510272dcb53bf1d2cae
Clustern <homerun372@gmail.com>
2023-01-17 21:46:57 +0900

    refactor: Person 클래스의 officeNumber 세터 변경
    TelephoneNumber 객체를 생성하는 방식으로 변경

--- src/ch9/9_4changeReferenceToValue-1.js
+++ src/ch9/9_4changeReferenceToValue-1.js
@@ -8,7 +8,9 @@ class Person {
         this._telephoneNumber = new TelephoneNumber(arg, this.officeNumber);
     }
     get officeNumber() {return this._telephoneNumber.number;}
-    set officeNumber(arg) {this._number = arg;}
+    set officeNumber(arg) {
+        this._telephoneNumber = new TelephoneNumber(arg, this.officeNumber);
+    }
 }
 
 class TelephoneNumber {
