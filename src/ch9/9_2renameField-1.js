4bc1b6d8677b3db68736e38601d5393390e6c01c
Clustern <homerun372@gmail.com>
2023-01-17 20:23:36 +0900

    refactor: name 필드 이름을 title 로 변경

--- src/ch9/9_2renameField-1.js
+++ src/ch9/9_2renameField-1.js
@@ -4,8 +4,8 @@ class Organization {
         this._country = data.country;
     }
 
-    get name() {return this._title;}
-    set name(aString) {this._title = aString;}
+    get title() {return this._title;}
+    set title(aString) {this._title = aString;}
     get country() {return this._country;}
     set country(aCountryCode) {this._country = aCountryCode;}
 }
