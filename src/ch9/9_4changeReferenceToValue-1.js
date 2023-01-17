d4b20363c63666341e052da7dc00227e1f71c3c7
Clustern <homerun372@gmail.com>
2023-01-17 21:44:12 +0900

    refactor: TelephoneNumber 클래스 선언 변경
    areaCode 와 number 필드를 생성자에서 데이터를 입력받도록 변경

--- src/ch9/9_4changeReferenceToValue-1.js
+++ src/ch9/9_4changeReferenceToValue-1.js
@@ -10,6 +10,10 @@ class Person {
 }
 
 class TelephoneNumber {
+    constructor(areaCode, number) {
+        this._areaCode = areaCode;
+        this._number = number;
+    }
     get areaCode() {return this._areaCode;}
     set areaCode(arg) {this._areaCode = arg;}
     get number() {return this._number;}
