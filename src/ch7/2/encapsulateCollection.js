9aa1341a8ebc867f6af2a2e42867aa1216efbd20
AWOS <uws0331@gmail.com>
2023-01-18 17:26:17 +0900

    feat: 예제 추가(컬렉션 캡슐화하기)

---
+++ src/ch7/2/encapsulateCollection.js
@@ -0,0 +1,31 @@
+class Person {
+    constructor(name) {
+        this._name = name;
+        this._courses = [];
+    }
+
+    get name() { return this._name; }
+    get courses() { return this._courses; }
+    set courses(aList) { this._courses = aList; }
+}
+
+class Course {
+    constructor(name, isAdvanced) {
+        this._name = name;
+        this._isAdvanced = isAdvanced;
+    }
+
+    get name(){ return this._name; }
+    get isAdvanced() { return this._isAdvanced; }
+}
+
+numAdvancedCourses = aPerson.courses
+                    .filter(c => c.isAdvanced)
+                    .length
+                    ;
+
+const basicCourseNames = readBasicCourseNames(filename);
+aPerson.courses = basicCourseNames.map(name => new Course(name, false));
+for(const name of readBasicCourseNames(filename)) {
+    aPerson.courses.push(new Course(name));
+}
