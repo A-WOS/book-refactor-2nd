751d8d90406552bf7489d382b127c556e0ab02ca
AWOS <uws0331@gmail.com>
2023-01-18 17:38:24 +0900

    feat: 예제 코드 추가(9.1 변수 쪼개기)

---
+++ src/ch9/9_1splitVariable.js
@@ -0,0 +1,13 @@
+function distanceTravelled(scenario, time) {
+    let result;
+    let acc = scenario.primaryForce / scenario.mass; // 가속도(a) = 힘(F) / 질량(m)
+    let primaryTime = Math.min(time, scenario.delay);
+    result = 0.5 * acc * primaryTime * primaryTime; // 전파된 거리
+    let secondaryTime = time -scenario.delay;
+    if (secondaryTime > 0) { // 두 번째 힘을 반영해 다시 계산
+        let primaryVelociry = acc * scenario.delay;
+        acc = (scenario.primaryForce + scenario.secondaryForce) / scenario.mass;
+        result += primaryVelociry * secondaryTime + 0.5 * acc * secondaryTime * secondaryTime;
+    }
+    return result;
+}
