d5b38e257350e65caee0c618f076f19d0f1ddb61
Clustern <homerun372@gmail.com>
2023-01-18 16:06:51 +0900

    refactor: 고객 객체의 데이터 일관성 개선을 위해 참조로 변환하는 객체 생성

--- src/ch9/9_5changeValueToReference-1.js
+++ src/ch9/9_5changeValueToReference-1.js
@@ -1,3 +1,20 @@
+let _repositoryData;
+
+export function initialize() {
+    _repositoryData = {};
+    _repositoryData.customers = new Map();
+}
+
+export function registerCustomer(id) {
+    if (! _repositoryData.customers.has(id))
+        _repositoryData.customers.set(id, new Customer(id));
+    return findCustomer(id);
+}
+
+export function findCustomer(id) {
+    return _repositoryData.customers.get(id);
+}
+
 export class Order {
     constructor(data) {
         this._number = data.number;
@@ -15,4 +32,3 @@ class Customer {
 
     get id() {return this._id;}
 }
-
