fa230deaad5dfc900b11681f395622bdc29efee3
Clustern <homerun372@gmail.com>
2023-01-18 16:21:38 +0900

    refactor: 과도하게 긴 조건문 로직을 함수로 분리

--- src/ch10/10_1decomposeConditional-1.js
+++ src/ch10/10_1decomposeConditional-1.js
@@ -1,4 +1,8 @@
-if (!aDate.isBefore(plan.summerStart) && !aDate.isAfter(plan.sumerEnd))
+if (summer())
     charge = quantity * plan.summerRate;
 else
     charge = quantity * plan.regularRate + plan.regularServiceCharge;
+
+function summer() {
+    return !aDate.isBefore(plan.summerStart) && !aDate.isAfter(plan.sumerEnd);
+}
