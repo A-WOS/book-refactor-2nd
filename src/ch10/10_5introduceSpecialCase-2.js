e25a59edb6fc2fdb172cd4dfc2a622f3372fbbb8
Clustern <homerun372@gmail.com>
2023-01-20 08:52:24 +0900

    feat: 10장 5절 예제 2번 코드 추가

---
+++ src/ch10/10_5introduceSpecialCase-2.js
@@ -0,0 +1,26 @@
+class Site {
+    get customer() {return this._customer;}
+}
+
+class Customer {
+    get name() {}
+    get billingPlan() {}
+    set billingPlan(arg) {}
+    get paymentHistory() {}
+}
+
+// 클라이언트1
+const aCustomer = site.customer;
+let customerName;
+if (aCustomer === "미확인 고객") customerName = "거주자";
+else customerName = aCustomer.name;
+
+// 클라이언트2
+const Plan = (aCustomer === "미확인 고객") ?
+    registry.billingPlans.basic
+    : aCustomer.billingPlan;
+
+// 클라이언트3
+const weekDelinquent =  (aCustomer === "미확인 고객") ?
+    0
+    : aCustomer.paymentHistory.weekDelinquentInLastYear;
