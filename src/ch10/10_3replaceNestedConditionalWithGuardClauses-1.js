a89a4afdbc65438040d1219f5eaaac5d020ae28b
Clustern <homerun372@gmail.com>
2023-01-19 22:05:52 +0900

    refactor: 첫번째 if 문의 만족 시 실행 코드를 결과 값을 즉시 반환하도록 변경

--- src/ch10/10_3replaceNestedConditionalWithGuardClauses-1.js
+++ src/ch10/10_3replaceNestedConditionalWithGuardClauses-1.js
@@ -1,19 +1,15 @@
 function payAmount(employee) {
     let result;
-    if(employee.isSeparated) {
-        result = {amount: 0, reasonCode: "SEP"};
+    if(employee.isSeparated) return {amount: 0, reasonCode: "SEP"};
+    if (employee.isRetired) {
+        result = {amount: 0, reasonCode: "RET"};
     }
     else {
-        if (employee.isRetired) {
-            result = {amount: 0, reasonCode: "RET"};
-        }
-        else {
-            lorem.ipsum(dolor.sitAmet);
-            consectetur(adipiscing).elit();
-            sed.do.eiusmod = tempor.incididunt.ut(labore) && dolore(magna.aliqua);
-            ut.enim.ad(minim.veniam);
-            result = someFinalComputation();
-        }
+        lorem.ipsum(dolor.sitAmet);
+        consectetur(adipiscing).elit();
+        sed.do.eiusmod = tempor.incididunt.ut(labore) && dolore(magna.aliqua);
+        ut.enim.ad(minim.veniam);
+        result = someFinalComputation();
     }
     return result;
 }
