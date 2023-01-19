aa872ad0709bef3359452c5524fd64ea4d792183
Clustern <homerun372@gmail.com>
2023-01-19 23:22:27 +0900

    refactor: 함수들을 Rating 클래스로 변경 및 rating 함수에 적용

--- src/ch10/10_4replaceConditionalWithPloymorphism-2.js
+++ src/ch10/10_4replaceConditionalWithPloymorphism-2.js
@@ -1,44 +1,55 @@
 export function rating(voyage, history) {
-    const vpf = voyageProfitFactor(voyage, history);
-    const vr = voyageRisk(voyage);
-    const chr = captainHistoryRisk(voyage, history);
-    if (vpf * 3 > (vr + chr * 2)) return "A";
-    else return "B";
+    return new Rating(voyage, history).value;
 }
 
-function voyageRisk(voyage) {
-    let result = 1;
-    if (voyage.length > 4) result += 2;
-    if (voyage.length > 8) result += voyage.length - 8
-    if (["중국", "동인도"].includes(voyage.zone)) result += 4;
-    return Math.max(result, 0);
-}
-
-function captainHistoryRisk(voyage, history) {
-    let result = 1;
-    if (history.length < 5) result += 4;
-    result += history.filter(v => v.profit < 0).length;
-    if (voyage.zone === "중국" && hasChina(history)) result -= 2;
-    return Math.max(result, 0);
-}
-
-function hasChina(history) {
-    return history.some(v => "중국" === v.zone);
-}
-
-function voyageProfitFactor(voyage, history) {
-    let result = 2;
-    if (voyage.zone === "중국") result += 1;
-    if (voyage.zone === "동인도") result += 1;
-    if (voyage.zone === "중국" && hasChina(history)) {
-        result += 3;
-        if (history.length > 10) result += 1;
-        if (voyage.length > 12) result += 1;
-        if (voyage.length > 18) result -= 1;
+class Rating {
+    constructor(voyage, history) {
+        this._voyage = voyage;
+        this._history = history;
     }
-    else {
-        if (history.length > 8) result += 1;
-        if (voyage.length > 14) result -= 1;
+
+    get value() {
+        const vpf = this.voyageProfitFactor;
+        const vr = this.voyageRisk;
+        const chr = this.captainHistoryRisk;
+        if (vpf * 3 > (vr + chr * 2)) return "A";
+        else return "B";
     }
-    return result;
+
+    get voyageRisk() {
+        let result = 1;
+        if (this._voyage.length > 4) result += 2;
+        if (this._voyage.length > 8) result += this._voyage.length - 8
+        if (["중국", "동인도"].includes(this._voyage.zone)) result += 4;
+        return Math.max(result, 0);
+    }
+
+    get captainHistoryRisk() {
+        let result = 1;
+        if (this._history.length < 5) result += 4;
+        result += this._history.filter(v => v.profit < 0).length;
+        if (this._voyage.zone === "중국" && this.hasChina) result -= 2;
+        return Math.max(result, 0);
+    }
+
+    get hasChina() {
+        return this._history.some(v => "중국" === v.zone);
+    }
+
+    get voyageProfitFactor() {
+        let result = 2;
+        if (this._voyage.zone === "중국") result += 1;
+        if (this._voyage.zone === "동인도") result += 1;
+        if (this._voyage.zone === "중국" && this.hasChina) {
+            result += 3;
+            if (this._history.length > 10) result += 1;
+            if (this._voyage.length > 12) result += 1;
+            if (this._voyage.length > 18) result -= 1;
+        }
+        else {
+            if (this._history.length > 8) result += 1;
+            if (this._voyage.length > 14) result -= 1;
+        }
+        return result;
+    }
 }
