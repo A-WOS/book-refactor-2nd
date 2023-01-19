7557fce0547aa8afc9f3e04596428bcd524d9432
Clustern <homerun372@gmail.com>
2023-01-19 22:45:45 +0900

    refactor: 각 케이스문에 대한 plumage 게터 결과를 각 서브 클래스로 오버라이드

--- src/ch10/10_4replaceConditionalWithPloymorphism-1.js
+++ src/ch10/10_4replaceConditionalWithPloymorphism-1.js
@@ -4,16 +4,7 @@ class Bird {
     }
 
     get plumage() {
-        switch (this.type) {
-            case '유럽 제비':
-                throw "오류 발생";
-            case '아프리카 제비':
-                return (this.numberOfCoconuts > 2) ? "지쳤다" : "보통이다";
-            case '노르웨이 파랑 앵무':
-                return (this.voltage > 100) ? "그을렸다" : "예쁘다";
-            default:
-                return "알 수 없다";
-        }
+        return "알 수 없다";
     }
 
     get airSpeedVelocity() {
@@ -67,9 +58,13 @@ class EuropeanSwallow extends Bird {
 }
 
 class AfricanSwallow extends Bird {
-
+    get plumage() {
+        return (this.numberOfCoconuts > 2) ? "지쳤다" : "보통이다";
+    }
 }
 
 class NorwegianBlueParrot extends Bird {
-
+    get plumage() {
+        return (this.voltage > 100) ? "그을렸다" : "예쁘다";
+    }
 }
