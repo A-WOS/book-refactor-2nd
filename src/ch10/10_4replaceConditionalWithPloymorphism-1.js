bdc657e9b205178b7416b80e9dbd852a49899e1b
Clustern <homerun372@gmail.com>
2023-01-19 22:34:37 +0900

    refactor: 동일 케이스로 각각 다르게 동작하는 함수를 클래스로 묶음

--- src/ch10/10_4replaceConditionalWithPloymorphism-1.js
+++ src/ch10/10_4replaceConditionalWithPloymorphism-1.js
@@ -1,3 +1,36 @@
+class Bird {
+    constructor(birdObject) {
+        Object.assign(this, birdObject);
+    }
+
+    get plumage() {
+        switch (this.type) {
+            case '유럽 제비':
+                return "보통이다";
+            case '아프리카 제비':
+                return (this.numberOfCoconuts > 2) ? "지쳤다" : "보통이다";
+            case '노르웨이 파랑 앵무':
+                return (this.voltage > 100) ? "그을렸다" : "예쁘다";
+            default:
+                return "알 수 없다";
+        }
+    }
+
+    get airSpeedVelocity() {
+        switch (this.type) {
+            case '유럽 제비':
+                return 35;
+            case '아프리카 제비':
+                return 40 - 2 * this.numberOfCoconuts;
+            case '노르웨이 파랑 앵무':
+                return (this.isNailed) ? 0 : 10 + this.voltage / 10;
+            default:
+                return null;
+        }
+    }
+}
+
+
 function plumages(birds) {
     return new Map(birds.map(b => [b.name, plumage(b)]));
 }
@@ -7,27 +40,9 @@ function speeds(birds) {
 }
 
 function plumage(bird) {
-    switch (bird.type) {
-        case '유럽 제비':
-            return "보통이다";
-        case '아프리카 제비':
-            return (bird.numberOfCoconuts > 2) ? "지쳤다" : "보통이다";
-        case '노르웨이 파랑 앵무':
-            return (bird.voltage > 100) ? "그을렸다" : "예쁘다";
-        default:
-            return "알 수 없다";
-    }
+    return new Bird(bird).plumage;
 }
 
 function airSpeedVelocity(bird) {
-    switch (bird.type) {
-        case '유럽 제비':
-            return 35;
-        case '아프리카 제비':
-            return 40 - 2 * bird.numberOfCoconuts;
-        case '노르웨이 파랑 앵무':
-            return (bird.isNailed) ? 0 : 10 + bird.voltage / 10;
-        default:
-            return null;
-    }
+    return new Bird(bird).airSpeedVelocity;
 }
