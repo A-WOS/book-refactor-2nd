d87f1bb291506b4e92022244830394f275ecc7a3
Clustern <homerun372@gmail.com>
2023-01-19 21:53:01 +0900

    refactor: 반환하는 결과가 같은 로직을 or 을 사용하여 하나로 통합

--- src/ch10/10_2consoildateConditionalExpression-1.js
+++ src/ch10/10_2consoildateConditionalExpression-1.js
@@ -1,5 +1,4 @@
 function disablilityAmount(anEmployee) {
-    if (anEmployee.seniority < 2) return 0;
-    if (anEmployee.monthDisabled > 12) return 0;
-    if (anEmployee.isPartTime) return 0;
+    if ((anEmployee.seniority < 2) || (anEmployee.monthDisabled > 12) || (anEmployee.isPartTime))
+        return 0;
 }
