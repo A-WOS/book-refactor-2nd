3b7438feb7118f5b1161c489b56d2d22cb4af171
Clustern <homerun372@gmail.com>
2023-01-19 21:58:32 +0900

    refactor: 중첩 if 문의 조건 로직을 and 를 사용하여 하나로 통합

--- src/ch10/10_2consoildateConditionalExpression-2.js
+++ src/ch10/10_2consoildateConditionalExpression-2.js
@@ -1,6 +1,5 @@
 function empVaction(anEmployee) {
-    if (anEmployee.onVacation)
-        if (anEmployee.seniority > 10)
-            return 1;
+    if ((anEmployee.onVacation) && (anEmployee.seniority > 10))
+        return 1;
     return 0.5;
 }
