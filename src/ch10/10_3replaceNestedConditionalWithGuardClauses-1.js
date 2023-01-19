ea54e0b5f74551f5fea046566d65975f0492d661
Clustern <homerun372@gmail.com>
2023-01-19 22:07:54 +0900

    refactor: 리팩터링으로 불요해진 result 변수 인라인

--- src/ch10/10_3replaceNestedConditionalWithGuardClauses-1.js
+++ src/ch10/10_3replaceNestedConditionalWithGuardClauses-1.js
@@ -1,11 +1,9 @@
 function payAmount(employee) {
-    let result;
     if(employee.isSeparated) return {amount: 0, reasonCode: "SEP"};
     if (employee.isRetired) return {amount: 0, reasonCode: "RET"};
     lorem.ipsum(dolor.sitAmet);
     consectetur(adipiscing).elit();
     sed.do.eiusmod = tempor.incididunt.ut(labore) && dolore(magna.aliqua);
     ut.enim.ad(minim.veniam);
-    result = someFinalComputation();
-    return result;
+    return someFinalComputation();
 }
