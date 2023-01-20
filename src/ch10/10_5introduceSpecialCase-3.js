29bbf228c1ac22c4c11d12d5e99c8ef27d0f55b8
Clustern <homerun372@gmail.com>
2023-01-20 09:10:23 +0900

    feat: 10장 5절 예제 3번 코드 추가

---
+++ src/ch10/10_5introduceSpecialCase-3.js
@@ -0,0 +1,34 @@
+const a = {
+    name: "에크미 보스턴",
+    location: "Malden MA",
+    customer: {
+        name: "애크미 산업",
+        billingPlan: "Plan-451",
+        paymentHistory: {
+            weeksDelinquentInLastYear: 7
+        }
+    }
+}
+
+const b = {
+    name: "물류창고 15",
+    location: "Malden MA",
+    customer: "미확인 고객"
+}
+
+// 클라이언트1
+const site = acquireSiteData();
+const aCustomer = site.customer;
+let customerName;
+if (aCustomer === "미확인 고객") customerName = "거주자";
+else customerName = aCustomer.name;
+
+// 클라이언트2
+const Plan = (aCustomer === "미확인 고객") ?
+    registry.billingPlans.basic
+    : aCustomer.billingPlan;
+
+// 클라이언트3
+const weekDelinquent =  (aCustomer === "미확인 고객") ?
+    0
+    : aCustomer.paymentHistory.weekDelinquentInLastYear;
