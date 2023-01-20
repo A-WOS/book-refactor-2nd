a9d1d08d16e2c693c49d399179ea799cd122e517
Clustern <homerun372@gmail.com>
2023-01-20 10:09:04 +0900

    refactor: 리팩터링 결과로 불요해진 제어 플래그 제거

--- src/ch10/10_7replaceControlFlagWithBreak-1.js
+++ src/ch10/10_7replaceControlFlagWithBreak-1.js
@@ -1,17 +1,14 @@
 checkForMiscreants(people)
 
 function checkForMiscreants(people) {
-    let found = false;
     for (const p of people) {
-        if (!found) {
-            if (p === "조커") {
-                sendAlert();
-                return;
-            }
-            if (p === "사루만") {
-                sendAlert();
-                return;
-            }
+        if (p === "조커") {
+            sendAlert();
+            return;
+        }
+        if (p === "사루만") {
+            sendAlert();
+            return;
         }
     }
 }
