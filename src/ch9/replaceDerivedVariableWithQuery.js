232e878d968146cdb55c080b144365ae62ddaf7a
Bell <bell04204@gmail.com>
2023-01-28 01:13:52 +0900

    feat: 보기 흉한 ProductionPlan 클래스 추가

---
+++ src/ch9/replaceDerivedVariableWithQuery.js
@@ -0,0 +1,10 @@
+class ProductionPlan {
+    get production() {
+        return this._production;
+    }
+
+    applyAdjustment(anAdjustment) {
+        this._adjustments.push(anAdjustment);
+        this._production += anAdjustment.amount;
+    }
+}
