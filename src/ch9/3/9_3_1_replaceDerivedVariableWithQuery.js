c47995ee31f55d3aebeefd150388a3835c3f2b0c
AWOS <uws0331@gmail.com>
2023-01-28 07:20:08 +0900

    feat: 어서션을 추가

--- src/ch9/3/9_3_1_replaceDerivedVariableWithQuery.js
+++ src/ch9/3/9_3_1_replaceDerivedVariableWithQuery.js
@@ -1,12 +1,19 @@
 class ProductionPlan {
 
     constructor(production) {
-        this._production = production;
+        this._initialProduction = production;
+        this._productionAccumulator = 0;
         this._adjustments = [];
     }
-    get production() { return this._production; }
+    get production() { return this._initialProduction + this._production; }
     applyAdjustment(anAdjustment) {
         this._adjustments.push(anAdjustment);
-        this._production += anAdjustment.amount;
+        assert(this._productionAccumulator === this.calculatedProductionAccumulator);
+        return this._initialProduction + this._productionAccumulator;
+    }
+
+    get calculatedProductionAccumulator() {
+        return this._adjustments
+            .reduce((sum, a) => sum + a.amount, 0);
     }
 }
