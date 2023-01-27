ac98f24d9a90a5d8cd4ffb72508e0b4bb692e925
Tetrahedrite <det3652000@naver.com>
2023-01-28 02:02:11 +0900

    refactor: add assertion to test changes

--- src/ch9/3_replace_derived_varaible_with_query.js
+++ src/ch9/3_replace_derived_varaible_with_query.js
@@ -1,5 +1,14 @@
 class ProductionPlan {
-    get production() {return this._production;}
+    get production() {
+        assert(this._production === this.calculatedProduction)
+        return this._production;
+    }
+
+    get calculatedProduction() {
+        return this._adjustments
+            .reduce((sum, a) => sum + a.amount, 0);
+    }
+
     applyAdjustment(anAdjustment) {
         this._adjustments.push(anAdjustment);
         this._production += anAdjustment.amount;
