780a043285115971190bfad3b7ef2b39c59fe0de
Tetrahedrite <det3652000@naver.com>
2023-01-28 01:38:12 +0900

    refactor: split inputValue to inputValue, originalInputValue

--- src/ch9/1_split_variable_2.js
+++ src/ch9/1_split_variable_2.js
@@ -1,4 +1,5 @@
-function discount(inputValue, quantity) {
+function discount(originalInputValue, quantity) {
+    let inputValue = originalInputValue;
     if (inputValue > 50) inputValue = inputValue - 2;
     if (quantity > 100) inputValue = inputValue - 1;
     return inputValue;
