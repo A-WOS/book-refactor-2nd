9c61684717d2dcfdbfc2a40b41d26c59b9dd843c
Tetrahedrite <det3652000@naver.com>
2023-01-28 01:51:54 +0900

    refactor: rename variable name to title

--- src/ch9/2_rename_field.js
+++ src/ch9/2_rename_field.js
@@ -1,13 +1,13 @@
 class Organization {
     constructor(data) {
-        this._name = data.name;
+        this._title = (data.title !== undefined) ? data.title : data.name;
         this._country = data.country;
     }
 
-    get name() {return this._name;}
-    set name(aString) {this._name = aString;}
+    get title() {return this._title;}
+    set title(aString) {this._title = aString;}
     get country() {return this._country;}
     set country(aCountryCode) {this._country = aCountryCode;}
 }
 
-const organization = new Organization({name: "애크미 구스베리", country: "GB"});
+const organization = new Organization({title: "애크미 구스베리", country: "GB"});
