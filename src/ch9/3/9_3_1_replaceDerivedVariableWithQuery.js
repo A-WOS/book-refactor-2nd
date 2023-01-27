dceb5340c488f93e831871a087f64f3080e97529
AWOS <uws0331@gmail.com>
2023-01-27 23:52:03 +0900

    refactor: calculatedProduction 메서드를 인라인화

--- src/ch9/3/9_3_1_replaceDerivedVariableWithQuery.js
+++ src/ch9/3/9_3_1_replaceDerivedVariableWithQuery.js
@@ -1,14 +1,10 @@
 class ProductionPlan {
     get production() {
-        return this.calculatedProduction;
+        return this._adjustments
+            .reduce((sum, a) => sum + a.amount, 0)
     }
     applyAdjustment(anAdjustment) {
         this._adjustments.push(anAdjustment);
         this._production += anAdjustment.amount;
-    }
-
-    get calculatedProduction() {
-        return this._adjustments
-            .reduce((sum, a) => sum + a.amount, 0)
     }
 }
