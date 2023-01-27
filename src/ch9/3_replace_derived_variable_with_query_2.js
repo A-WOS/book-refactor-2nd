7f05027021a5b7069db6782fd63b52ddf5de2cf4
Tetrahedrite <det3652000@naver.com>
2023-01-28 02:11:35 +0900

    feat: Chapter 9.3 파생 변수를 질의 함수로 바꾸기 예시 2 기본 코드

---
+++ src/ch9/3_replace_derived_variable_with_query_2.js
@@ -0,0 +1,12 @@
+class ProductionPlan {
+    constructor(production) {
+        this._production = production;
+        this._adjustments = [];
+    }
+
+    get production() {return this._production;}
+    applyAdjustment(anAdjustment) {
+        this._adjustments.push(anAdjustment);
+        this._production += anAdjustment.amount;
+    }
+}
