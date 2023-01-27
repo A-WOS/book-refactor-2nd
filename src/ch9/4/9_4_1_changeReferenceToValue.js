005de99d25b290e753d2625691b856d9b216d086
AWOS <uws0331@gmail.com>
2023-01-28 07:26:04 +0900

    feat: 예제 코드 추가(9.4 참조를 값으로 바꾸기)

---
+++ src/ch9/4/9_4_1_changeReferenceToValue.js
@@ -0,0 +1,17 @@
+class Person {
+    constructor() {
+        this._telephoneNumber = new TelephoneNumber();
+    }
+
+    get officeAreaCode() { return this._telephoneNumber.areaCode; }
+    set officeAreaCode(arg) { this._telephoneNumber.areaCode = arg; }
+    get officeNumber() { return this._telephoneNumber.number; }
+    set officeNumber(arg) { this._telephoneNumber.number = arg; }
+}
+
+class TelephoneNumber {
+    get areaCode() { return this.areaCode; }
+    set areaCode(arg) { this.areaCode = arg; }
+    get number() { return this._number; }
+    set number(arg) { this._number = arg; }
+}
