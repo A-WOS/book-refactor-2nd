1d9ba73c94c71c4e5a806fa9b0162c1b16a7207c
Bell <bell04204@gmail.com>
2023-01-28 02:49:44 +0900

    feat: 조건 반대로 만들기 예시 추가

--- src/ch10/replaceNestedConditionalWithGuardClauses.js
+++ src/ch10/replaceNestedConditionalWithGuardClauses.js
@@ -2,11 +2,23 @@ function payAmount(employee) {
     if (employee.isSeparated) return {amount: 0, reasonCode: "SEP"};
 
     if (employee.isRetired) return {amount: 0, reasonCode: "RET"};
-    
+
     // 급여 계산 로직
     lorem.ipsum(dolor.sitAmet);
     consectetur(adipiscing).elit();
     sed.do.eiusmod = tempor.incididunt.ut(labore) && dolore(magna.aliqua);
     ut.enim.ad(minim.veniam);
     return someFinalComputation();
+}
+
+function adjustedCapital(anInstrument) {
+    let result = 0;
+
+    if (anInstrument.capital > 0) {
+        if (anInstrument.interestRate > 0 && anInstrument.duration > 0) {
+            result = (anInstrument.income / anInstrument.duration) * anInstrument.adjustmentFactor;
+        }
+    }
+    
+    return result;
 }
