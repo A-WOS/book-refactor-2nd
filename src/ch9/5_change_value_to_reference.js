92aab5256e11ccb7dac54c90e1e298e19ad3b523
Tetrahedrite <det3652000@naver.com>
2023-01-28 02:38:00 +0900

    refactor: add global repository to store customer object

--- src/ch9/5_change_value_to_reference.js
+++ src/ch9/5_change_value_to_reference.js
@@ -14,3 +14,20 @@ class Customer {
 
     get id() {return this._id;}
 }
+
+let _repositoryData;
+
+export function initialize() {
+    _repositoryData = {};
+    _repositoryData.customers = new Map();
+}
+
+export function registerCustomer(id) {
+    if (! _repositoryData.customers.has(id))
+        _repositoryData.customers.set(id, new Customer(id));
+    return findCustomer(id);
+}
+
+export function findCustomer(id) {
+    return _repositoryData.customers.get(id);
+}
