6359ff1bd136e33e83abfa7334176bcb81bc2a65
Bell <bell04204@gmail.com>
2023-01-28 01:30:40 +0900

    refactor: production method inline

--- src/ch9/replaceDerivedVariableWithQuery.js
+++ src/ch9/replaceDerivedVariableWithQuery.js
@@ -1,12 +1,7 @@
 class ProductionPlan {
     get production() {
-        return this.calculatedProduction;
-    }
-
-    get calculatedProduction() {
         return this._adjustments.reduce((sum, a) => sum + a.amount, 0);
     }
-
     applyAdjustment(anAdjustment) {
         this._adjustments.push(anAdjustment);
         this._production += anAdjustment.amount;
