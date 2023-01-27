aec757cbbd36e2e285640725539fa15d5a6e7cd7
Tetrahedrite <det3652000@naver.com>
2023-01-28 04:07:22 +0900

    refactor: add new class ExperiencedChinaRating, create factory method for rating class

--- src/ch10/4_replace_conditional_with_polymorphism_2.js
+++ src/ch10/4_replace_conditional_with_polymorphism_2.js
@@ -49,6 +49,16 @@ class Rating {
     }
 }
 
+class ExperiencedChinaRating extends Rating {
+
+}
+
+function createRating(voyage, history) {
+    if (voyage.zone === "중국" && history.some(v => "중국" === v.zone))
+        return new ExperiencedChinaRating(voyage, history);
+    else return new Rating(voyage, history);
+}
+
 function rating(voyage, history) {
     return new Rating(voyage, history).value;
 }
