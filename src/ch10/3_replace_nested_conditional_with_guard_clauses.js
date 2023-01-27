796ad05a723cfd433e9c40529adf4dfbe271ba9f
Tetrahedrite <det3652000@naver.com>
2023-01-28 03:09:31 +0900

    refactor: remove unused result variable and inlining

--- src/ch10/3_replace_nested_conditional_with_guard_clauses.js
+++ src/ch10/3_replace_nested_conditional_with_guard_clauses.js
@@ -1,5 +1,4 @@
 function payAmount(employee) {
-    let result;
     if (employee.isSeparated) return {amount: 0, reasonCode: "SEP"};
     if (employee.isRetired) return {amount: 0, reasonCode: "RET"};
     // 급여 계산 로직
@@ -7,6 +6,5 @@ function payAmount(employee) {
     consectetur(adipiscing).elit();
     sed.do.eiusmod = tempor.incididunt.ut(labore) && dolore(magna.aliqua);
     ut.enim.ad(minim.veniam);
-    result = someFinalComputation();
-    return result;
+    return someFinalComputation();
 }
