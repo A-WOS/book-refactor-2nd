78f2401bc81fbc78ebd25022646fe1f85127e828
Tetrahedrite <det3652000@naver.com>
2023-01-28 03:24:08 +0900

    refactor: change if statements to guard clauses, inlining result variable

--- src/ch10/3_replace_nested_conditional_with_guard_clauses_2.js
+++ src/ch10/3_replace_nested_conditional_with_guard_clauses_2.js
@@ -1,9 +1,6 @@
 function adjustedCapital(anInstrument) {
-    let result = 0;
-    if (anInstrument.capital > 0) {
-        if (anInstrument.interestRate > 0 && anInstrument.duration > 0) {
-            result = (anInstrument.income / anInstrument.duration) * anInstrument.adjustmentFactor;
-        }
-    }
-    return result;
+    if (anInstrument.capital <= 0
+        || anInstrument.interestRate <= 0
+        || anInstrument.duration <= 0) return 0;
+    return (anInstrument.income / anInstrument.duration) * anInstrument.adjustmentFactor;
 }
