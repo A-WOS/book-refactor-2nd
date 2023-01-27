27f5b7fdebf7c1a6f57da7beeeef4405e103cca0
Tetrahedrite <det3652000@naver.com>
2023-01-28 02:19:57 +0900

    refactor: remove assertion

--- src/ch9/3_replace_derived_variable_with_query_2.js
+++ src/ch9/3_replace_derived_variable_with_query_2.js
@@ -9,6 +9,11 @@ class ProductionPlan {
         return this._initialProduction + this._productionAccumulator;
     }
 
+    get calculatedProductionAccumulator() {
+        return this._adjustments
+            .reduce((sum, a) => sum + a.amount, 0);
+    }
+
     applyAdjustment(anAdjustment) {
         this._adjustments.push(anAdjustment);
         this._production += anAdjustment.amount;
