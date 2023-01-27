8af7d02f5c73d9978c5c48dfb91c297610c97f4a
Bell <bell04204@gmail.com>
2023-01-28 03:23:27 +0900

    refactor: plumage, airSpeedVelocity 로직을 새 종별 서브클래스에 위임

--- src/ch10/replaceConditionalWithPolymorphism.js
+++ src/ch10/replaceConditionalWithPolymorphism.js
@@ -20,29 +20,11 @@ class Bird {
     }
 
     get plumage() {
-        switch (this.type) {
-            case '유럽 제비':
-                return "보통이다";
-            case '아프리카 제비':
-                return (bird.numberOfCoconuts > 2) ? "지쳤다" : "보통이다";
-            case '노르웨이 파랑 앵무':
-                return (bird.voltage > 100) ? "그을렸다" : "예쁘다";
-            default:
-                return "알 수 없다";
-        }
+        return "알 수 없다";
     }
 
     get airSpeedVelocity() {
-        switch (this.type) {
-            case '유럽 제비':
-                return 35;
-            case '아프리카 제비':
-                return 40 - 2 * bird.numberOfCoconuts;
-            case '노르웨이 파랑 앵무':
-                return (bird.isNailed) ? 0 : 10 + bird.voltage / 10;
-            default:
-                return null;
-        }
+        return null;
     }
 }
 
@@ -60,13 +42,31 @@ function createBird(bird) {
 }
 
 class EuropeanSwallow extends Bird {
+    get plumage() {
+        return "보통이다";
+    }
 
+    get airSpeedVelocity() {
+        return 35;
+    }
 }
 
 class AfricanSwallow extends Bird {
+    get plumage() {
+        return (this.numberOfCoconuts > 2) ? "지쳤다" : "보통이다";
+    }
 
+    get airSpeedVelocity() {
+        return 40 - 2 * this.numberOfCoconuts;
+    }
 }
 
 class NorwegianBlueParrot extends Bird {
+    get plumage() {
+        return (this.voltage > 100) ? "그을렸다" : "예쁘다";
+    }
 
+    get airSpeedVelocity() {
+        return (this.isNailed) ? 0 : 10 + this.voltage / 10;
+    }
 }
