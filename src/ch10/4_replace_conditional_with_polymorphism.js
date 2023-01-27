903601a893a5b502796683df134e46c08a82b4a2
Tetrahedrite <det3652000@naver.com>
2023-01-28 03:38:36 +0900

    refactor: add Bird subclass and factory method to create bird

--- src/ch10/4_replace_conditional_with_polymorphism.js
+++ src/ch10/4_replace_conditional_with_polymorphism.js
@@ -1,33 +1,64 @@
-function plumages(birds) {
-    return new Map(birds.map(b => [b.name, plumage(b)]));
-}
+class Bird {
+    constructor(birdObject) {
+        Object.assign(this, birdObject);
+    }
 
-function speeds(birds) {
-    return new Map(birds.map(b => [b.name, airSpeedVelocity(b)]));
-}
+    get plumage() {
+        switch (this.type) {
+            case '유럽 제비':
+                return "보통이다";
+            case '아프리카 제비':
+                return (this.numberOfCoconuts > 2) ? "지쳤다" : "보통이다";
+            case '노르웨이 파랑 앵무':
+                return (this.voltage > 100) ? "그을렸다" : "예쁘다";
+            default:
+                return "알 수 없다";
+        }
+    }
 
-function plumage(bird) {
-    switch (bird.type) {
-        case '유럽 제비':
-            return "보통이다";
-        case '아프리카 제비':
-            return (bird.numberOfCoconuts > 2) ? "지쳤다" : "보통이다";
-        case '노르웨이 파랑 앵무':
-            return (bird.voltage > 100) ? "그을렸다" : "예쁘다";
-        default:
-            return "알 수 없다";
+    get airSpeedVelocity() {
+        switch (this.type) {
+            case '유럽 제비':
+                return 35;
+            case '아프리카 제비':
+                return 40 - 2 * this.numberOfCoconuts;
+            case '노르웨이 파랑 앵무':
+                return (this.isNailed) ? 0 : 10 + this.voltage / 10;
+            default:
+                return null;
+        }
     }
 }
 
-function airSpeedVelocity(bird) {
+function plumages(bird) {
+    return createBird(bird).plumage;
+}
+
+function speeds(bird) {
+    return createBird(bird).airSpeedVelocity;
+}
+
+function createBird(bird) {
     switch (bird.type) {
         case '유럽 제비':
-            return 35;
+            return new EuropeanSwallow(bird);
         case '아프리카 제비':
-            return 40 - 2 * bird.numberOfCoconuts;
+            return new AfricanSwallow(bird);
         case '노르웨이 파랑 앵무':
-            return (bird.isNailed) ? 0 : 10 + bird.voltage / 10;
+            return new NorwegianBlueParrot(bird);
         default:
             return null;
     }
+}
+
+class EuropeanSwallow extends Bird {
+
+}
+
+class AfricanSwallow extends Bird {
+
+}
+
+class NorwegianBlueParrot extends Bird {
+
 }
