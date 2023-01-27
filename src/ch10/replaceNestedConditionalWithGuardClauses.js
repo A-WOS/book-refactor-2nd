9010864f44ecbea214bed4d241a745f1fe6ab2b9
Bell <bell04204@gmail.com>
2023-01-28 02:50:44 +0900

    refactor: 보호 구문을 추가하면서 조건 반전

--- src/ch10/replaceNestedConditionalWithGuardClauses.js
+++ src/ch10/replaceNestedConditionalWithGuardClauses.js
@@ -14,11 +14,11 @@ function payAmount(employee) {
 function adjustedCapital(anInstrument) {
     let result = 0;
 
-    if (anInstrument.capital > 0) {
-        if (anInstrument.interestRate > 0 && anInstrument.duration > 0) {
-            result = (anInstrument.income / anInstrument.duration) * anInstrument.adjustmentFactor;
-        }
+    if (anInstrument.capital <= 0) return result;
+
+    if (anInstrument.interestRate > 0 && anInstrument.duration > 0) {
+        result = (anInstrument.income / anInstrument.duration) * anInstrument.adjustmentFactor;
     }
-    
+
     return result;
 }
