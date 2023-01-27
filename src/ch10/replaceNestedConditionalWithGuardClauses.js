9083482ef75261282d0042a81cef6f3e9244546a
Bell <bell04204@gmail.com>
2023-01-28 02:52:17 +0900

    refactor: 조건식 가독성 개선

--- src/ch10/replaceNestedConditionalWithGuardClauses.js
+++ src/ch10/replaceNestedConditionalWithGuardClauses.js
@@ -16,7 +16,7 @@ function adjustedCapital(anInstrument) {
 
     if (anInstrument.capital <= 0) return result;
 
-    if (!(anInstrument.interestRate > 0 && anInstrument.duration > 0)) return result;
+    if (anInstrument.interestRate <= 0 || anInstrument.duration <= 0) return result;
 
     result = (anInstrument.income / anInstrument.duration) * anInstrument.adjustmentFactor;
 
