d50ed4c2b3e0800e075f28edea1fe92ac9196659
Tetrahedrite <det3652000@naver.com>
2023-01-28 02:03:50 +0900

    refactor: inlining calculatedProduction to production

--- src/ch9/3_replace_derived_varaible_with_query.js
+++ src/ch9/3_replace_derived_varaible_with_query.js
@@ -1,13 +1,8 @@
 class ProductionPlan {
     get production() {
-        return this.calculatedProduction;
-    }
-
-    get calculatedProduction() {
         return this._adjustments
             .reduce((sum, a) => sum + a.amount, 0);
     }
-
     applyAdjustment(anAdjustment) {
         this._adjustments.push(anAdjustment);
         this._production += anAdjustment.amount;
