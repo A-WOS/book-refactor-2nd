d3bfd4a1bf05e4c7a5802c7c1a8668288d0a5ad1
Tetrahedrite <det3652000@naver.com>
2023-01-28 02:57:35 +0900

    feat: add new conditional expression with nested if statement

--- src/ch10/2_consolidate_conditional_expression.js
+++ src/ch10/2_consolidate_conditional_expression.js
@@ -1,6 +1,11 @@
 function disabilityAmount(anEmployee) {
     if (isNotEligibleForDisability()) return 0;
-    // 장애 수당 계산
+
+    if (anEmployee.onVacation)
+        if (anEmployee.seniority > 10)
+            return 1;
+
+    return 0.5;
 
     function isNotEligibleForDisability() {
         return (anEmployee.seniority < 2)
