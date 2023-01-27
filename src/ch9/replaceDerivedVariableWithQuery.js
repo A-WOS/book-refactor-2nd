98f8f6daa6e52aa6551e7229287298abe66fb7a4
Bell <bell04204@gmail.com>
2023-01-28 01:27:55 +0900

    refactor: assertion 추가

--- src/ch9/replaceDerivedVariableWithQuery.js
+++ src/ch9/replaceDerivedVariableWithQuery.js
@@ -1,6 +1,11 @@
 class ProductionPlan {
     get production() {
+        assert(this._production === this.calculatedProduction);
         return this._production;
+    }
+
+    get calculatedProduction() {
+        return this._adjustments.reduce((sum, a) => sum + a.amount, 0);
     }
 
     applyAdjustment(anAdjustment) {
