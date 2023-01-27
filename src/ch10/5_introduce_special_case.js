9af8fd0fd9232d0c336e12c330f9ed1de0649055
Tetrahedrite <det3652000@naver.com>
2023-01-28 04:31:37 +0900

    feat: Chapter 10.5 특이 케이스 추가하기 기본 코드

---
+++ src/ch10/5_introduce_special_case.js
@@ -0,0 +1,18 @@
+class Site {
+    get customer() {return this._customer;}
+}
+
+class Customer {
+    get name() {
+        // 고객 이름
+    }
+    get billingPlan() {
+        // 요금제
+    }
+    set billingPlan(arg) {
+        // 요금제
+    }
+    get paymentHistory() {
+        // 납부 이력
+    }
+}
