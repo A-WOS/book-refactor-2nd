346c84f0a7c45ac2b9357c6b38d4a93597e9f17b
Bell <bell04204@gmail.com>
2023-01-28 04:46:21 +0900

    refactor: 미확인 고객 확인 로직을 전용 함수를 사용하도록 개선

--- src/ch10/introduceSpecialCase.js
+++ src/ch10/introduceSpecialCase.js
@@ -42,15 +42,15 @@ function isUnknown(arg) {
 const aCustomer = site.customer;
 // ... 수 많은 코드 ...
 let customerName;
-if (aCustomer === "미확인 고객") customerName = "거주자";
+if (isUnknown(aCustomer)) customerName = "거주자";
 else customerName = aCustomer.name;
 
 // 클라이언트 2
-const plan = (aCustomer === "미확인 고객") ? registry.billingPlans.basic : aCustomer.billingPlan;
+const plan = (isUnknown(aCustomer)) ? registry.billingPlans.basic : aCustomer.billingPlan;
 
 // 클라이언트 3
 if (aCustomer !== "미확인 고객") aCustomer.billingPlan = newPlan;
 
 // 클라이언트 4
-const weeksDelinquent = (aCustomer === "미확인 고객") ? 0 : aCustomer.paymentHistory.weeksDelinquentInLastYear;
+const weeksDelinquent = (isUnknown(aCustomer)) ? 0 : aCustomer.paymentHistory.weeksDelinquentInLastYear;
 
