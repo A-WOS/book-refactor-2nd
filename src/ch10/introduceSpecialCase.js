2897af540ae6ac6f91495c5bdf9737041ea9644e
Bell <bell04204@gmail.com>
2023-01-28 04:39:46 +0900

    feat: 특이 케이스를 위한 기본 코드 추가

---
+++ src/ch10/introduceSpecialCase.js
@@ -0,0 +1,39 @@
+class Site {
+    get customer() {
+        return this._customer;
+    }
+}
+
+class Customer {
+    get name() {
+        return this._name;
+    }
+
+    get billingPlan() {
+        return this._billingPlan;
+    }
+
+    set billingPlan(arg) {
+        this._billingPlan = arg;
+    }
+
+    get paymentHistory() {
+        return this._paymentHistory;
+    }
+}
+
+// 클라이언트 1
+const aCustomer = site.customer;
+// ... 수 많은 코드 ...
+let customerName;
+if (aCustomer === "미확인 고객") customerName = "거주자";
+else customerName = aCustomer.name;
+
+// 클라이언트 2
+const plan = (aCustomer === "미확인 고객") ? registry.billingPlans.basic : aCustomer.billingPlan;
+
+// 클라이언트 3
+if (aCustomer !== "미확인 고객") aCustomer.billingPlan = newPlan;
+
+// 클라이언트 4
+const weeksDelinquent = (aCustomer === "미확인 고객") ? 0 : aCustomer.paymentHistory.weeksDelinquentInLastYear;
