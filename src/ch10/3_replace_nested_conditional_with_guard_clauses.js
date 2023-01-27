bc4b0f793dd01811b0953b8468e97df827e23c8c
Tetrahedrite <det3652000@naver.com>
2023-01-28 03:05:46 +0900

    feat: Chapter 10.3 중첩 조건문을 보호 구문으로 바꾸기 기본 코드

---
+++ src/ch10/3_replace_nested_conditional_with_guard_clauses.js
@@ -0,0 +1,20 @@
+function payAmount(employee) {
+    let result;
+    if (employee.isSeparated) { // 퇴사한 직원인가?
+        result = {amount: 0, reasonCode: "SEP"};
+    }
+    else {
+        if (employee.isRetired) { // 은퇴한 직원인가?
+            result = {amount: 0, reasonCode: "RET"};
+        }
+        else {
+            // 급여 계산 로직
+            lorem.ipsum(dolor.sitAmet);
+            consectetur(adipiscing).elit();
+            sed.do.eiusmod = tempor.incididunt.ut(labore) && dolore(magna.aliqua);
+            ut.enim.ad(minim.veniam);
+            result = someFinalComputation();
+        }
+    }
+    return result;
+}
