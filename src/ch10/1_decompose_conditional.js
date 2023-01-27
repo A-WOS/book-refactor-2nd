515e37b5fbc9d031bf9f9685ce8fd90ab88f86e0
Tetrahedrite <det3652000@naver.com>
2023-01-28 02:45:16 +0900

    feat: Chapter 10.1 조건문 분해하기 기본 코드

---
+++ src/ch10/1_decompose_conditional.js
@@ -0,0 +1,8 @@
+function calculatePrice() {
+    let charge;
+    if (!aDate.isBefore(plan.summerStart) && !aDate.isAfter(plan.summerEnd))
+        charge = quantity * plan.summerRate;
+    else
+        charge = quantity * plan.regularRate + plan.regularServiceCharge;
+    return charge;
+}
