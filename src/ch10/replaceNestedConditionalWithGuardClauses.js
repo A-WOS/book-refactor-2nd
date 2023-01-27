31cd07fc7f8b127eb44dbf6d31add9ea96f787b7
Bell <bell04204@gmail.com>
2023-01-28 02:42:45 +0900

    refactor: 하위 조건을 보호 구문으로 변경

--- src/ch10/replaceNestedConditionalWithGuardClauses.js
+++ src/ch10/replaceNestedConditionalWithGuardClauses.js
@@ -1,18 +1,12 @@
 function payAmount(employee) {
-    let result;
-
     if (employee.isSeparated) return {amount: 0, reasonCode: "SEP"};
 
-    if (employee.isRetired) { // 은퇴한 직원인가?
-        result = {amount: 0, reasonCode: "RET"};
-    } else {
-        // 급여 계산 로직
-        lorem.ipsum(dolor.sitAmet);
-        consectetur(adipiscing).elit();
-        sed.do.eiusmod = tempor.incididunt.ut(labore) && dolore(magna.aliqua);
-        ut.enim.ad(minim.veniam);
-        result = someFinalComputation();
-    }
-
-    return result;
+    if (employee.isRetired) return {amount: 0, reasonCode: "RET"};
+    
+    // 급여 계산 로직
+    lorem.ipsum(dolor.sitAmet);
+    consectetur(adipiscing).elit();
+    sed.do.eiusmod = tempor.incididunt.ut(labore) && dolore(magna.aliqua);
+    ut.enim.ad(minim.veniam);
+    return someFinalComputation();
 }
