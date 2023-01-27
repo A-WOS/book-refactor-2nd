10dbe506ed47baff97483e8280a6ace8842285c1
Bell <bell04204@gmail.com>
2023-01-28 02:53:24 +0900

    refactor: 불필요한 가변 변수 제거

--- src/ch10/replaceNestedConditionalWithGuardClauses.js
+++ src/ch10/replaceNestedConditionalWithGuardClauses.js
@@ -12,11 +12,7 @@ function payAmount(employee) {
 }
 
 function adjustedCapital(anInstrument) {
-    let result = 0;
+    if (anInstrument.capital <= 0 || anInstrument.interestRate <= 0 || anInstrument.duration <= 0) return 0;
 
-    if (anInstrument.capital <= 0 || anInstrument.interestRate <= 0 || anInstrument.duration <= 0) return result;
-
-    result = (anInstrument.income / anInstrument.duration) * anInstrument.adjustmentFactor;
-
-    return result;
+    return (anInstrument.income / anInstrument.duration) * anInstrument.adjustmentFactor;
 }
