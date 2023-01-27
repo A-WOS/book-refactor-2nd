dd56bd68250a49cbcf54b4bead222ffb292a0a6e
Bell <bell04204@gmail.com>
2023-01-28 04:08:26 +0900

    refactor: 문장을 호출한 곳으로 이동

--- src/ch10/replaceConditionalWithPolymorphism2.js
+++ src/ch10/replaceConditionalWithPolymorphism2.js
@@ -51,15 +51,15 @@ class Rating {
 
         if (this.voyage.zone === "중국") result += 1;
         if (this.voyage.zone === "동인도") result += 1;
-        result += this.voyageAndHistoryLengthFactor;
+        result += this.historyLengthFactor;
+        result += this.voyageLengthFactor;
 
         return result;
     }
 
-    get voyageAndHistoryLengthFactor() {
+    get voyageLengthFactor() {
         let result = 0;
 
-        result += this.historyLengthFactor;
         if (this.voyage.length > 14) result -= 1;
 
         return result;
@@ -77,12 +77,11 @@ class ExperiencedChinaRating extends Rating {
         return Math.max(result, 0);
     }
 
-    get voyageAndHistoryLengthFactor() {
+    get voyageLengthFactor() {
         let result = 0;
 
         result += 3;
 
-        result += this.historyLengthFactor;
         if (this.voyage.length > 12) result += 1;
         if (this.voyage.length > 18) result -= 1;
 
