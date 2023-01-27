17f64734e91ce5fc753cd4c14966356f4b1a9d21
AWOS <uws0331@gmail.com>
2023-01-27 23:47:11 +0900

    feat: assert 문 추가하여 테스트

--- src/ch9/3/9_3_1_replaceDerivedVariableWithQuery.js
+++ src/ch9/3/9_3_1_replaceDerivedVariableWithQuery.js
@@ -1,7 +1,15 @@
 class ProductionPlan {
-    get production() {return this._production;}
+    get production() {
+        assert(this._production === this.calculatedProduction);
+        return this._production;
+    }
     applyAdjustment(anAdjustment) {
         this._adjustments.push(anAdjustment);
         this._production += anAdjustment.amount;
+    }
+
+    get calculatedProduction() {
+        return this._adjustments
+            .reduce((sum, a) => sum + a.amount, 0)
     }
 }
