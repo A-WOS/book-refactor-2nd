6b20cce6271c8d8cf6241bf8ad569e40e128a49e
Bell <bell04204@gmail.com>
2023-01-28 03:09:04 +0900

    refactor: plumage, airSpeedVelocity 함수를 Bird 클래스로 이동

--- src/ch10/replaceConditionalWithPolymorphism.js
+++ src/ch10/replaceConditionalWithPolymorphism.js
@@ -7,27 +7,41 @@ function speed(birds) {
 }
 
 function plumage(bird) {
-    switch (bird.type) {
-        case '유럽 제비':
-            return "보통이다";
-        case '아프리카 제비':
-            return (bird.numberOfCoconuts > 2) ? "지쳤다" : "보통이다";
-        case '노르웨이 파랑 앵무':
-            return (bird.voltage > 100) ? "그을렸다" : "예쁘다";
-        default:
-            return "알 수 없다";
-    }
+    return new Bird(bird).plumage;
 }
 
 function airSpeedVelocity(bird) {
-    switch (bird.type) {
-        case '유럽 제비':
-            return 35;
-        case '아프리카 제비':
-            return 40 - 2 * bird.numberOfCoconuts;
-        case '노르웨이 파랑 앵무':
-            return (bird.isNailed) ? 0 : 10 + bird.voltage / 10;
-        default:
-            return null;
+    return new Bird(bird).airSpeedVelocity;
+}
+
+class Bird {
+    constructor(birdObject) {
+        Object.assign(this, birdObject);
+    }
+
+    get plumage() {
+        switch (this.type) {
+            case '유럽 제비':
+                return "보통이다";
+            case '아프리카 제비':
+                return (bird.numberOfCoconuts > 2) ? "지쳤다" : "보통이다";
+            case '노르웨이 파랑 앵무':
+                return (bird.voltage > 100) ? "그을렸다" : "예쁘다";
+            default:
+                return "알 수 없다";
+        }
+    }
+
+    get airSpeedVelocity() {
+        switch (this.type) {
+            case '유럽 제비':
+                return 35;
+            case '아프리카 제비':
+                return 40 - 2 * bird.numberOfCoconuts;
+            case '노르웨이 파랑 앵무':
+                return (bird.isNailed) ? 0 : 10 + bird.voltage / 10;
+            default:
+                return null;
+        }
     }
 }
