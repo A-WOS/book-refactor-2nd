36e286622e0faa01ca9f4e776976197654423be6
AWOS <uws0331@gmail.com>
2023-01-28 07:11:14 +0900

    feat: 예제 코드 추가

--- src/ch9/3/9_3_1_replaceDerivedVariableWithQuery.js
+++ src/ch9/3/9_3_1_replaceDerivedVariableWithQuery.js
@@ -1,9 +1,12 @@
 class ProductionPlan {
-    get production() {
-        return this._adjustments
-            .reduce((sum, a) => sum + a.amount, 0)
+
+    constructor(production) {
+        this._production = production;
+        this._adjustments = [];
     }
+    get production() { return this._production; }
     applyAdjustment(anAdjustment) {
         this._adjustments.push(anAdjustment);
+        this._production += anAdjustment.amount;
     }
 }
