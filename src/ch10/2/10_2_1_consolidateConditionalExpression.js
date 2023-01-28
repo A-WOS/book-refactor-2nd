3b0a52c1b394dbe11d1acd4a74d7437cb0bd953c
AWOS <uws0331@gmail.com>
2023-01-28 09:27:03 +0900

    feat: 예제 코드 추가(10.2 조건식 통합하기 - and 사용하기)

--- src/ch10/2/10_2_1_consolidateConditionalExpression.js
+++ src/ch10/2/10_2_1_consolidateConditionalExpression.js
@@ -9,4 +9,9 @@ function disabilityAmount(anEmployee) {
         || (anEmployee.isPartTime));
     }
 
+    if (anEmployee.onVacation)
+        if (anEmployee.seniority > 10)
+            return 1;
+    return 0.5;
+
 }
