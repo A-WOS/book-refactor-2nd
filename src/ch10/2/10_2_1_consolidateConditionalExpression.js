aa1061d886c0ae7330a84fb1fd484e98f56198b8
AWOS <uws0331@gmail.com>
2023-01-28 09:28:18 +0900

    refactor: and 를 사용한 if 문 코드 수정

--- src/ch10/2/10_2_1_consolidateConditionalExpression.js
+++ src/ch10/2/10_2_1_consolidateConditionalExpression.js
@@ -9,9 +9,7 @@ function disabilityAmount(anEmployee) {
         || (anEmployee.isPartTime));
     }
 
-    if (anEmployee.onVacation)
-        if (anEmployee.seniority > 10)
-            return 1;
+    if ((anEmployee.onVacation)
+        && (anEmployee.seniority > 10)) return 1;
     return 0.5;
-
 }
