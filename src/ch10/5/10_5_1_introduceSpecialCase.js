632d945fdea33913396d6d1481493e7f5f2d5622
AWOS <uws0331@gmail.com>
2023-01-28 18:28:39 +0900

    refactor: 특이 케이스일 때 Site 클래스가 UnknownCustomer 객체를 반환하도록 수정

--- src/ch10/5/10_5_1_introduceSpecialCase.js
+++ src/ch10/5/10_5_1_introduceSpecialCase.js
@@ -1,5 +1,5 @@
 class Site {
-    get customer() { return this._customer; }
+    get customer() { return (this._customer === "미확인 고객") ? new UnknownCustomer() : this._customer; }
 }
 
 class Customer {
