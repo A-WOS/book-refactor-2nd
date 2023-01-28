f2030bfea0f516e45ba2b51b18b681de08276bb4
AWOS <uws0331@gmail.com>
2023-01-28 18:15:49 +0900

    feat: 예제 코드 추가(10.5 특이 케이스 추가하기)

---
+++ src/ch10/5/10_5_1_introduceSpecialCase.js
@@ -0,0 +1,10 @@
+class Site {
+    get customer() { return this._customer; }
+}
+
+class Customer {
+    get name() { return this._name; } // 고객 이름
+    get billingPlan() { return this._billingPlan;} // 요금제
+    set billingPlan(arg) { return this._billingPlan = arg; }
+    get paymentHistory() { return this._paymentHistory; } // 납부 이력
+}
