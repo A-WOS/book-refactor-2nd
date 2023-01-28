83561a735bf693c89179f362b3c7b5d61f3e6728
AWOS <uws0331@gmail.com>
2023-01-28 19:03:50 +0900

    refactor: NullPaymentHistory 클래스 생성 및 생성한 클래스 적용

--- src/ch10/5/10_5_1_introduceSpecialCase.js
+++ src/ch10/5/10_5_1_introduceSpecialCase.js
@@ -1,20 +1,54 @@
 class Site {
-    get customer() { return (this._customer === "미확인 고객") ? new UnknownCustomer() : this._customer; }
+    get customer() {
+        return (this._customer === "미확인 고객") ? new UnknownCustomer() : this._customer;
+    }
 }
 
 class Customer {
-    get name() { return this._name; } // 고객 이름
-    get billingPlan() { return this._billingPlan;} // 요금제
-    set billingPlan(arg) { return this._billingPlan = arg; }
-    get paymentHistory() { return this._paymentHistory; } // 납부 이력
-    get isUnknown() { return false; }
+    get name() {
+        return this._name;
+    } // 고객 이름
+    get billingPlan() {
+        return this._billingPlan;
+    } // 요금제
+    set billingPlan(arg) {
+        return this._billingPlan = arg;
+    }
+
+    get paymentHistory() {
+        return this._paymentHistory;
+    } // 납부 이력
+    get isUnknown() {
+        return false;
+    }
 }
 
 class UnknownCustomer {
-    get isUnknown() { return this.true; }
-    get name() { return "거주자"; }
-    get billingPlan() { return registry.billingPlans.basic; }
-    set billingPlan(arg) { /* 무시한다. */ }
+    get isUnknown() {
+        return this.true;
+    }
+
+    get name() {
+        return "거주자";
+    }
+
+    get billingPlan() {
+        return registry.billingPlans.basic;
+    }
+
+    set billingPlan(arg) { /* 무시한다. */
+    }
+
+    get paymentHistory() {
+        return new NullPaymentHistory();
+    }
+}
+
+class NullPaymentHistory {
+    get weeksDelinquentInLastYear() {
+        return 0;
+    }
+
 }
 
 function isUnknown(arg) {
@@ -37,7 +71,5 @@ function client3Code(aCustomer, newPlan) {
 }
 
 function client4Code() {
-    const weeksDelinquent = (isUnknown(aCustomer)) ?
-        0
-        : aCustomer.paymentHistory.weeksDelinquentInLastYear;
+    const weeksDelinquent = aCustomer.paymentHistory.weeksDelinquentInLastYear;
 }
