2b7432f23ec86767403c72e166c6405823f6c536
AWOS <uws0331@gmail.com>
2023-01-28 17:59:32 +0900

    refactor: 두 가지 동작을 하는 함수를 두 개의 함수로 추출(서브 클래스)

--- src/ch10/4/10_4_2_replaceConditionalWithPolymorphism.js
+++ src/ch10/4/10_4_2_replaceConditionalWithPolymorphism.js
@@ -67,11 +67,15 @@ class ExperiencedChinaRating extends Rating {
     get voyageAndHistoryLengthFactor() {
         let result = 0;
         result += 3;
-        if (this.history.length > 10) result += 1;
+        result += this.historyLengthFactor;
         if (this.voyage.length > 12) result += 1;
         if (this.voyage.length > 18) result -= 1;
 
         return result;
+    }
+
+    get historyLengthFactor() {
+        return (this.history.length > 10) ? 1 : 0;
     }
 
 }
