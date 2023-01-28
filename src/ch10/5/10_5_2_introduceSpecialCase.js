2d2e0208d1177f92b5962686ae6330ba78994b8f
AWOS <uws0331@gmail.com>
2023-01-28 19:13:50 +0900

    feat: 예제 코드 추가(10.5 특이 케이스 추가하기 - 객체 리터럴 이용하기)

---
+++ src/ch10/5/10_5_2_introduceSpecialCase.js
@@ -0,0 +1,29 @@
+class Site {
+    get customer() {return this._customer;}
+}
+
+class Customer {
+    get name() {/**/}
+    get billingPlan() {/**/}
+    set billingPlan(arg) {/**/}
+    get paymentHistory() {/**/}
+}
+
+function client1Code() {
+    const aCustomer = site.customer;
+    let customer;
+    if (aCustomer === "미확인 고객") customerName = "거주자";
+    else customerName = aCustomer.name;
+}
+
+function client2Code() {
+    const plan = (aCustomer === "미확인 고객") ?
+        registry.billingPlans.basic
+        : aCustomer.billingPlan;
+}
+
+function client3Code() {
+    const weeksDelinquent = (aCustomer === "미확인 고객") ?
+        0
+        : aCustomer.paymentHistory.weeksDelinquentInLastYear;
+}
