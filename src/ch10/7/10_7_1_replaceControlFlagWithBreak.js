9a877823c88e09a539b1de38e03f0c6835581a91
AWOS <uws0331@gmail.com>
2023-01-28 21:26:36 +0900

    refactor: 제어 플래그를 참조하는 코드 제거

--- src/ch10/7/10_7_1_replaceControlFlagWithBreak.js
+++ src/ch10/7/10_7_1_replaceControlFlagWithBreak.js
@@ -1,20 +1,18 @@
 function findMiscreant() {
     // 생략 (중요하지 않은 코드)
     checkForMiscreants(people);
+
     // 생략
 
     function checkForMiscreants(people) {
-        let found = false;
         for (const p of people) {
-            if (!found) {
-                if (p === "조커") {
-                    sendAlert();
-                    return;
-                }
-                if (p === "사루만") {
-                    sendAlert();
-                    return;
-                }
+            if (p === "조커") {
+                sendAlert();
+                return;
+            }
+            if (p === "사루만") {
+                sendAlert();
+                return;
             }
         }
     }
