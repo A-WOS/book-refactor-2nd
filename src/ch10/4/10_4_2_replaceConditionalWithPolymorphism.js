c7d2a2838d08f5cf80071fb24b70fc869da10844
AWOS <uws0331@gmail.com>
2023-01-28 17:54:14 +0900

    refactor: 변형 동작을 서브 클래스로 추출

--- src/ch10/4/10_4_2_replaceConditionalWithPolymorphism.js
+++ src/ch10/4/10_4_2_replaceConditionalWithPolymorphism.js
@@ -44,15 +44,8 @@ class Rating { // 함수들을 Rating 클래스로 묶었다.
 
     get voyageAndHistoryLengthFactor() {
         let result = 0;
-        if (this.voyage.zone === "중국" && this.hasChinaHistory) {
-            result += 3;
-            if (this.history.length > 10) result += 1;
-            if (this.voyage.length > 12) result += 1;
-            if (this.voyage.length > 18) result -= 1;
-        } else {
-            if (this.history.length > 8) result += 1;
-            if (this.voyage.length > 14) result -= 1;
-        }
+        if (this.history.length > 8) result += 1;
+        if (this.voyage.length > 14) result -= 1;
         return result;
     }
 
@@ -65,6 +58,16 @@ class ExperiencedChinaRating extends Rating {
     get captainHistoryRisk() {
         const result = super.captainHistoryRisk - 2;
         return Math.max(result, 0);
+    }
+
+    get voyageAndHistoryLengthFactor() {
+        let result = 0;
+        result += 3;
+        if (this.history.length > 10) result += 1;
+        if (this.voyage.length > 12) result += 1;
+        if (this.voyage.length > 18) result -= 1;
+
+        return result;
     }
 
 }
