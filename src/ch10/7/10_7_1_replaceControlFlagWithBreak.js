7f787da242460b35b340cbbe34ba36a99614d73e
AWOS <uws0331@gmail.com>
2023-01-28 21:27:55 +0900

    refactor: 코드 간소화

--- src/ch10/7/10_7_1_replaceControlFlagWithBreak.js
+++ src/ch10/7/10_7_1_replaceControlFlagWithBreak.js
@@ -5,15 +5,6 @@ function findMiscreant() {
     // 생략
 
     function checkForMiscreants(people) {
-        for (const p of people) {
-            if (p === "조커") {
-                sendAlert();
-                return;
-            }
-            if (p === "사루만") {
-                sendAlert();
-                return;
-            }
-        }
+        if (people.some(p => ["조커", "사루만"].includes(p))) sendAlert();
     }
 }
