2b8ac36704168986b0a48bb158f0892ed54c338e
AWOS <uws0331@gmail.com>
2023-01-28 21:21:52 +0900

    refactor: 어서션을 세터 메서드에 추가

--- src/ch10/6/10_6_1_introduceAssertion.js
+++ src/ch10/6/10_6_1_introduceAssertion.js
@@ -1,9 +1,10 @@
 class Customer {
     applyDiscount(aNumber) {
-        if (!this.discountRate) return aNumber;
-        else {
-            assert(this.discountRate >= 0);
-            return aNumber - (this.discountRate * aNumber);
-        }
+        return aNumber - (this._discountRate * aNumber);
+    }
+
+    set discountRate(aNumber) {
+        assert(null === aNumber || aNumber >= 0);
+        this._discountRate = aNumber;
     }
 }
