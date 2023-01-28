9aacd5cf160581eeef5811b82d95399014dbe60b
AWOS <uws0331@gmail.com>
2023-01-28 09:23:03 +0900

    refactor: 최종 조건식을 함수로 추출

--- src/ch10/2/10_2_1_consolidateConditionalExpression.js
+++ src/ch10/2/10_2_1_consolidateConditionalExpression.js
@@ -1,7 +1,12 @@
 function disabilityAmount(anEmployee) {
-    if ((anEmployee.seniority < 2)
-        || (anEmployee.monthsDisabled > 12)
-        || (anEmployee.isPartTime))
-    return 0;
+
     // 장애 수단 계산
+    if (isNotEligibleForDisability()) return 0;
+
+    function isNotEligibleForDisability(){ // 장애 수당 적용 여부 확인
+        return ((anEmployee.seniority < 2)
+        || (anEmployee.monthsDisabled > 12)
+        || (anEmployee.isPartTime));
+    }
+
 }
