e46a325e49fe2f7e25d54fad6b4d689bde3531bf
AWOS <uws0331@gmail.com>
2023-01-28 21:20:05 +0900

    feat: applyDiscount 함수에 어서션 추가

--- src/ch10/6/10_6_1_introduceAssertion.js
+++ src/ch10/6/10_6_1_introduceAssertion.js
@@ -1,6 +1,9 @@
 class Customer {
     applyDiscount(aNumber) {
         if (!this.discountRate) return aNumber;
-        else return aNumber - (this.discountRate * aNumber);
+        else {
+            assert(this.discountRate >= 0);
+            return aNumber - (this.discountRate * aNumber);
+        }
     }
 }
