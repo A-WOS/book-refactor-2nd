04dfa04a5c7585a51976d8e13b8e1dc5e1ce9c3c
Clustern <homerun372@gmail.com>
2023-01-29 15:49:06 +0900

    refactor: rushDeliveryDate 함수의 조건문 거짓 부분을 regularDeliveryDate 함수로 추출

--- src/ch11/11_3RemoveFlagArgument-1.js
+++ src/ch11/11_3RemoveFlagArgument-1.js
@@ -1,13 +1,6 @@
 function deliveryDate(anOrder, isRush) {
-    if (isRush) {return rushDeliveryDate(anOrder);}
-    else {
-        let deliveryTime;
-        if (["MA", "CT", "NY"].includes(anOrder.deliveryState)) deliveryTime = 2;
-        else if (["ME", "NH"].includes(anOrder.deliveryState)) deliveryTime = 3;
-        else deliveryTime = 4;
-        return anOrder.placeOn.plusDays(2 + deliveryTime);
-
-    }
+    if (isRush) return rushDeliveryDate(anOrder);
+    else return regularDeliveryDate(anOrder);
 }
 
 function rushDeliveryDate(anOrder) {
@@ -16,6 +9,14 @@ function rushDeliveryDate(anOrder) {
     else if (["NY", "NH"].includes(anOrder.deliveryState)) deliveryTime = 2;
     else deliveryTime = 3;
     return anOrder.placeOn.plusDays(1 + deliveryTime);
+}
+
+function regularDeliveryDate(anOrder) {
+    let deliveryTime;
+    if (["MA", "CT", "NY"].includes(anOrder.deliveryState)) deliveryTime = 2;
+    else if (["ME", "NH"].includes(anOrder.deliveryState)) deliveryTime = 3;
+    else deliveryTime = 4;
+    return anOrder.placeOn.plusDays(2 + deliveryTime);
 }
 
 //호출부
