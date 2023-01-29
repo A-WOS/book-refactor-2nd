5aa6beb54947fd04d656ea6051949f77e7b7c4d6
Clustern <homerun372@gmail.com>
2023-01-29 14:16:28 +0900

    refacotr: bottomBand 함수 제거 및 withBand 함수 호출로 대체

--- src/ch11/11_2ParameterizeFunction-2.js
+++ src/ch11/11_2ParameterizeFunction-2.js
@@ -1,14 +1,10 @@
 function baseCharge(usage) {
     if (usage < 0) return usd(0);
     const amount =
-        bottomBand(usage) * 0.03
+        withBand(usage, 0, 100) * 0.03
         + withBand(usage, 100, 200) * 0.15
         + topBand(usage) * 0.07;
     return usd(amount);
-}
-
-function bottomBand(usage) {
-    return Math.min(usage, 100);
 }
 
 function withBand(usage, bottom, top) {
