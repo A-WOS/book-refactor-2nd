a21128c16ec3db94552105c14c111f02d38226c7
Clustern <homerun372@gmail.com>
2023-01-29 13:47:43 +0900

    refactor: alertForMiscreant 함수에서 질의 관련 코드 제거

--- src/ch11/11_1SeparateQueryFromModifier-1.js
+++ src/ch11/11_1SeparateQueryFromModifier-1.js
@@ -2,14 +2,13 @@ function alertForMiscreant(people) {
     for (const p of people) {
         if (p === "조커") {
             setOffAlarms();
-            return "조커";
+            return;
         }
         if (p === "사루만") {
             setOffAlarms();
-            return "사루만";
+            return;
         }
     }
-    return ""
 }
 
 function findMiscreant(people) {
