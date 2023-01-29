4f281cffaf3ebd90fa45fd33d7c75cfb2658c569
Clustern <homerun372@gmail.com>
2023-01-29 14:12:24 +0900

    refacotr: middleBand 함수 이름을 withBand 로 변경 및 매개변수 추가

--- src/ch11/11_2ParameterizeFunction-2.js
+++ src/ch11/11_2ParameterizeFunction-2.js
@@ -2,7 +2,7 @@ function baseCharge(usage) {
     if (usage < 0) return usd(0);
     const amount =
         bottomBand(usage) * 0.03
-        + middleBand(usage) * 0.15
+        + withBand(usage, 100, 200) * 0.15
         + topBand(usage) * 0.07;
     return usd(amount);
 }
@@ -11,8 +11,8 @@ function bottomBand(usage) {
     return Math.min(usage, 100);
 }
 
-function middleBand(usage) {
-    return usage > 100 ? Math.min(usage, 200) - 100 : 0;
+function withBand(usage, bottom, top) {
+    return usage > bottom ? Math.min(usage, top) - bottom : 0;
 }
 
 function topBand(usage) {
