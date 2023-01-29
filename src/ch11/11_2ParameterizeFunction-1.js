9ef9d04d67907079557013fd947f9fb48dde01c8
Clustern <homerun372@gmail.com>
2023-01-29 13:58:45 +0900

    feat: 11장 2절 예제 코드 추가

---
+++ src/ch11/11_2ParameterizeFunction-1.js
@@ -0,0 +1,7 @@
+function tenPercentRaise(aPerson) {
+    aPerson.slary = aPerson.salary.multiply(1.1);
+}
+
+function fivePercentRaise(aPerson) {
+    aPerson.slary = aPerson.salary.multiply(1.05);
+}
