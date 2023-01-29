82ccf26606f0c2834c8927985c1bcbae39f2e97c
Clustern <homerun372@gmail.com>
2023-01-29 15:56:52 +0900

    refactor: 리팩터링으로 불요해진 deliveryDate 함수 제거

--- src/ch11/11_3RemoveFlagArgument-1.js
+++ src/ch11/11_3RemoveFlagArgument-1.js
@@ -1,8 +1,3 @@
-function deliveryDate(anOrder, isRush) {
-    if (isRush) return rushDeliveryDate(anOrder);
-    else return regularDeliveryDate(anOrder);
-}
-
 function rushDeliveryDate(anOrder) {
     let deliveryTime;
     if (["MA", "CT"].includes(anOrder.deliveryState)) deliveryTime = 1;
