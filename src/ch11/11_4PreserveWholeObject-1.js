e98b325b2e90f1f56f31f6c78cd6378d0d3783a8
Clustern <homerun372@gmail.com>
2023-01-29 17:00:28 +0900

    refactor: withinRange 함수 인라인

--- src/ch11/11_4PreserveWholeObject-1.js
+++ src/ch11/11_4PreserveWholeObject-1.js
@@ -1,10 +1,7 @@
 class HeatingPlan {
-    withinRange(bottom, top) {
-        return (bottom >= this._temperatureRange.low) && (top >= this._temperatureRange.high)
-    }
-
     xxNEWwithinRange(aNumberRange) {
-        return this.withinRange(aNumberRange.low, aNumberRange.high)
+        return (aNumberRange.low >= this._temperatureRange.low) &&
+            (aNumberRange.high >= this._temperatureRange.high)
     }
 }
 
