04cc5ac9c9b761f3b865bc9e35af00f8ee90b05d
Clustern <homerun372@gmail.com>
2023-01-29 13:44:13 +0900

    refactor: alertForMiscreant 함수를 findMiscreant 이름으로 복제

--- src/ch11/11_1SeparateQueryFromModifier-1.js
+++ src/ch11/11_1SeparateQueryFromModifier-1.js
@@ -11,3 +11,17 @@ function alertForMiscreant(people) {
     }
     return ""
 }
+
+function findMiscreant(people) {
+    for (const p of people) {
+        if (p === "조커") {
+            setOffAlarms();
+            return "조커";
+        }
+        if (p === "사루만") {
+            setOffAlarms();
+            return "사루만";
+        }
+    }
+    return ""
+}
