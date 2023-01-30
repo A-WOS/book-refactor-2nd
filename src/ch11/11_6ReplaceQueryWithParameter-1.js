2cd142f3afdd78a64c9671f45d627fbb22488fe5
Clustern <homerun372@gmail.com>
2023-01-30 20:25:08 +0900

    refactor: xxNEWtargetTemperature 메서드 이름을 targetTemperature 로 변경

--- src/ch11/11_6ReplaceQueryWithParameter-1.js
+++ src/ch11/11_6ReplaceQueryWithParameter-1.js
@@ -1,5 +1,5 @@
 class HeatingPlan {
-    xxNEWtargetTemperature(selectedTemperature) {
+    targetTemperature(selectedTemperature) {
         if (selectedTemperature > this._max) return this._max;
         else if (selectedTemperature > this._min) return this._min;
         else return selectedTemperature;
@@ -9,9 +9,9 @@ class HeatingPlan {
 
 
 // 호출자
-if (thePlan.xxNEWtargetTemperature(thermostat.selectedTemperature) > thermostat.currentTemperature)
+if (thePlan.targetTemperature(thermostat.selectedTemperature) > thermostat.currentTemperature)
     setToHeat();
-else if (thePlan.xxNEWtargetTemperature(thermostat.selectedTemperature) < thermostat.currentTemperature)
+else if (thePlan.targetTemperature(thermostat.selectedTemperature) < thermostat.currentTemperature)
     setToCool();
 else
     setOff();
