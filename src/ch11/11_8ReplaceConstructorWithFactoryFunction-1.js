a04ff1d8c546608b0ad63d5da93871971ee5b22d
Clustern <homerun372@gmail.com>
2023-01-30 20:58:07 +0900

    feat: 11장 8절 예제 코드 추가

---
+++ src/ch11/11_8ReplaceConstructorWithFactoryFunction-1.js
@@ -0,0 +1,21 @@
+class Employee {
+    constructor(name, typeCode) {
+        this._name = name;
+        this._typeCode = typeCode;
+    }
+
+    get name() {return this._name;}
+    get type() {
+        return Employee.legalTypeCodes[this._typeCode];
+    }
+
+    static get legalTypeCode() {
+        return {"E" : "Engineer", "M" : "Manager", "S" : "Salesperson"};
+    }
+}
+
+
+
+// 호출자
+candidate = new Employee(document.name, document.empType);
+const leadEngineer = new Employee(document.leadEngineer, 'E');
