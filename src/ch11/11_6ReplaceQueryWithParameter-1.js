f590000402b62bdb443f279b798dc142f8a92676
Clustern <homerun372@gmail.com>
2023-01-30 20:17:29 +0900

    feat: 11장 6절 예제 코드 추가

---
+++ src/ch11/11_6ReplaceQueryWithParameter-1.js
@@ -0,0 +1,14 @@
+class HeatingPlan {
+    get targetTemperature() {
+        if (thermostat.selectedTemperature > this._max) return this._max;
+        else if (thermostat.selectedTemperature > this._min) return this._min;
+        else return thermostat.selectedTemperature;
+    }
+}
+
+
+
+// 호출자
+if (thePlan.targetTemperature > thermostat.currentTemperature) setToHeat();
+else if (thePlan.targetTemperature < thermostat.currentTemperature) setToCool();
+else setOff();
