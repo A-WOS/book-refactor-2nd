3eb14be1d422289ef0411f0044570e2c62ad0f46
Clustern <homerun372@gmail.com>
2023-01-31 17:32:31 +0900

    refactor: smoker 여부에 대한 조건문 부분을 scoreSmoking 함수로 추출

--- src/ch11/11_9ReplaceFunctionWithCommand-1.js
+++ src/ch11/11_9ReplaceFunctionWithCommand-1.js
@@ -16,11 +16,7 @@ class Score {
         this._healthLevel = 0;
         this._highMedicalRiskFlag = false;
 
-        if (this._medicalExam.isSmoker) {
-            this._healthLevel += 10;
-            this._highMedicalRiskFlag = true;
-        }
-
+        this.scoreSmoking();
         this._certificationGrade = "regular";
         if (this._scoringGuide.stateWithLoCertification(this._candidate.originState)) {
             this._certificationGrade = "Low";
@@ -29,6 +25,13 @@ class Score {
         //비슷한 코드가 한참 이어짐
         this._result -= Math.max(this._healthLevel - 5, 0);
         return this._result
+    }
+
+    scoreSmoking() {
+        if (this._medicalExam.isSmoker) {
+            this._healthLevel += 10;
+            this._highMedicalRiskFlag = true;
+        }
     }
 }
 
