141fca5e11333b9b9568edf4e8bfa8a247ac6d22
Clustern <homerun372@gmail.com>
2023-01-31 18:04:51 +0900

    refactor: totalAscent 변수 값의 초기값을 calculateAscent 함수 반환값으로 하고 불변으로 선언

--- src/ch11/11_11ReturnModifiedValue-1.js
+++ src/ch11/11_11ReturnModifiedValue-1.js
@@ -1,8 +1,7 @@
-let totalAscent = 0;
+const totalAscent = calculateAscent();
 let totalTime = 0;
 let totalDistance = 0;
-calculateAscent();
-totalAscent = calculateTime();
+calculateTime();
 calculateDistance();
 const pace = totalTime / 60 / totalDistance;
 
