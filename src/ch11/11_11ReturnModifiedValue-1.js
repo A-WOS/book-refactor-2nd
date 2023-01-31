e0042769d723f695b7cfe14047a8514574fc5613
Clustern <homerun372@gmail.com>
2023-01-31 18:02:55 +0900

    refactor: calculateAscent 함수 내에서 totalAscent 변수 이름을 result 로 변경

--- src/ch11/11_11ReturnModifiedValue-1.js
+++ src/ch11/11_11ReturnModifiedValue-1.js
@@ -7,11 +7,11 @@ calculateDistance();
 const pace = totalTime / 60 / totalDistance;
 
 function calculateAscent() {
-    let totalAscent = 0;
+    let result = 0;
     for (let i = 1; i < points.length; i++) {
         const verticalChange = points[i].elevation - points[i - 1].elevation;
-        totalAscent = (verticalChange > 0) ? verticalChange : 0;
+        result = (verticalChange > 0) ? verticalChange : 0;
     }
 
-    return totalAscent;
+    return result;
 }
