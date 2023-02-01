2c9e48c2c379803caa8f41103448d0989f5e2516
AWOS <uws0331@gmail.com>
2023-02-01 09:00:12 +0900

    fix: withinBand 함수의 매개변수 호출

--- src/ch11/2/11_2_2_parameterizeFunction.js
+++ src/ch11/2/11_2_2_parameterizeFunction.js
@@ -6,7 +6,7 @@ function baseCharge(usage) {
     if (usage < 0) return usd(0);
     const amount =
         bottomBand(usage) * 0.03
-        + withinBand(usage) * 0.05
+        + withinBand(usage, 100, 200) * 0.05
         +topBand(usage) * 0.07;
     return usd(amount);
 }
