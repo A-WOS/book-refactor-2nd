e8786853b6906ef1384140eb9c23838e5f8ae90b
AWOS <uws0331@gmail.com>
2023-02-01 09:20:38 +0900

    fix: 리팩터링하다가 함수 범위 수정

--- src/ch11/3/11_3_1_removeFlagArgument.js
+++ src/ch11/3/11_3_1_removeFlagArgument.js
@@ -2,21 +2,22 @@ function deliveryDate(anOrder, isRush) {
     if (isRush) return rushDeliveryDate(anOrder);
     else return regularDeliveryDate(anOrder);
 
-    function rushDeliveryDate(anOrder) {
-        let deliveryTime;
-        if (["MA", "CT"].includes(anOrder.deliveryState)) deliveryTime = 1;
-        else if (["NY", "NH"].includes(anOrder.deliveryState)) deliveryTime = 2;
-        else deliveryTime = 3;
-        return anOrder.placeOn.plusDays(1 + deliveryTime);
-    }
+}
 
-    function regularDeliveryDate(anOrder) {
-        let deliveryTime;
-        if (["MA", "CT", "NY"].includes(anOrder.deliveryState)) deliveryTime = 2;
-        else if (["ME", "NH"].includes(anOrder.deliveryState)) deliveryTime = 3;
-        else deliveryTime = 4;
-        return anOrder.placeOn.plusDays(2 + deliveryTime);
-    }
+function rushDeliveryDate(anOrder) {
+    let deliveryTime;
+    if (["MA", "CT"].includes(anOrder.deliveryState)) deliveryTime = 1;
+    else if (["NY", "NH"].includes(anOrder.deliveryState)) deliveryTime = 2;
+    else deliveryTime = 3;
+    return anOrder.placeOn.plusDays(1 + deliveryTime);
+}
+
+function regularDeliveryDate(anOrder) {
+    let deliveryTime;
+    if (["MA", "CT", "NY"].includes(anOrder.deliveryState)) deliveryTime = 2;
+    else if (["ME", "NH"].includes(anOrder.deliveryState)) deliveryTime = 3;
+    else deliveryTime = 4;
+    return anOrder.placeOn.plusDays(2 + deliveryTime);
 }
 
 aShipment.deliveryDate = deliveryDate(anOrder, true);
