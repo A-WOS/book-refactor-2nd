bb9c8a82b36d492e31440a349d1937be44e9edcb
AWOS <uws0331@gmail.com>
2023-02-01 09:03:37 +0900

    refactor: topBand 함수를 새로 만든 매개변수화 함수(withinBand)를 호출하도록 변경

--- src/ch11/2/11_2_2_parameterizeFunction.js
+++ src/ch11/2/11_2_2_parameterizeFunction.js
@@ -7,10 +7,6 @@ function baseCharge(usage) {
     const amount =
         withinBand(usage, 0, 100) * 0.03
         + withinBand(usage, 100, 200) * 0.05
-        + topBand(usage) * 0.07;
+        + withinBand(usage, 200, Infinity) * 0.07;
     return usd(amount);
-}
-
-function topBand(usage) {
-    return usage > 200 ? usage - 200 : 0;
 }
