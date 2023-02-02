cf2b8a72c508563b416ecf16d006adf46c128d3d
AWOS <uws0331@gmail.com>
2023-02-03 02:10:36 +0900

    refactor: if-else 조건문을 메서드로 추출

--- src/ch11/6/11_6_1_replaceQueryWithParameter.js
+++ src/ch11/6/11_6_1_replaceQueryWithParameter.js
@@ -1,6 +1,10 @@
 class HeatingPlan {
     get targetTemperature() {
         const selectedTemperature = thermostat.selectedTemperature;
+        return this.xxNEWtargetTemperature(selectedTemperature);
+    }
+
+    xxNEWtargetTemperature(selectedTemperature) {
         if (selectedTemperature > this._max) return this._max;
         else if (selectedTemperature < this._min) return this._min;
         else return selectedTemperature;
