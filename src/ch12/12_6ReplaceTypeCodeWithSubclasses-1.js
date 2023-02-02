ecdb2a9645e85bf5cd01b7cb81d7690e11ca21ee
Clustern <homerun372@gmail.com>
2023-02-02 16:34:26 +0900

    refactor: 슈퍼 클래스의 type 필드와 게터 제거

--- src/ch12/12_6ReplaceTypeCodeWithSubclasses-1.js
+++ src/ch12/12_6ReplaceTypeCodeWithSubclasses-1.js
@@ -2,15 +2,11 @@ class Employee {
     constructor(name, type) {
         this.validateType(type);
         this._name = name;
-        this._type = type;
     }
 
     vaildateType(arg) {
         if (!["enggineer", "manager", "salesperson"].includes(arg))
             throw new Error(`${arg}라는 직원 유형은 없습니다.`);
-    }
-    get type() {
-        return this._type;
     }
 
     toString() {
