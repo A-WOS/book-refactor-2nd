ddc685310b1701487740707fd96fae34a5c87aed
Clustern <homerun372@gmail.com>
2023-02-03 06:09:57 +0900

    refactor: 남성일 경우의 로직을 슈퍼클래스로 옮김

--- src/ch12/12_7RemoveSubclass-1.js
+++ src/ch12/12_7RemoveSubclass-1.js
@@ -17,7 +17,7 @@ class Person {
     }
 
     get isMale() {
-        return this instanceof Male;
+        return "M" === this._genderCode;
     }
 }
 
@@ -36,7 +36,7 @@ class Female extends Person {
 function createPerson(aRecord) {
     switch (aRecord.gender) {
         case 'M':
-            return new Male(aRecord.name);
+            return new Person(aRecord.name, "M");
         case 'F':
             return new Female(aRecord.name);
         default:
