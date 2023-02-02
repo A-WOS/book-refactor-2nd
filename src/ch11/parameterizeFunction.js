c52323e57862adee208ab50dd368ad0b81b5a6b6
Bell <bell04204@gmail.com>
2023-02-03 00:19:11 +0900

    refactor: middleBand 함수를 매개변수화 함수로 변경

--- src/ch11/parameterizeFunction.js
+++ src/ch11/parameterizeFunction.js
@@ -1,17 +1,17 @@
+function withinBand(usage, bottom, top) {
+    return usage > bottom ? Math.min(usage, top) - bottom : 0;
+}
+
 function baseCharge(usage) {
     if (usage < 0) return usd(0);
 
-    const amount = bottomBand(usage) * 0.03 + middleBand(usage) * 0.05 + topBand(usage) * 0.07;
+    const amount = bottomBand(usage) * 0.03 + withinBand(usage, 100, 200) * 0.05 + topBand(usage) * 0.07;
 
     return usd(amount);
 }
 
 function bottomBand(usage) {
     return Math.min(usage, 100);
-}
-
-function middleBand(usage) {
-    return usage > 100 ? Math.min(usage, 200) - 100 : 0;
 }
 
 function topBand(usage) {
