6a6ac1df7014fbecf1e92e4c7700827e91d86998
Clustern <homerun372@gmail.com>
2023-02-02 16:45:44 +0900

    feat: 12장 6절 예제 2번 코드 추가

---
+++ src/ch12/12_6ReplaceTypeCodeWithSubclasses-2.js
@@ -0,0 +1,27 @@
+class Employee {
+    constructor(name, type) {
+        this.validateType(type);
+        this._name = name;
+        this._type = type;
+    }
+
+    validateType(arg) {
+        if (!["enggineer", "manager", "salesperson"].includes(arg))
+            throw new Error(`${arg}라는 직원 유형은 없습니다.`);
+    }
+    get type() {
+        return this._type;
+    }
+
+    set type(arg) {
+        this._type = arg;
+    }
+
+    get capitalizedType() {
+        return this._type.chartAt(0).toUpperCase() + this._type.substr(1).toLowerCase();
+    }
+
+    toString() {
+        return `${this._name} (${this.capitalizedType})`;
+    }
+}
