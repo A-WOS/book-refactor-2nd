d022a7358256c5988c505071c21de49499ada00f
AWOS <uws0331@gmail.com>
2023-02-02 23:24:03 +0900

    refactor: 함수 옮기기 수행

--- src/ch11/4/11_4_2_preserveWholeObject.js
+++ src/ch11/4/11_4_2_preserveWholeObject.js
@@ -1,7 +1,14 @@
-const tempRange = aRoom.daysTempRange;
-const low = tempRange.low;
-const high = tempRange.high;
-const isWithinRange = aPlan.withinRange(low, high);
+class Heating {
 
+    xxNEWwithinRange(tempRange) {
+        const low = tempRange.low;
+        const high = tempRange.high;
+        const isWithinRange = this.withinRange(low, high);
+        return isWithinRange;
+    }
+}
+
+const tempRange = aRoom.daysTempRange;
+const isWithinRange = aPlan.xxNEWwithinRange(tempRange);
 if (!isWithinRange)
     alerts.push("방 온도가 지정 범위를 벗어났습니다.");
