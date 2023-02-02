11b3d4730a9db83bd13caffc8b7287544b27aea9
AWOS <uws0331@gmail.com>
2023-02-03 01:48:08 +0900

    feat: 예제 코드 추가(11.6 질의 함수를 매개변수로 바꾸기)

---
+++ src/ch11/6/11_6_1_replaceQueryWithParameter.js
@@ -0,0 +1,11 @@
+class HeatingPlan {
+    get targetTemperature() {
+        if (thermostat.selectedTemperature > this._max) return this._max;
+        else if (thermostat.selectedTemperature < this._min) return this._min;
+        else return thermostat.selectedTemperature;
+    }
+}
+
+if (thePlan.targetTemperature > thermostat.currentTemperature) setToHeat();
+else if (thePlan.targetTemperature < thermostat.currentTemperature) setToCool();
+else setOff();
