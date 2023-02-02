303a44af25433cefb719c662aabf79d4bda3964f
Clustern <homerun372@gmail.com>
2023-02-02 15:50:51 +0900

    feat: 12장 1절 예제 코드 추가

---
+++ src/ch12/12_1PullUpMethod-1.js
@@ -0,0 +1,11 @@
+class Employee extends Party {
+    get annualCost() {
+        return this.monthlyCost * 12;
+    }
+}
+
+class Department extends Party {
+    get totalAnnualCost() {
+        return this.monthlyCost * 12;
+    }
+}
