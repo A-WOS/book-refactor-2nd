77e07d9a16e5dc4bb3b1f0796c4248c15c342d97
Clustern <homerun372@gmail.com>
2023-02-02 15:59:45 +0900

    refactor: 서브 클래스에서만 존재하는 monthlyCost 메서드를 슈퍼클래스에서 호출 시 서브클래스 책임 오류 출력

--- src/ch12/12_1PullUpMethod-1.js
+++ src/ch12/12_1PullUpMethod-1.js
@@ -2,10 +2,20 @@ class Party {
     get annualCost() {
         return this.monthlyCost * 12;
     }
+
+    get monthlyCost() {
+        throw new SubclassResponsibilityError();
+    }
 }
 
 class Employee extends Party {
+    get monthlyCost() {
+
+    }
 }
 
 class Department extends Party {
+    get monthlyCost() {
+
+    }
 }
