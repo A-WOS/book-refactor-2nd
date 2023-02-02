a1ae84302847f701201b7c3a343991d733a347aa
Clustern <homerun372@gmail.com>
2023-02-02 16:50:29 +0900

    refactor: 타입 필드를 객체로 변경

--- src/ch12/12_6ReplaceTypeCodeWithSubclasses-2.js
+++ src/ch12/12_6ReplaceTypeCodeWithSubclasses-2.js
@@ -9,19 +9,35 @@ class Employee {
         if (!["enggineer", "manager", "salesperson"].includes(arg))
             throw new Error(`${arg}라는 직원 유형은 없습니다.`);
     }
+
+    get typeString() {
+        return this._type.toString();
+    }
+
     get type() {
         return this._type;
     }
 
     set type(arg) {
-        this._type = arg;
+        this._type = new EmployeeType(arg);
     }
 
     get capitalizedType() {
-        return this._type.chartAt(0).toUpperCase() + this._type.substr(1).toLowerCase();
+        return this.typeString.charAt(0).toUpperCase()
+            + this.typeString.substr(1).toLowerCase();
     }
 
     toString() {
         return `${this._name} (${this.capitalizedType})`;
+    }
+}
+
+class EmployeeType {
+    constructor(aString) {
+        this._value = aString;
+    }
+
+    toString() {
+        return this._value
     }
 }
