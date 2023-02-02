c745735cbdd735a5bea11c0576ff69a5d2a486c8
Clustern <homerun372@gmail.com>
2023-02-03 05:55:31 +0900

    refactor: 생성할 클래스를 선택하는 로직을 함수로 추출

--- src/ch12/12_7RemoveSubclass-1.js
+++ src/ch12/12_7RemoveSubclass-1.js
@@ -24,16 +24,25 @@ class Female extends Person {
     }
 }
 
+function createPerson(aRecord) {
+    let p;
+    switch (aRecord.gender) {
+        case 'M':
+            p = new Male(aRecord.name);
+            break;
+        case 'F':
+            p = new Female(aRecord.name);
+            break;
+        default:
+            p = new Person(aRecord.name);
+    }
+    return p;
+}
+
 function loadFromInput(data) {
     const result = [];
     data.forEach(aRecord => {
-        let p;
-        switch (aRecord.gender) {
-            case 'M': p = new Male(aRecord.name); break;
-            case 'F': p = new Female(aRecord.name); break;
-            default: p = new Person(aRecord.name);
-        }
-        result.push(p);
+        result.push(createPerson(aRecord));
     });
     return result;
 }
