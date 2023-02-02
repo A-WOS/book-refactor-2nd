7ffe5996a295129bacba48755ecaf160e609f95b
AWOS <uws0331@gmail.com>
2023-02-02 23:14:49 +0900

    feat: 기존 메서드 부분을 변수로 추출

--- src/ch11/4/11_4_2_preserveWholeObject.js
+++ src/ch11/4/11_4_2_preserveWholeObject.js
@@ -1,5 +1,6 @@
 const low = aRoom.daysTempRange.low;
 const high = aRoom.daysTempRange.high;
+const isWithinRange = aPlan.withinRange(low, high);
 
-if (!aPlan.withinRange(low, high))
+if (!isWithinRange)
     alerts.push("방 온도가 지정 범위를 벗어났습니다.");
