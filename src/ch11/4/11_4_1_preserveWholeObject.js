7518c94f6ad48f35c8fdcf18af0b9397073cb1bf
AWOS <uws0331@gmail.com>
2023-02-02 23:06:47 +0900

    refactor: 기존 함수를 호출하는 코드를 새 함수를 호출하도록 코드 수정

--- src/ch11/4/11_4_1_preserveWholeObject.js
+++ src/ch11/4/11_4_1_preserveWholeObject.js
@@ -12,5 +12,5 @@ class HeatingPlan {
 const low = aRoom.daysTempRange.low;
 const high = aRoom.daysTempRange.high;
 
-if (!aPlan.withinRange(low, high))
+if (!aPlan.xxNEWwithinRange(aRoom.daysTempRange))
     alerts.push("방 온도가 지정 범위를 벗어났습니다.");
