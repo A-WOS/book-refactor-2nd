14b4680f67d3264dde7d0c3bdb7f70d962afda86
Clustern <homerun372@gmail.com>
2023-02-04 02:28:50 +0900

    refactor: Scroll 의 CatalogItem 클래스 상속 제거

--- src/ch12/12_11ReplaceSuperclassWithDelegate-1.js
+++ src/ch12/12_11ReplaceSuperclassWithDelegate-1.js
@@ -18,9 +18,8 @@ class CatalogItem {
     }
 }
 
-class Scroll extends CatalogItem {
+class Scroll {
     constructor(id, title, tags, dateLastCleaned) {
-        super(id, title, tags);
         this._catalogItem = new CatalogItem(id, title, tags);
         this._lastCleaned = dateLastCleaned;
     }
