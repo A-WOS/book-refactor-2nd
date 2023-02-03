9330c0abd96eb0eb5beafaf97d3c6a2fa81d2b9d
AWOS <uws0331@gmail.com>
2023-02-04 02:42:11 +0900

    refactor: 공통된 메서드를 슈퍼 클래스로 이동

--- src/ch12/1/12_1_1_pullUpMethod.js
+++ src/ch12/1/12_1_1_pullUpMethod.js
@@ -1,11 +1,11 @@
-class Employee extends Party {
+class Party {
     get annualCost() {
         return this.monthlyCost * 12;
     }
 }
 
+class Employee extends Party {
+}
+
 class Department extends Party {
-    get annualCost() {
-        return this.monthlyCost * 12;
-    }
 }
