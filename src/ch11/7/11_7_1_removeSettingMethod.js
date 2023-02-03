fb495069928effb0b27070a71c0d1e7817c0973a
AWOS <uws0331@gmail.com>
2023-02-03 16:59:07 +0900

    refactor: 생성자에서 ID를 별도로 받도록 개선

--- src/ch11/7/11_7_1_removeSettingMethod.js
+++ src/ch11/7/11_7_1_removeSettingMethod.js
@@ -1,10 +1,11 @@
 class Person {
+    constructor(id) {
+        this._id = id;
+    }
     get name() { return this._name; }
     set name(arg) { this._name = arg; }
     get id() { return this._id; }
-    set id(arg) { return this._id = arg; }
 }
 
-const martin = new Person();
+const martin = new Person("1234");
 martin.name = "마틴";
-martin.id = "1234";
