b0138b2e68c5ef313750b36d4df6fa01198b36c9
Clustern <homerun372@gmail.com>
2023-02-03 14:44:14 +0900

    refactor: 서브 클래스에서 공통된 속성인 name 필드를 슈퍼 클래스로 추출

--- src/ch12/12_8ExtractSuperclass-1.js
+++ src/ch12/12_8ExtractSuperclass-1.js
@@ -1,12 +1,13 @@
 class Party {
-
+    constructor(name) {
+        this._name = name;
+    }
 }
 
 class Employee extends Party {
     constructor(name, id, monthlyCost) {
-        super();
+        super(name);
         this._id = id;
-        this._name = name;
         this._monthlyCost = monthlyCost;
     }
 
@@ -29,8 +30,7 @@ class Employee extends Party {
 
 class Department extends Party {
     constructor(name, staff) {
-        super();
-        this._name = name;
+        super(name);
         this._staff = staff;
     }
 
