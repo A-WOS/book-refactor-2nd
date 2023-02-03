70949caa72584957fa03a6d621d37efb667b67a6
AWOS <uws0331@gmail.com>
2023-02-04 00:52:56 +0900

    refactor: 생성자가 받던 모든 매개변수를 charge 메서드로 옮기기

--- src/ch11/10/11_10_1_replaceCommandWithFunction.js
+++ src/ch11/10/11_10_1_replaceCommandWithFunction.js
@@ -4,14 +4,15 @@ class ChargeCalculator {
         this._usage = usage;
         this._provider = provider;
     }
-    get charge() {
+    charge(customer, usage, provider) {
         const baseCharge = this._customer.baseRate * this._usage;
         return baseCharge + this._provider.connectionCharge;
     }
 }
 
 function charge(customer, usage, provider) {
-    return new ChargeCalculator(customer, usage, provider).charge();
+    return new ChargeCalculator(customer, usage, provider)
+        .charge(customer, usage, provider);
 }
 
 monthCharge = charge(customer, usage, provider);
