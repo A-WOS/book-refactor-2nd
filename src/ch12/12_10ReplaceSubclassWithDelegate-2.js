9098e9f2f0677563da319d27172a48b484667249
Clustern <homerun372@gmail.com>
2023-02-03 15:47:47 +0900

    refactor: 리팩터링 결과로 의미가 없어진 EuropeanSwallow 클래스 제거

--- src/ch12/12_10ReplaceSubclassWithDelegate-2.js
+++ src/ch12/12_10ReplaceSubclassWithDelegate-2.js
@@ -1,7 +1,5 @@
 function createBird(data) {
     switch (data.type) {
-        case '유럽 제비':
-            return new EuropeanSwallow(data);
         case '아프리카 제비':
             return new AfricanSwallow(data);
         case '노르웨이 파랑 앵무':
@@ -37,12 +35,6 @@ class Bird {
 
     get airSpeedVelocity() {
         return this._speciesDelegate ? this._speciesDelegate.airSpeedVelocity() : null;
-    }
-}
-
-class EuropeanSwallow extends Bird {
-    get airSpeedVelocity() {
-        return this._speciesDelegate.airSpeedVelocity;
     }
 }
 
