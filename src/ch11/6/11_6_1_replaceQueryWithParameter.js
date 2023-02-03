792ed0d60b6fb33c50aa1dc0d8f6fe7873eee0ca
AWOS <uws0331@gmail.com>
2023-02-03 16:51:56 +0900

    refactor: 새 메서드의 이름을 원래 메서드의 이름으로 변경

--- src/ch11/6/11_6_1_replaceQueryWithParameter.js
+++ src/ch11/6/11_6_1_replaceQueryWithParameter.js
@@ -1,19 +1,15 @@
 class HeatingPlan {
-    get targetTemperature() {
-        return this.xxNEWtargetTemperature(thermostat.selectedTemperature);
-    }
-
-    xxNEWtargetTemperature(selectedTemperature) {
+    targetTemperature(selectedTemperature) {
         if (selectedTemperature > this._max) return this._max;
         else if (selectedTemperature < this._min) return this._min;
         else return selectedTemperature;
     }
 }
 
-if (thePlan.xxNEWtargetTemperature(thermostat.selectedTemperature) >
+if (thePlan.targetTemperature(thermostat.selectedTemperature) >
     thermostat.currentTemperature)
     setToHeat();
-else if (thePlan.xxNEWtargetTemperature(thermostat.selectedTemperature) <
+else if (thePlan.targetTemperature(thermostat.selectedTemperature) <
     thermostat.currentTemperature)
     setToCool();
 else setOff();
