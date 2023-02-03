8eb692bcc78de5c40a2076add9be8ad969ab727e
AWOS <uws0331@gmail.com>
2023-02-04 00:56:31 +0900

    refactor: 건네 받은 매개변수를 사용하도록 수정

--- src/ch11/10/11_10_1_replaceCommandWithFunction.js
+++ src/ch11/10/11_10_1_replaceCommandWithFunction.js
@@ -1,12 +1,7 @@
 class ChargeCalculator {
-    constructor(customer, usage, provider) {
-        this._customer = customer;
-        this._usage = usage;
-        this._provider = provider;
-    }
     charge(customer, usage, provider) {
-        const baseCharge = this._customer.baseRate * this._usage;
-        return baseCharge + this._provider.connectionCharge;
+        const baseCharge = customer.baseRate * usage;
+        return baseCharge + provider.connectionCharge;
     }
 }
 
