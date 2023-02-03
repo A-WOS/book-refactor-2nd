7cb6bfd6b5bf1f983eade789ebe76415cc92c7bc
AWOS <uws0331@gmail.com>
2023-02-04 00:58:15 +0900

    refactor: 최상위 charge 함수로 인라인 및 죽은 명령 클래스 제거

--- src/ch11/10/11_10_1_replaceCommandWithFunction.js
+++ src/ch11/10/11_10_1_replaceCommandWithFunction.js
@@ -1,13 +1,6 @@
-class ChargeCalculator {
-    charge(customer, usage, provider) {
-        const baseCharge = customer.baseRate * usage;
-        return baseCharge + provider.connectionCharge;
-    }
-}
-
 function charge(customer, usage, provider) {
-    return new ChargeCalculator(customer, usage, provider)
-        .charge(customer, usage, provider);
+    const baseCharge = customer.baseRate * usage;
+    return baseCharge + provider.connectionCharge;
 }
 
 monthCharge = charge(customer, usage, provider);
