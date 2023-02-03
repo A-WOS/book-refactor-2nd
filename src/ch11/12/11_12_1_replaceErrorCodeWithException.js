62dcd45fe5f32d11f7bd78b65c27e38da13c66cd
AWOS <uws0331@gmail.com>
2023-02-04 01:40:39 +0900

    feat: 리팩터링으로 추가된 예외를 처리하기 위해 서브클래스 생성

--- src/ch11/12/11_12_1_replaceErrorCodeWithException.js
+++ src/ch11/12/11_12_1_replaceErrorCodeWithException.js
@@ -1,3 +1,11 @@
+class OrderProcessingError extends Error {
+    constructor(errorCode) {
+        super(`주문 처리 오류 ${errorCode}`);
+        this.code = errorCode;
+    }
+    get name() { return "OrderProcessingError"; }
+}
+
 function localShippingRules(country) {
     const data = countryData.shippingRules[country];
     if (data) return new ShippingRules(data);
