6344be83c40d90a1cf4462ba81dc41def5a86011
AWOS <uws0331@gmail.com>
2023-02-03 21:22:52 +0900

    refactor: 함수 추출하기를 통해 명령을 중첩함수처럼 다루도록 개선

--- src/ch11/9/11_9_1_replaceFunctionWithCommand.js
+++ src/ch11/9/11_9_1_replaceFunctionWithCommand.js
@@ -14,10 +14,7 @@ class Score {
         this._healthLevel = 0;
         this._highMedicalRiskFlag = false;
 
-        if (this._medicalExam.isSmoker) {
-            this._healthLevel += 10;
-            this._highMedicalRiskFlag = true;
-        }
+        this.scoreSmoking();
         this._certificationGrade = "regular";
         if (this._scoringGuide.stateWithLowCertification(this._candidate.originState)) {
             this._certificationGrade = "low";
@@ -26,5 +23,12 @@ class Score {
         // 비슷한 코드가 한참 이어짐
         this._result -= Math.max(this._healthLevel - 5, 0);
         return this._result;
+    }
+
+    scoreSmoking() {
+        if (this._medicalExam.isSmoker) {
+            this._healthLevel += 10;
+            this._highMedicalRiskFlag = true;
+        }
     }
 }
