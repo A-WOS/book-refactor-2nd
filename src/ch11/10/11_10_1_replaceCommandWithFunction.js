2c209813795f9734521c3e41a7d48a0e7386c0c6
AWOS <uws0331@gmail.com>
2023-02-03 21:31:17 +0900

    feat: replaceCommandWithFunction 기본 예제 코드 추가

---
+++ src/ch11/10/11_10_1_replaceCommandWithFunction.js
@@ -0,0 +1,15 @@
+class ChargeCalculator {
+    constructor(customer, usage, provider) {
+        this._customer = customer;
+        this._usage = usage;
+        this._provider = provider;
+    }
+    get baseCharge() {
+        return this._customer.baseRate * this._usage;
+    }
+    get charge() {
+        return this._baseCharge + this._provider.connectionCharge;
+    }
+}
+
+monthCharge = new ChargeCalculator(customer, usage, provider).charge;
