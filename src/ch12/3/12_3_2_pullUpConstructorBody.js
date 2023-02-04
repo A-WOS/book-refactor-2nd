6a4948ec5039abb8b80928a7880fb4c993680b6d
AWOS <uws0331@gmail.com>
2023-02-04 18:42:23 +0900

    refactor: 공통 코드를 함수로 추출

--- src/ch12/3/12_3_2_pullUpConstructorBody.js
+++ src/ch12/3/12_3_2_pullUpConstructorBody.js
@@ -8,10 +8,14 @@ class Manager extends Employee {
     constructor(name, grade) {
         super(name);
         this._grade = grade;
-        if (this.isPrivileged) this.assignCar(); // 모든 서브클래스가 수행한다.
+        this.finishConstruction();
     }
 
     get isPrivileged() {
         return this._grade > 4;
+    }
+
+    finishConstruction() {
+        if (this.isPrivileged) this.assignCar(); // 모든 서브클래스가 수행한다.
     }
 }
