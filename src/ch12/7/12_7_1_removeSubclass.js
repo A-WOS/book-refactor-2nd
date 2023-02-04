4cf7e64904028ddd5fc4ed53ed55d04417316288
AWOS <uws0331@gmail.com>
2023-02-04 19:53:52 +0900

    refactor: 슈퍼 클래스에서 처리되도록 설정하고 서브클래스는 제거

--- src/ch12/7/12_7_1_removeSubclass.js
+++ src/ch12/7/12_7_1_removeSubclass.js
@@ -1,40 +1,32 @@
 class Person {
     constructor(name, genderCode) {
         this._name = name;
-        this._genderCode = genderCode || "X";
+        this._genderCode = genderCode;
     }
 
     get name() {
         return this._name;
     }
 
+    get gender() {
+        return "X";
+    }
     get genderCode() {
         return this._genderCode;
     }
 
-    get isMale() {return this instanceof Male;}
-}
-
-class Male extends Person {
-    get genderCode() {
-        return "M";
-    }
-}
-
-class Female extends Person {
-    get genderCode() {
-        return "F";
-    }
+    get isMale() {return "M" === this._genderCode;}
+    get isFemale() {return "F" === this._genderCode;}
 }
 
 function createPerson(aRecord) {
     switch (aRecord.gender) {
         case 'M':
-            return new Male(aRecord.name);
+            return new Male(aRecord.name, "M");
         case 'F':
-            return new Female(aRecord.name);
+            return new Female(aRecord.name, "F");
         default:
-            return new Person(aRecord.name);
+            return new Person(aRecord.name, "X");
     }
 }
 
