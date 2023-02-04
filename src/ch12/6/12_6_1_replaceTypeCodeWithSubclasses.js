2214fb7fd7d81a0f0e3506a23ebb83247fb28b71
AWOS <uws0331@gmail.com>
2023-02-04 18:57:51 +0900

    style: 타입 코드 필드와 슈퍼클래스의 게터를 제거

--- src/ch12/6/12_6_1_replaceTypeCodeWithSubclasses.js
+++ src/ch12/6/12_6_1_replaceTypeCodeWithSubclasses.js
@@ -2,13 +2,11 @@ class Employee {
     constructor(name, type) {
         this.validateType(type);
         this._name = name;
-        this._type = type;
     }
     validateType(arg) {
         if (!["engineer", "manager", "salesperson"].includes(arg))
             throw new Error(`${arg}라는 직원 유형은 없습니다.`);
     }
-    get type() { return this._type; }
     toString() {return `${this._name} (${this._type})`;}
 }
 
