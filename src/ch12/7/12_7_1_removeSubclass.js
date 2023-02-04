e8de32dcd0505681014d2663b0623320d6189eff
AWOS <uws0331@gmail.com>
2023-02-04 19:49:10 +0900

    refactor: 성별을 나타낼 필드 추가 및 생성자에서 매개변수로 받아 설정

--- src/ch12/7/12_7_1_removeSubclass.js
+++ src/ch12/7/12_7_1_removeSubclass.js
@@ -1,14 +1,15 @@
 class Person {
-    constructor(name) {
+    constructor(name, genderCode) {
         this._name = name;
+        this._genderCode = genderCode || "X";
     }
 
     get name() {
         return this._name;
     }
 
-    get gender() {
-        return "X";
+    get genderCode() {
+        return this._genderCode;
     }
 
     get isMale() {return this instanceof Male;}
@@ -42,5 +43,3 @@ function loadFromInput(data) {
 }
 
 const numberOfMales = people.filter(p => p.isMale).length;
-
-
