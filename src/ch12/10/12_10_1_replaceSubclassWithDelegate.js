1eb94bb189e4105862c29078cac216280bc3f9c2
AWOS <uws0331@gmail.com>
2023-02-04 21:09:26 +0900

    refactor: 서브 클래스 삭제

--- src/ch12/10/12_10_1_replaceSubclassWithDelegate.js
+++ src/ch12/10/12_10_1_replaceSubclassWithDelegate.js
@@ -31,18 +31,6 @@ class Booking {
     }
 }
 
-class PremiumBooking extends Booking {
-    constructor(show, date, extras) {
-        super(show, date);
-        this._extras = extras;
-    }
-
-    get basePrice() {
-        return Math.round(super.basePrice + this._extras.premiumFee);
-    }
-
-}
-
 class PremiumBookingDelegate {
     constructor(hostBooking, extras) {
         this._host = hostBooking;
@@ -64,7 +52,7 @@ function createBooking(show, date) {
 }
 
 function createPremiumBooking(show, date, extras) {
-    const result = new PremiumBooking(show, date, extras);
+    const result = new Booking(show, date, extras);
     result._bePremium(extras);
     return result;
 }
