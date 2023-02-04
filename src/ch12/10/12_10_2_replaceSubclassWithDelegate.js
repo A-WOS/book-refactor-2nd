52e1e040ef06adf3c58126597ce146a1c2d4f4b1
AWOS <uws0331@gmail.com>
2023-02-04 21:22:00 +0900

    feat: 위임을 선택하는 메서드 생성 및 생성자에서 처리하도록 설정

--- src/ch12/10/12_10_2_replaceSubclassWithDelegate.js
+++ src/ch12/10/12_10_2_replaceSubclassWithDelegate.js
@@ -15,11 +15,20 @@ class Bird {
     constructor(data) {
         this._name = data.name;
         this._plumage = data.plumage;
+        this._speciesDelegate = this.selectSpeciesDelegate(data);
     }
-
     get name() { return this._name;}
     get plumage() { return this._plumage || "보통이다"; }
     get airSpeedVelocity() { return null; }
+
+    selectSpeciesDelegate(data) {
+        switch (data.type) {
+            case '유럽 제비':
+                return new EuropeanSwallowDelegate();
+            default:
+                return null;
+        }
+    }
 }
 
 class EuropeanSwallow extends Bird {
