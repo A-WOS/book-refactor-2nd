43165edad85c781464fb33534b2e0a3e228e5fa8
AWOS <uws0331@gmail.com>
2023-02-04 18:47:37 +0900

    refactor: 서브 클래스 한곳에서만 작동하는 필드는 서브클래스로 이동

--- src/main/java/net/bellsoft/ch12/pushDownField.java
+++ src/main/java/net/bellsoft/ch12/pushDownField.java
@@ -1,6 +1,6 @@
-class Employee { // 자바코드
-    private String quota;
-}
+class Employee {}
 
 class Engineer extends Employee {}
-class Salesperson extends Employee {}
+class Salesperson extends Employee {
+    private String quota;
+}
