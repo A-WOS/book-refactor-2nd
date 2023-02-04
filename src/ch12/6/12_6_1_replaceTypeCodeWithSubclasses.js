91e7d282f41abfdf400c8f6833313f15605403a1
AWOS <uws0331@gmail.com>
2023-02-04 19:03:05 +0900

    refactor: switch 문이 똑같은 검증을 해주므로 필요없는 코드 제거

--- src/ch12/6/12_6_1_replaceTypeCodeWithSubclasses.js
+++ src/ch12/6/12_6_1_replaceTypeCodeWithSubclasses.js
@@ -1,32 +1,40 @@
 class Employee {
-    constructor(name, type) {
-        this.validateType(type);
+    constructor(name) {
         this._name = name;
     }
-    validateType(arg) {
-        if (!["engineer", "manager", "salesperson"].includes(arg))
-            throw new Error(`${arg}라는 직원 유형은 없습니다.`);
+
+    toString() {
+        return `${this._name} (${this._type})`;
     }
-    toString() {return `${this._name} (${this._type})`;}
 }
 
 class Engineer extends Employee {
-    get type() { return "engineer"; }
+    get type() {
+        return "engineer";
+    }
 }
 
 class Manager extends Employee {
-    get type() { return "manager"; }
+    get type() {
+        return "manager";
+    }
 }
 
 class Salesperson extends Employee {
-    get type() {return "salesperson"; }
+    get type() {
+        return "salesperson";
+    }
 }
 
 function createEmployee(name, type) {
     switch (type) {
-        case "engineer": return new Engineer(name, type);
-        case "manager": return new Manager(name, type);
-        case "salesperson": return new Salesperson(name, type);
+        case "engineer":
+            return new Engineer(name);
+        case "manager":
+            return new Manager(name);
+        case "salesperson":
+            return new Salesperson(name);
+        default:
+            throw new Error(`${type}라는 직원 유형은 없습니다.`);
     }
-    return new Employee(name, type);
 }
