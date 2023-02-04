a4dc8070f96b74f54b722b57bfa0d55816d82796
AWOS <uws0331@gmail.com>
2023-02-04 18:33:24 +0900

    refactor: 중복 코드를 서브클래스에서 슈퍼클래스로 이동

--- src/main/java/net/bellsoft/pullUpField.java
+++ src/main/java/net/bellsoft/pullUpField.java
@@ -1,7 +1,5 @@
-class Employee {...} // 자바 코드
-class Salesperson extends Employee {
-    private String name;
+class Employee {
+    protected String name;
 }
-class Engineer extends Employee {
-    private String name;
-}
+class Salesperson extends Employee {}
+class Engineer extends Employee {}
