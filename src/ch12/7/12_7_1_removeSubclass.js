9189b1f12e8641734ebc10e10168ab071dd1f24a
AWOS <uws0331@gmail.com>
2023-02-04 19:40:08 +0900

    refactor: 생성할 클래스를 선택하는  로직을 함수로 추출

--- src/ch12/7/12_7_1_removeSubclass.js
+++ src/ch12/7/12_7_1_removeSubclass.js
@@ -3,29 +3,46 @@ class Person {
         this._name = name;
     }
 
-    get name() { return this._name; }
+    get name() {
+        return this._name;
+    }
 
-    get gender() { return "X"; }
+    get gender() {
+        return "X";
+    }
 }
 
 class Male extends Person {
-    get genderCode() { return "M"; }
+    get genderCode() {
+        return "M";
+    }
 }
 
 class Female extends Person {
-    get genderCode() { return "F"; }
+    get genderCode() {
+        return "F";
+    }
+}
+
+function createPerson(aRecord) {
+    let p;
+    switch (aRecord.gender) {
+        case 'M':
+            p = new Male(aRecord.name);
+            break;
+        case 'F':
+            p = new Female(aRecord.name);
+            break;
+        default:
+            p = new Person(aRecord.name);
+    }
+    return p;
 }
 
 function loadFromInput(data) {
     const result = [];
     data.forEach(aRecord => {
-        let p;
-        switch (aRecord.gender) {
-            case 'M': p = new Male(aRecord.name); break;
-            case 'F': p = new Female(aRecord.name); break;
-            default: p = new Person(aRecord.name);
-        }
-        result.push(p);
+        result.push(createPerson(aRecord));
     });
     return result;
 }
