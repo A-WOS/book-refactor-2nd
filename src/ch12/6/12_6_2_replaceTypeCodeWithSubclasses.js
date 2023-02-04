3bac6b811c1b01fdde68b6eba9d54c77710c8860
AWOS <uws0331@gmail.com>
2023-02-04 19:12:31 +0900

    refactor: 팩터리 메서드 생성 및 type 값에 대한 서브 클래스 작성

--- src/ch12/6/12_6_2_replaceTypeCodeWithSubclasses.js
+++ src/ch12/6/12_6_2_replaceTypeCodeWithSubclasses.js
@@ -17,10 +17,31 @@ class Employee {
     }
     get typeString() {return this._type.toString();}
     get type() { return this._type; }
-    set type(arg) { this._type = arg; }
+    set type(arg) { this._type = Employee.createEmployeeType(arg); }
+
+    static createEmployeeType(aString) {
+        switch(aString) {
+            case "engineer": return new Engineer();
+            case "manager": return new Manager();
+            case "salesperson": return new Salesperson();
+            default: throw new Error(`${aString}라는 직우너 유형은 없습니다.`);
+        }
+    }
 
     get capitalizedType() {
         return this.typeString.charAt(0).toUpperCase() + this.typeString.substr(1).toLowerCase();
     }
     toString() {return `${this._name} (${this.capitalizedType})`;}
+}
+
+class Engineer extends EmployeeType {
+    toString() {return "engineer";}
+}
+
+class Manager extends EmployeeType {
+    toString() {return "manager";}
+}
+
+class Salesperson extends EmployeeType {
+    toString() {return "salesperson";}
 }
