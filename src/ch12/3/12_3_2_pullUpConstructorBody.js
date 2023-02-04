1e08ea852aeac1af948e5ef8f81698e848bef634
AWOS <uws0331@gmail.com>
2023-02-04 18:42:56 +0900

    refactor: 추출한 메서드를 슈퍼클래스로 이동

--- src/ch12/3/12_3_2_pullUpConstructorBody.js
+++ src/ch12/3/12_3_2_pullUpConstructorBody.js
@@ -2,6 +2,9 @@ class Employee {
     constructor(name) {};
     get isPrivileged() {};
     assignCar() {};
+    finishConstruction() {
+        if (this.isPrivileged) this.assignCar(); // 모든 서브클래스가 수행한다.
+    }
 }
 
 class Manager extends Employee {
@@ -13,9 +16,5 @@ class Manager extends Employee {
 
     get isPrivileged() {
         return this._grade > 4;
-    }
-
-    finishConstruction() {
-        if (this.isPrivileged) this.assignCar(); // 모든 서브클래스가 수행한다.
     }
 }
