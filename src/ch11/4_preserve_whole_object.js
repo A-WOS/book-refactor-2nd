3a076711ecc5311cc6c96cda36598ae62a2a807b
Tetrahedrite <det3652000@naver.com>
2023-02-09 22:23:14 +0900

    refactor: inline withinRange to xxNEWwithinRange

--- src/ch11/4_preserve_whole_object.js
+++ src/ch11/4_preserve_whole_object.js
@@ -1,13 +1,10 @@
 if (!aPlan.xxNEWwithinRange(aRoom.daysTempRange))
     alerts.push("방 온도가 지정 범위를 벗어났습니다.");
 
-class HeatingPlan {
-    withinRange(bottom, top) {
-        return (bottom >= this._temperatureRange.low)
-            && (top <= this._temperatureRange.high);
-    }
 
+class HeatingPlan {
     xxNEWwithinRange(aNumberRange) {
-        return this.withinRange(aNumberRange.low, aNumberRange.high);
+        return (aNumberRange.low >= this._temperatureRange.low)
+            && (aNumberRange.high <= this._temperatureRange.high);
     }
 }
