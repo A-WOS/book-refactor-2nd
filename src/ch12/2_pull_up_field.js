2ed1552b42b2d6c2ed3e171c84c7883bf750f5ba
Tetrahedrite <det3652000@naver.com>
2023-02-10 01:44:01 +0900

    feat: Chapter 12.2 필드 올리기 기본 코드

---
+++ src/ch12/2_pull_up_field.js
@@ -0,0 +1,17 @@
+class Employee {
+
+}
+
+class Salesperson extends Employee {
+    constructor(name) {
+        super();
+        this.name = name;
+    }
+}
+
+class Engineer extends Employee {
+    constructor(name) {
+        super();
+        this.name = name;
+    }
+}
