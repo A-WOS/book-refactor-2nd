59e60e68473fccf275ca876b8a434871a8c885d4
Tetrahedrite <det3652000@naver.com>
2023-02-10 00:24:17 +0900

    refactor: remove setter and add constructor instead of id in Person class

--- src/ch11/7_remove_setting_method.js
+++ src/ch11/7_remove_setting_method.js
@@ -1,12 +1,13 @@
 function call() {
-    const martin = new Person();
+    const martin = new Person("1234");
     martin.name = "마틴";
-    martin.id = "1234";
 }
 
 class Person {
+    constructor(id) {
+        this.id = id;
+    }
+
     get name() { return this._name; }
     set name(arg) { this._name = arg; }
-    get id() { return this._id; }
-    set id(arg) { this._id = arg; }
 }
