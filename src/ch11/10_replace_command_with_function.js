870313287a217d28e624d4e9f12dfcd7d2b16c47
Tetrahedrite <det3652000@naver.com>
2023-02-10 00:53:53 +0900

    feat: Chapter 11.10 명령을 함수로 바꾸기 기본 코드

---
+++ src/ch11/10_replace_command_with_function.js
@@ -0,0 +1,19 @@
+function call() {
+    const monthCharge = new ChargeCalculator(customer, usage, provider).charge;
+}
+
+class ChargeCalculator {
+    constructor(customer, usage, provider) {
+        this._customer = customer;
+        this._usage = usage;
+        this._provider = provider;
+    }
+
+    get baseCharge() {
+        return this._customer.baseRate * this._usage;
+    }
+
+    get charge() {
+        return this.baseCharge + this._provider.connectionCharge;
+    }
+}
