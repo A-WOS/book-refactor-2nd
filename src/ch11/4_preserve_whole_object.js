b94adf2d027205870244866d06b73f2910050ce9
Tetrahedrite <det3652000@naver.com>
2023-02-09 22:24:29 +0900

    refactor: rename xxNEWwithinRange to withinRange

--- src/ch11/4_preserve_whole_object.js
+++ src/ch11/4_preserve_whole_object.js
@@ -1,9 +1,9 @@
-if (!aPlan.xxNEWwithinRange(aRoom.daysTempRange))
+if (!aPlan.withinRange(aRoom.daysTempRange))
     alerts.push("방 온도가 지정 범위를 벗어났습니다.");
 
 
 class HeatingPlan {
-    xxNEWwithinRange(aNumberRange) {
+    withinRange(aNumberRange) {
         return (aNumberRange.low >= this._temperatureRange.low)
             && (aNumberRange.high <= this._temperatureRange.high);
     }
