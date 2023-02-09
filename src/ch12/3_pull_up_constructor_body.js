fdaef855978f9ca1063f19950c8a955680e07443
Tetrahedrite <det3652000@naver.com>
2023-02-10 01:48:51 +0900

    refactor: move constructor body for init `name` to super class (Party)

--- src/ch12/3_pull_up_constructor_body.js
+++ src/ch12/3_pull_up_constructor_body.js
@@ -1,18 +1,20 @@
-class Party { }
+class Party {
+    constructor(name) {
+        this._name = name;
+    }
+}
 
 class Employee extends Party {
     constructor(name, id, monthlyCost) {
-        super();
+        super(name);
         this._id = id;
-        this._name = name;
         this._monthlyCost = monthlyCost;
     }
 }
 
 class Department extends Party {
     constructor(name, staff) {
-        super();
-        this._name = name;
+        super(name);
         this._staff = staff;
     }
 }
