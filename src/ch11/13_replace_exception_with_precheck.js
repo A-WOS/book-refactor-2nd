5c86422ee4397d827ff6110fdc2bbd23e1decb85
Tetrahedrite <det3652000@naver.com>
2023-02-10 01:34:57 +0900

    refactor: inline if statement with ternary operator

--- src/ch11/13_replace_exception_with_precheck.js
+++ src/ch11/13_replace_exception_with_precheck.js
@@ -5,20 +5,12 @@ class ResourcePool {
     }
 
     get() {
-        let result;
-        if (this.available.isEmpty()) {
-            result = Resource.create();
-            this.allocated.add(result);
-        } else {
-            result = this.available.pop();
-            this.allocated.add(result);
-        }
+        const result = this.available.isEmpty() ? Resource.create() : this.available.pop();
+        this.allocated.add(result);
         return result;
     }
 }
 
 class Resource {
-    static create() {
-
-    }
+    static create() { }
 }
