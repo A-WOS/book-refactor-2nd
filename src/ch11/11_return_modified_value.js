a37e76bec20206a799cbccbe31e0c7bd9e1a5ef4
Tetrahedrite <det3652000@naver.com>
2023-02-10 01:04:48 +0900

    refactor: make calculateAscent function to return value

--- src/ch11/11_return_modified_value.js
+++ src/ch11/11_return_modified_value.js
@@ -1,14 +1,16 @@
 let totalAscent = 0;
 let totalTime = 0;
 let totalDistance = 0;
-calculateAscent();
+totalAscent = calculateAscent();
 calculateTime();
 calculateDistance();
 const pace = totalTime / 60 / totalDistance;
 
 function calculateAscent() {
+    let result = 0;
     for (let i = 1; i < points.length; i++) {
         const verticalCharge = points[i].elevation - points[i - 1].elevation;
-        totalAscent += (verticalCharge > 0) ? verticalCharge : 0;
+        result += (verticalCharge > 0) ? verticalCharge : 0;
     }
+    return result;
 }
