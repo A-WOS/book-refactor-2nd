310fc13b9aeea3b8bcbeb075d9325784bbd3225b
Tetrahedrite <det3652000@naver.com>
2023-02-09 22:41:51 +0900

    refactor: move xxNEWwithinRange function to class method

--- src/ch11/4_preserve_whole_object_2.js
+++ src/ch11/4_preserve_whole_object_2.js
@@ -1,18 +1,18 @@
 const tempRange = aRoom.daysTempRange;
-const isWithinRange = xxNEWwithinRange(aPlan, tempRange);
+const isWithinRange = aPlan.xxNEWwithinRange(tempRange);
 if (!isWithinRange)
     alerts.push("방 온도가 지정 범위를 벗어났습니다.");
-
-function xxNEWwithinRange(aPlan, tempRange) {
-    const low = tempRange.low;
-    const high = tempRange.high;
-    const isWithinRange = aPlan.withinRange(low, high);
-    return isWithinRange;
-}
 
 class HeatingRoom {
     withinRange(bottom, top) {
         return (bottom >= this._temperatureRange.low)
             && (top <= this._temperatureRange.high)
+    }
+
+    xxNEWwithinRange(tempRange) {
+        const low = tempRange.low;
+        const high = tempRange.high;
+        const isWithinRange = this.withinRange(low, high);
+        return isWithinRange;
     }
 }
