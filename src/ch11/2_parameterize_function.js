df5fdde277106fa3c1d88ac39dbe8963d61baebe
Tetrahedrite <det3652000@naver.com>
2023-02-09 19:40:24 +0900

    feat: Chapter 11.2 함수 매개변수화하기 기본 코드

---
+++ src/ch11/2_parameterize_function.js
@@ -0,0 +1,7 @@
+function tenPercentRaise(aPerson) {
+    aPerson.salary = aPerson.salary.multiply(1.1);
+}
+
+function fivePercentRaise(aPerson) {
+    aPerson.salary = aPerson.salary.multiply(1.05);
+}
