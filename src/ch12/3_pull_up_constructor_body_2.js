446a2c7c329f66111bcf1dc328d258a25e8ec834
Tetrahedrite <det3652000@naver.com>
2023-02-10 01:54:34 +0900

    feat: Chapter 12.3 생성자 본문 올리기 예시 2 기본 코드

---
+++ src/ch12/3_pull_up_constructor_body_2.js
@@ -0,0 +1,25 @@
+class Employee extends Party {
+    constructor(name) {
+        super(name);
+    }
+
+    get isPrivileged() {
+
+    }
+
+    assignCar() {
+
+    }
+}
+
+class Manager extends Employee {
+    constructor(name, grade) {
+        super(name);
+        this._grade = grade;
+        if (this.isPrivileged) this.assignCar();
+    }
+
+    get isPrivileged() {
+        return this._grade > 4;
+    }
+}
