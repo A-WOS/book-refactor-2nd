62f89cc12d979e82c4e7ff26309ea0809c6b8362
Tetrahedrite <det3652000@naver.com>
2023-02-09 21:29:37 +0900

    refactor: replace to other band function withinBand function

--- src/ch11/2_parameterize_function_2.js
+++ src/ch11/2_parameterize_function_2.js
@@ -1,9 +1,9 @@
 function baseCharge(usage) {
     if (usage < 0) return usd(0);
     const amount =
-        bottomBand(usage) * 0.03
+        withinBand(usage, 0, 100) * 0.03
         + withinBand(usage, 100, 200) * 0.05
-        + topBand(usage) * 0.07;
+        + withinBand(usage, 200, Infinity) * 0.07;
     return usd(amount);
 }
 
