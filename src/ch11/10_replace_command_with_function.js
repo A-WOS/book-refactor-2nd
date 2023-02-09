54720f2df566b8bab91c57130d230b6e9c4ed41a
Tetrahedrite <det3652000@naver.com>
2023-02-10 01:00:46 +0900

    refactor: replace ChargeCalculator.charge with charge function

--- src/ch11/10_replace_command_with_function.js
+++ src/ch11/10_replace_command_with_function.js
@@ -3,17 +3,6 @@ function call() {
 }
 
 function charge(customer, usage, provider) {
-    return new ChargeCalculator(customer, usage, provider).charge(customer, usage, provider);
-}
-
-class ChargeCalculator {
-    constructor(customer, usage, provider) {
-        this._customer = customer;
-        this._usage = usage;
-        this._provider = provider;
-    }
-    charge(customer, usage, provider) {
-        const baseCharge = this._customer.baseRate * this._usage;
-        return baseCharge + this._provider.connectionCharge;
-    }
+    const baseCharge = customer.baseRate * usage;
+    return baseCharge + provider.connectionCharge;
 }
