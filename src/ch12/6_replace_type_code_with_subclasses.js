92c05d7fb96cdb646621194c963b0f92dc3eef39
Tetrahedrite <det3652000@naver.com>
2023-02-10 03:44:32 +0900

    refactor: add switch-case statement for create Engineer object

--- src/ch12/6_replace_type_code_with_subclasses.js
+++ src/ch12/6_replace_type_code_with_subclasses.js
@@ -22,5 +22,8 @@ class Engineer extends Employee {
 }
 
 function createEmployee(name, type) {
+    switch (type) {
+        case "engineer": return new Engineer(name, type);
+    }
     return new Employee(name, type);
 }
