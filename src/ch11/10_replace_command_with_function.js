4c0d82352f7ed7f1bb334a29af3c5ad3ab90ba5b
Tetrahedrite <det3652000@naver.com>
2023-02-10 00:56:38 +0900

    refactor: inline baseCharge to charge method

--- src/ch11/10_replace_command_with_function.js
+++ src/ch11/10_replace_command_with_function.js
@@ -12,12 +12,8 @@ class ChargeCalculator {
         this._usage = usage;
         this._provider = provider;
     }
-
-    get baseCharge() {
-        return this._customer.baseRate * this._usage;
-    }
-
     get charge() {
-        return this.baseCharge + this._provider.connectionCharge;
+        const baseCharge = this._customer.baseRate * this._usage;
+        return baseCharge + this._provider.connectionCharge;
     }
 }
