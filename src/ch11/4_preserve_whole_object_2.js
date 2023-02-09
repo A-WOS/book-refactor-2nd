f76d0771b992a2b935cb11d59e27867326fd924c
Tetrahedrite <det3652000@naver.com>
2023-02-09 22:40:38 +0900

    refactor: extract variable and wrap withinRange function for refactor

--- src/ch11/4_preserve_whole_object_2.js
+++ src/ch11/4_preserve_whole_object_2.js
@@ -1,7 +1,14 @@
-const low = aRoom.daysTempRange.low;
-const high = aRoom.daysTempRange.high;
-if (!aPlan.withinRange(low, high))
+const tempRange = aRoom.daysTempRange;
+const isWithinRange = xxNEWwithinRange(aPlan, tempRange);
+if (!isWithinRange)
     alerts.push("방 온도가 지정 범위를 벗어났습니다.");
+
+function xxNEWwithinRange(aPlan, tempRange) {
+    const low = tempRange.low;
+    const high = tempRange.high;
+    const isWithinRange = aPlan.withinRange(low, high);
+    return isWithinRange;
+}
 
 class HeatingRoom {
     withinRange(bottom, top) {
