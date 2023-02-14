d38f5bc3ed89e61c7c2503490815accbb54451a0
Tetrahedrite <det3652000@naver.com>
2023-02-15 03:13:03 +0900

    refactor: add init of the `_catalogItem` field to the constructor of the `Scroll` class before breaking inheritance

--- src/ch12/11_replace_superclass_with_delegate.js
+++ src/ch12/11_replace_superclass_with_delegate.js
@@ -14,6 +14,7 @@ class CatalogItem {
 class Scroll extends CatalogItem {
     constructor(id, title, tags, dateLastCleaned) {
         super(id, title, tags);
+        this._catalogItem = new CatalogItem(id, title, tags);
         this._lastCleaned = dateLastCleaned;
     }
 
