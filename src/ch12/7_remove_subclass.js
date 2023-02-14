68bd83d35da34ce920d60581c9a70115ecc2d729
Tetrahedrite <det3652000@naver.com>
2023-02-14 22:57:58 +0900

    refactor: modify `createPerson` function to instantiate `Person` with genderCode 'X' on default, remove unused code

--- src/ch12/7_remove_subclass.js
+++ src/ch12/7_remove_subclass.js
@@ -2,18 +2,6 @@ function call() {
     const numberOfMales = people.filter(p => p.isMale).length;
 }
 
-function createPerson(name) {
-    return new Person(name);
-}
-
-function createMale(name) {
-    return new Male(name);
-}
-
-function createFemale(name) {
-    return new Female(name);
-}
-
 function loadFromInput(data) {
     return data.map(aRecord => createPerson(aRecord));
 }
@@ -22,14 +10,14 @@ function createPerson(aRecord) {
     switch (aRecord.gender) {
         case 'M': return new Person(aRecord.name, "M");
         case 'F': return new Person(aRecord.name, "F");
-        default: return new Person(aRecord.name);
+        default: return new Person(aRecord.name, "X");
     }
 }
 
 class Person {
     constructor(name, genderCode) {
         this._name = name;
-        this._genderCode = genderCode || "X";
+        this._genderCode = genderCode;
     }
     get name() { return this._name; }
     get genderCode() { return this._genderCode; }
