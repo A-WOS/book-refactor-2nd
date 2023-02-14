62c45345327d29b05febab06c8c6542de6baf7f4
Tetrahedrite <det3652000@naver.com>
2023-02-14 22:39:39 +0900

    feat: add factory method to create instance of `Person`

--- src/ch12/7_remove_subclass.js
+++ src/ch12/7_remove_subclass.js
@@ -2,6 +2,18 @@ function call() {
     const numberOfMales = people.filter(p => p instanceof Male).length;
 }
 
+function createPerson(name) {
+    return new Person(name);
+}
+
+function createMale(name) {
+    return new Male(name);
+}
+
+function createFemale(name) {
+    return new Female(name);
+}
+
 class Person {
     constructor(name) {
         this._name = name;
