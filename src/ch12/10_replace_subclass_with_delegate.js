62d0e1d1dfe5f1e7774832b2db0bf7a3876187ee
Tetrahedrite <det3652000@naver.com>
2023-02-15 00:38:32 +0900

    refactor: move `hasTalkback` function from `PremiumBooking` to `PremiumBookingDelegate`

--- src/ch12/10_replace_subclass_with_delegate.js
+++ src/ch12/10_replace_subclass_with_delegate.js
@@ -40,10 +40,6 @@ class PremiumBooking extends Booking {
         this._extras = extras;
     }
 
-    get hasTalkback() {
-        return this._show.hasOwnProperty('talkback')
-    }
-
     get basePrice() {
         return Math.round(super.basePrice + this._extras.premiumFee);
     }
@@ -57,5 +53,9 @@ class PremiumBookingDelegate {
     constructor(hostBooking, extras) {
         this._host = hostBooking;
         this._extras = extras;
+    }
+
+    get hasTalkback() {
+        return this._host._show.hasOwnProperty('talkback')
     }
 }
