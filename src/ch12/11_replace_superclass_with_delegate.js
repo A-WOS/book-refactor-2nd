1ef47f9b98d75e305ca867af18e8402830511f1d
Tetrahedrite <det3652000@naver.com>
2023-02-15 02:43:07 +0900

    feat: Chapter 12.11 슈퍼클래스를 위임으로 바꾸기 기본 코드

---
+++ src/ch12/11_replace_superclass_with_delegate.js
@@ -0,0 +1,28 @@
+class CatalogItem {
+    constructor(id, title, tags) {
+        this._id = id;
+        this._title = title;
+        this._tags = tags;
+    }
+
+    get id() { return this._id; }
+    get title() { return this._title; }
+    hasTag(arg) { return this._tags.includes(arg); }
+}
+
+// Wrong modeling for scroll: scroll can be one or more in one CatalogItem
+class Scroll extends CatalogItem {
+    constructor(id, title, tags, dateLastCleaned) {
+        super(id, title, tags);
+        this._lastCleaned = dateLastCleaned;
+    }
+
+    needsCleaning(targetDate) {
+        const threshold = this.hasTag("revered") ? 700 : 1500;
+        return this.daysSinceLastCleaning(targetDate) > threshold;
+    }
+
+    daysSinceLastCleaning(targetDate) {
+        return this._lastCleaned.until(targetDate, ChronoUnit.DAYS);
+    }
+}
