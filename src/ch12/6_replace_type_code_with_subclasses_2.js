5d11da6cc1ddd72f19eae905d718102c0e9033cb
Tetrahedrite <det3652000@naver.com>
2023-02-14 22:21:52 +0900

    refactor: add `typeString` function and modify `capitalizedType` function to use `typeString` instead of `type`

--- src/ch12/6_replace_type_code_with_subclasses_2.js
+++ src/ch12/6_replace_type_code_with_subclasses_2.js
@@ -10,11 +10,12 @@ class Employee {
             throw new Error(`${arg}라는 직원 유형은 없습니다.`);
     }
 
+    get typeString() { return this._type.toString(); }
     get type() { return this._type; }
     set type(arg) { this._type = arg; }
 
     get capitalizedType() {
-        return this._type.charAt(0).toUpperCase() + this._type.substr(1).toLowerCase();
+        return this.typeString.charAt(0).toUpperCase() + this.typeString.substr(1).toLowerCase();
     }
 
     toString() {
